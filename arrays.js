ba1c3b9cf92c539f3b8b129ef412a2a42a370a09
LaDerrius Williams <mrldaniellewilliams@gmail.com>
2016-08-31 00:51:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function descructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
 
   return array
