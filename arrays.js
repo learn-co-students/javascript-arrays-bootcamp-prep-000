5a48522de9add74bca015579189a0b358f3daf1e
Heather Campbell <heathermkcampbell@gmail.com>
2016-09-06 06:07:35 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (array, element) {
+  return [...element, array];
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray (array, element) {
+  return [array, ...element];
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  return array.push(element);
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  return array.pop();
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1, (array.length));
+}
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, (array.length-1));
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
