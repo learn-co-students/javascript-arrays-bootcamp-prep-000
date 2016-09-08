3adc24837c392e4801dfcdf1c305560ad1bf5e24
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-08 12:23:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
+
+var array = new Array
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element)
+return array
+}
