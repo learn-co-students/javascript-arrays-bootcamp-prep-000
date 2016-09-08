e3626bafa0bbbd5523e581e9893782b4559eea7f
Heather Campbell <heathermkcampbell@gmail.com>
2016-09-08 05:53:53 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,19 +1,21 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
-  return [...element, array];
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray (array, element) {
-  return [array, ...element];
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  return array.push(element);
+  array.push(element);
+  return array;
 }
 
 function removeElementFromBeginningOfArray (array) {
