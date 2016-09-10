7ed979d0b3b7504d41d7040cd587c5a53e3c385d
Stephen Bowers <srb4887@gmail.com>
2016-09-10 23:30:04 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var arr = array;
+  return [element, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var arr = array;
+  return [...arr, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray (array) {
+  var arr = array.slice(1);
+  return arr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  var arr = array.slice(0, array.length - 1)
+  return arr;
+}
