1b6b3ff77674d7006bb8fd831fd7d98398c914e6
Andrew W <kairospirate@gmail.com>
2016-09-14 20:21:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,10 +5,10 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function descructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
 
-  return array
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
@@ -16,9 +16,9 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
+  array.push(element);
 
-  return array
+  return array;
 }
 
 function accessElementInArray(array, index) {
