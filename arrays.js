0d108c3e39e014b98338a523cd63630844987e44
chris gyasi <jblck09@gmail.com>
2016-09-17 21:43:06 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,5 +7,28 @@ function addElementToBeginningOfArray(nArray, variable){
 }
 
 function destructivelyAddElementToBeginningOfArray(nArray, variable){
-  return nArray.unshift(variable);
+  nArray.unshift(variable)
+  return nArray;
+}
+
+function addElementToEndOfArray(nArray, variable){
+  return [...nArray, variable]
+}
+
+function destructivelyAddElementToEndOfArray(nArray, variable){
+  nArray.push(variable)
+  return nArray
+}
+
+function accessElementInArray(nArray, index){
+  return nArray[index]
+}
+
+function removeElementFromBeginningOfArray(nArray){
+  nArray.shift()
+  return nArray
+}
+
+function removeElementFromEndOfArray(nArray){
+  return nArray.slice(0, nArray.length -1)
 }
