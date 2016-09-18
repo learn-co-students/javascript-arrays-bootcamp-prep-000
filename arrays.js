3c9c6eddce1cb513713bf44a7f0a7179d5424f63
David Y <imdavidy@gmail.com>
2016-09-18 18:23:16 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -30,5 +30,5 @@ var destructivelyRemoveElementFromEndOfArray = function(array){
   return array.pop();
 };
 var removeElementFromEndOfArray = function(array){
-  return array.slice(0, -1);
+  return array.slice(0, array.length - 1);
 }
