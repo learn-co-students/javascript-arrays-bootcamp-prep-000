b32952d74e24a7259bbb7c1a3712b5d8bb6e79dc
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-19 18:11:17 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element){
+ return [element, ...array]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+   array.unshift(element)
+   return array
+}
+
+function addElementToEndOfArray(array,element){
+  return[array,...element]
+}
