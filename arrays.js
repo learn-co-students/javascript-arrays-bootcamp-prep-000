e3e887f46c139a9dcbe37a7441d84a0fdd08c048
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-19 19:38:53 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -11,5 +11,24 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array,element){
-  return[array,...element]
+  return[...array,element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array){
+ array.shift();
+ return array;
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop();
+  return array;
 }
