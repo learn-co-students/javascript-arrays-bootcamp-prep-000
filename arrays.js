cb93533899f7aa3189ac290908c25733fedd7615
Lu Xing <lxing.nyc@gmail.com>
2016-09-21 19:41:28 +0000

    done

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return newArray = [element, ...array];
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
