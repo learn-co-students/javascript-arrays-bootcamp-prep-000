0ec67dd46296c8a5b3d9c47a3fe75df3614f2b72
Kelly Baez <kellybaez10@aim.com>
2016-09-23 21:16:21 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+done()
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+done()
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
+
+function accessElementInArray(array, index) {
+
+}
