48f4cf69275739e6eb7e42f17a01505db961cefb
Kelly Baez <kellybaez10@aim.com>
2016-09-24 01:05:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,16 +1,40 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-done()
+
 
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-done()
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  [element, ...array]
+  array.unshift(element)
+  return array
+}
 
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  [...array, element]
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
+  var element = array[index]
+  return element
+}
 
+function removeElementFromBeginningOfArray(array) {
+  array.shift([0])
+  return array
+}
+
+
+function removeElementFromEndOfArray(array) {
+  array.pop([0])
+  return array
 }
