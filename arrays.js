cc9dcf682803a8079a72f069e42349bd138dbacf
Robert Bene <robcbene@hotmail.com>
2016-09-25 22:53:40 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var array = [1, 2, 3]
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+array.unshift (element)
+return array;
+}
+
+function addElementToEndOfArray (array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push (element)
+  return array;
+}
+
+function accessElementInArray (array, index) {
+  return (array[index]);
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1)
+}
+
+function removeElementFromEndOfArray (array) {
+  array.pop()
+  return array;
+}
