170f63ec6f6193124b187d5c1c27a748825ec01c
miles hammock <miles0569@gmail.com>
2016-09-30 17:41:37 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = [
+  'snickers', 'hundred grand', 'kitkat', 'skittles'
+]
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
