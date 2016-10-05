176eb36a20f8b80dbf13a4b0599bbe55d9614633
Pamela Lee <pamela@mintybanana.com>
2016-10-05 22:16:29 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array
+}
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
+function accessElementInArray(array, index) {
+  return (array[index]);
+}
+function removeElementFromBeginningOfArray(array,element) {
+  array.shift(element);
+  return array
+}
+function removeElementFromEndOfArray(array, element) {
+  array.pop(element);
+  return array
+}
