1d5d2e1594df7a104b43c1653de6bdb80819fa01
Haebin Liew <haebinliew@gmail.com>
2016-10-07 03:56:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,18 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
+  array.unshift(element);
   return array;
 }
 
-function addElementToEndOfArray() {
-  return [...array, element];
+function addElementToEndOfArray(array, element) {
+  array = [...array, element];
+  return array;
 }
 
-function destructivelyAddElementToEndOfArray() {
-  array = [...array, element];
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
   return array;
 }
 
