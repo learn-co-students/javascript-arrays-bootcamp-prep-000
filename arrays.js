80dd3643c4143d3edfe5abed9f6a6f3c6742d0ea
Tim nguyen <timdung_tim@yahoo.com>
2016-10-13 02:08:25 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(array,element) {
+	return [element,...array];
+}
+/* 1 */
+function destructivelyAddElementToBeginningOfArray(array,element) {
+ return [element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+   array.unshift(element);
+	 return array;
+}
+
+function addElementToEndOfArray(array,element){
+
+	return [...array,element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+
+return [...array, element];
+
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+	  array.push(element);
+		return array;
+}
+
+function accessElementInArray(array,index) {
+	return array[index];
+}
+
+function removeElementFromBeginningOfArray(array) {
+	  array.shift();
+		return array;
+}
+
+function removeElementFromEndOfArray(array) {
+	 array.pop();
+	 return array;
+}
