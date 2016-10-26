74e51cfd474ab37d2b30fd1ae16567a8c28206c6
Alexis Vazquez <alexisvaz93@yahoo.com>
2016-10-26 01:36:46 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,55 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element){
+  array = [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+//---------------------------------------------
+
+
+function addElementToEndOfArray(array, element){
+  array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+//--------------------------------------------
+
+function accessElementInArray(array, index){
+  array = array[index]
+  return array
+}
+//--------------------------------------------
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array = array.shift(0)
+  return array
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array = array.slice(-1)
+  return array
+}
+
+
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
+}
+
+
+function removeElementFromEndOfArray(array){
+  array = array.slice(0, array.length - 1)
+  return array
+}
