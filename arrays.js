98db2bc52d309dd3870b6b21b8ee81eca0ca2a95
jason brown <j.grant.brown@gmail.com>
2016-10-28 15:18:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -25,3 +25,7 @@ function accessElementInArray(array, index){
 function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift();
 }
+
+function removeElementFromBegininnigOfArray(array){
+  return array.slice(1)
+}
