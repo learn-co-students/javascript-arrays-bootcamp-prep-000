b2580188a7f608b7f553d3604470063447f01105
jason brown <j.grant.brown@gmail.com>
2016-10-28 15:23:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -29,3 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
