0cc58fd2d4f2e99817546eda39c781172df855ca
jason brown <j.grant.brown@gmail.com>
2016-10-28 15:13:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -17,3 +17,7 @@ function addElementToEndOfArray(array,element){
 function destructivelyAddElementToEndOfArray(array,element){
   return array.push(element);
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
