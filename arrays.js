867ddcb5182e1612b008318d143b678be80aed63
jason brown <j.grant.brown@gmail.com>
2016-10-28 14:30:29 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars =[ 'snickers','hundred grand','kitkat','skittles']
-function addElementToBeginningOfArray([],element){
-  
+function addElementToBeginningOfArray(array,element){
+
 }
