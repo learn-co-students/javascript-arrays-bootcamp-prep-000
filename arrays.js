d4af03cad41b1ceed03ae84064ed00f27064dbd3
Elizabeth Hall <elizabethhall205@gmail.com>
2016-10-29 21:20:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return[element, ...array]
+}
+
+function destructivelyAddElementsToBeginningOfArray(array, element) {
+  array.unshift(element)
+
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+            return[...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+              array.push[element]
+
+    return array
+}
