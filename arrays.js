ff9a125c926bf660a9874ee9d8f4763f8d667ff4
Mahdi Ezzedine <mahdi.ezzedinee@gmail.com>
2016-11-03 11:31:55 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(array,element) {
+array.unshift(element)
+return array
+}
+function destructivelyAddElementToBeginningOfArray(array,element) {
+array.unshift(element)
+return array
+}
+function addElementToEndOfArray(array,element) {
+array.push(element)
+return array
+}
+function destructivelyAddElementToEndOfArray(array,element) {
+array.push(element)
+return array
+}
+function accessElementInArray(array,index) {
+return array[index]
+}
+function removeElementFromBeginningOfArray(array) {
+array.shift()
+return array
+}
+function removeElementFromEndOfArray(array) {
+array.pop()
+return array
+}
