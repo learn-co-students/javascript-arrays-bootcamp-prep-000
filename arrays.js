db7fe29b60bd9d6012c06ca58cf513dc59e3920b
DeAundra Peek <jingyu1@gmail.com>
2016-11-06 04:05:27 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -16,8 +16,14 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index){
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
 }
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length -1);
