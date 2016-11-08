d35fed3b945f1f944bf40d521e0223dfd9ec3527
Yi-Xuan Lau <yixuan.lau@gmail.com>
2016-11-08 09:47:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, element){
+  return [element, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(arr, element){
+  return [...arr, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(arr, index){
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift();
+}
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop();
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length - 1)
+}
