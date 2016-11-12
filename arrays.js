d523be31ffc55ad815221da550ce2f21c937d887
Matteuw Pickman <mattpickman123@gmail.com>
2016-11-12 00:29:43 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element)  { return(
+  array = [(element),...array])
+}
+function destructivelyAddElementToBeginningOfArray(array, element)  {
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element)  {
+  return( array = [...array, (element)])
+}
+function destructivelyAddElementToEndOfArray(array, element)  {
+  array.push(element)
+  return array }
+function accessElementInArray(array, index) {
+  return(array[2])
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return(array.shift())
+}
+function removeElementFromBeginningOfArray(array) {
+  return(array.slice(1))
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return(array.pop())
+}
+function removeElementFromEndOfArray(array) {
+  return array = (array.slice(0, array.length - 1) )
+}
