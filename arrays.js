f5aa19e02bbe044793d5953016b8017c4f17d456
Rich Sanchez <supernalrobin37@gmail.com>
2016-11-16 17:33:42 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a,b){
+  return [b,...a];
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+  a.unshift(b);
+  return a;
+}
+
+function addElementToEndOfArray(a,b){
+  return [...a,b]
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  a.push(b);
+  return a;
+}
+
+function accessElementInArray(a, b){
+  return a[b];
+}
+
+function removeElementFromBeginningOfArray(a, b){
+  c = a.slice(1);
+  return c;
+}
+
+function destructivelyRemoveElementFromEndOfArray(a, b){
+  a.pop();
+  return a;
+}
+
+function removeElementFromEndOfArray(a, b){
+  c = a.slice(0, a.length - 1);
+  return c;
+}
