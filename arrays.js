49454762c9538f42da206d458c7f5b02f5b14feb
Chibi Chu <flubbyflub@gmail.com>
2016-11-18 21:47:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (arr, element) {
+  arr = [element, ...arr]
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray (arr, element) {
+  arr = [...arr, element];
+  return arr;
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr, index) {
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr){
+  arr = arr.slice(1);
+  return arr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  arr = arr.slice(0, arr.length - 1)
+  return arr;
+}
