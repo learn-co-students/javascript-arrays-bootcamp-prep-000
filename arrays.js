b53518565d0de1b31e0e3b2821ef4e76b652a003
Javier Ernesto Moran Lemus <moranlemusj@carleton.edu>
2016-11-25 18:33:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray(array, element){
+  var newarray = [element, ...array]
+  return newarray
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  var newarray = [...array, element]
+  return newarray
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElemetFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  var newarray = array.slice(1)
+  return newarray
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+function removeElementFromEndOfArray(array){
+  var newarray = array.slice(0, array.length - 1)
+  return newarray
+}
