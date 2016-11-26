0d53c24419b8763cefe01d33c5b514b2678b9718
Michael Capps <michaelcapps1772@yahoo.com>
2016-11-26 22:45:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array;
+  newArray.unshift(element);
+  return newArray;
+};
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+};
+
+function addElementToEndOfArray(array, element){
+  var newArray = array;
+  newArray.push(element);
+  return newArray;
+};
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+};
+
+function accessElementInArray(array, index){
+  return array[index];
+};
+
+function removeElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+};
+
+function removeElementFromEndOfArray(array){
+  array.pop();
+  return array;
+};
