48bc2b18d6d5964e1f82eb5db3655d9ed1d107aa
Josh Cone <joshcone@googlemail.com>
2016-12-04 18:08:37 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+var addElementToBeginningOfArray = function(array, "bangbang") {
+  array.unshift("bangbang");
+  return array;
+}
