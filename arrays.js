20104a852da96294ba13df993fd2dc454c4d2e54
Rudy Zapata <rstar026@yahoo.com>
2016-12-09 19:39:51 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+var array = ["array", "element"]
+function addElementToBeginningOfArray (){
+
+  return array.unshift["element"]
+}
