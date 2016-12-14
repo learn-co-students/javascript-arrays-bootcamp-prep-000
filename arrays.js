3918ac7ee46e39e685aa72dd5023d0c7a977d4c7
veena penugonda <veenav46@gmail.com>
2016-12-14 06:45:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -12,11 +12,31 @@ array.unshift(element);
 return array ;
  }
 function addElementToEndOfArray (array, element){
-addElement = [array,...element];
-return addElement;
+array= [...array,element];
+return array;
 }
 
-function destructivelyAddElementToEndOfArray(){
+function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+function removeElementFromBeginningOfArray (array){
+  array = array.slice(1);
+  return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+function removeElementFromEndOfArray(array){
+array =  array.slice(0, array.length - 1);
   return array;
 }
