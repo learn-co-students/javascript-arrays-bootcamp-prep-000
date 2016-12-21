8dc8103bcabf52e125c927932fb998b78123adf2
Lily Ling <lilyyling@gmail.com>
2016-12-21 07:23:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var ingredient1= "bread"
+var ingredient2= "mild cheese"
+var ingredient3= "sharp cheese"
+var ingredient4= "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+function chocolateBars(1,2,3,4){
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kit kat',
+  'skittles']
+}
+
+
+function addElementToBeginningOfArray(){
+  var addElementToBeginningOfArray =
+['foo']}
+{
+addElementToBeginningOfArray.push(1)
+}
+}
+}
