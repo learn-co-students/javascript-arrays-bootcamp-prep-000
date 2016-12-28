1a782a900265b8cca48a8b9b18333fd9386ebdc4
MANIKANTA GANDHAM <manikantagandham11@gmail.com>
2016-12-28 21:07:10 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -11,3 +11,21 @@ function addElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array){
+ array.shift()
+ return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
