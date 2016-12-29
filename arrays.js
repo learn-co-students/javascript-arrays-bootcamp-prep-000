c8dae3850c299185ff78796a95cdb7e3fafdcbcf
Elvis Delahoz <elvisdelahoz@gmail.com>
2016-12-29 04:29:21 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = [
+  'snickers',
+   'hundred grand',
+    'kitkat',
+     'skittles'
+]
+chocolateBars;
+foo = 0; myarray = ["entry0", "entry1"];
+function addElementToBeginningOfArray(myarray, foo) { //  var foo; var myarray = ["entry0", "entry1"];
+    myarray = [`foo`, ...myarray];
+    var mynewarray = myarray;
+    return myarray;
+}
+addElementToBeginningOfArray(myarray, 1);
+function destructivelyAddElementToBeginningOfArray(myarray, foo) {
+  myarray = [`foo`, ...myarray];
+  var mynewarray = myarray;
+  return myarray;
+}
+destructivelyAddElementToBeginningOfArray(myarray, 1);
+/*
+function destructivelyAddElementToEndOfArray(myarray, foo) {
+  myarray.push(`foo`);
+  myarray.unshift(`foo`);
+  myarray;
+  }
+destructivelyAddElementToEndOfArray(myarray, 1);
+function addElementToEndOfArray(myarray, foo) {
+  myarray = [...myarray, `foo`];
+  var mynewarray = myarray;
+  mynewarray;
+}
+addElementToEndOfArray(myarray, 1);
+function accessElementInArray() {
+
+}
+accessElementInArray();
+*/
