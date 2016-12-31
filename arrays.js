0d75dad494426005a984f4daa0e9c2cabbf2ab5c
Ron Nero <ronaldc.nero@gmail.com>
2016-12-31 06:26:22 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray() {
+  var rapperRank = [1,"Jay",3,"Big"];
+    rapperRank.unshift("12");
+    return rapperRank;
+}
