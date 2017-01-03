16f1adae4f083f4e95f401455ff816aa5a48df7f
Mike Tangaro <mtangaro@gmail.com>
2017-01-03 17:53:55 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray () {
+var hockeyTeams = ["Blues", "Blackhawks"]
+  hockeyTeams = ["Wild", ...hockeyTeams]
+  console.log(hockeyTeams)
+}
