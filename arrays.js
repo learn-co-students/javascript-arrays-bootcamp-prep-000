27b483956e408418162319e2b8e259ea475813c3
Sabin Cauldron <presspermanent@gmail.com>
2017-01-11 05:29:50 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(array[0]);
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(0);
+  return array.slice(1);
 }
 
 function removeElementFromEndOfArray(array) {
