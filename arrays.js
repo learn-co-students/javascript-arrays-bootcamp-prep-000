f081370407aa45e4bda0aa3e48b1a03c031f34f9
Art Mour <artmour@gmail.com>
2017-01-15 01:08:23 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -9,4 +9,23 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1)
 }
