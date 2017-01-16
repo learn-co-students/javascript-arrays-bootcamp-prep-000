07d8c004be4c1b62481813bd91a7cb880c3f993b
Juha  Bach <cdalsoniii@gmail.com>
2017-01-16 14:35:28 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var addElementToBeginningOfArray = ([], foo) => {
+
+var newArray = [foo, ...chocolateBars];
+
+return newArray
+
+}
+
+var destructivelyAddElementToEndOfArray = ([], foo) => {
+
+}
