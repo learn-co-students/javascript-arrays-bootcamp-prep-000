4c3c60a9e3b96eb339eba849f94d73490c66c1fa
Vivian Kulumba <vkulumba@gmail.com>
2017-01-19 03:03:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,35 +10,28 @@ function addElementToBeginningOfArray() {
 var array = [1]
 
 function destructivelyAddElementToBeginningofArray() {
-  [1, ...'foo'];
+  ['foo', ...array];
   return (['foo', 1]);
 }
 
 var array = [1]
 
 function addElementToEndOfArray() {
-  array.push('foo');
+  [1, ...'foo'];
   return ([1, 'foo']);
 }
 
 var array = [1]
 
 function destructivelyAddElementToEndOfArray() {
-  array.unshift('foo');
+  array.push('foo');
   return ([1, 'foo']);
 }
 
-var array = ([1,2,3], 2)
+var array = [1,2,3]
 
 function accessElementInArray() {
-  return ([3]);
-}
-
-var days = ["Monday", "Tuesday", "Wednesday"]
-
-function destructivelyRemoveElementFromBegginingOfArray() {
-  days.shift(["Monday"]);
-  return (["Tuesday", "Wednesday"]);  
+	console.log (array[2]);
 }
 
 var array = [1,2,3]
@@ -46,13 +39,6 @@ var array = [1,2,3]
 function removeElementFromBeginningOfArray() {
   array.shift([1])
   return([2,3])
-}
-
-var array = [1,2,3]
-
-function destructivelyRemoveElementFromEndOfArray() {
-  array.pop([2]);
-  return([1,2]);
 }
 
 var array = [1,2,3]
