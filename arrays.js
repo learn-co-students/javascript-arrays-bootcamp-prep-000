6a32b1f319d86b719c6b53563ee9c20f5bcec239
Jeremy Esqueda <jeremy@wiredjuice.com>
2017-01-21 01:23:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray([array, ...chocolateBars], element){
+  return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift;
+}
