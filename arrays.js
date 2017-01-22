d3ee6ac1ffb57ab68b7e68f80e0bdda1ea4fc008
Tyler Bristol <tahbristol@gmail.com>
2017-01-22 01:50:59 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,14 +1,4 @@
-var ingredient1 = "bread";
-var ingredient2 = "mild cheese";
-var ingredient3 = "sharp cheese";
-var ingredient4 = "butter";
-var ingredient5 = "tomato";
-var ingredient6 = "garlic";
 
-var grilledCheeseIngredients = ['bread','mild cheese','sharp cheese','butter','tomato','garlic'];
-var tomatoSauceIngredients = ['tomato','garlic','olive oil','basil','oregano'];
-
-var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(arr,e){
     var newArray = arr;
