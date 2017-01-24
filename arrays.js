b118851fa62249c17b4bd61a6c26c7c47d54ccd5
Victor Kolatau <pitbulldogy@gmail.com>
2017-01-24 19:30:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+
+var tooly = [1]
+var joke = "foo"
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [joke, ...tooly]
+}
+var priNup = [1]
+function addElementToEndOfArray(array, element) {
+  return [...priNup, joke]
+}
+
+
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+var numeroUno = [1]
+  numeroUno.unshift('foo')
+  return numeroUno
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  priNup.push('foo')
+  return priNup
+}
+
+
+
+numbers = [1,2,3]
+function accessElementInArray(array, index){
+  return numbers [2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(element, array){
+  return destructivelyRemoveElementFromBeginningOfArray.shift(element);
+}
