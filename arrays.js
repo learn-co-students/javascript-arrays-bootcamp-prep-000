d42aed7a61df756ccb83574a416e4d32582b9fba
Henno Fourie <bortch@gmail.com>
2017-01-27 09:02:53 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,20 +5,42 @@ var chocolateBars = [
   "skittles"
 ];
 
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array]
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
 }
 
-var destructivelyRemoveElementFromEndOfArray = [
-  "Mercedes Benz",
-  "BMW",
-  "Porsche"
-]
-destructivelyRemoveElementFromEndOfArray.slice(0, destructivelyRemoveElementFromEndOfArray.length - 1)
-destructivelyRemoveElementFromEndOfArray()
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift (element)
+  return array
+}
 
-var removeElementFromBeginningOfArray = [
-  "Toyota",
-  "Nissan",
-  "Mazda"
-]
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push (element)
+  return array
+}
+
+function accessElementInArray(array, element) {
+  return (array[2])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift ()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop ()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1 )
+}
