6fd37460c7f9a5ae369b34d406b3ff6c0ac18e27
Tayeb Mc <gomail654@gmail.com>
2017-01-31 17:37:07 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snicker", "hundred grand", "kitkat","skittles"];
+
+var addElementToBeginningOfArray = ["array", "element"];
+
+var addElementToBeginningOfArray = ["array", "element"];
+["foo", 1, ...addElementToBeginningOfArray];  
+
+var destructivelyAddElementToBeginningOfArray = ["array","element"];
+ destructivelyAddElementToBeginningOfArray.unshift("foo", 1);
+
+ var destructiveddElementToEndOfArray = ["array", "element"];
+ destruciveAddElementToEndgOfArray.push (1, "foo");  
+
+ var accessElementInArray = [1, 2, 3];
+ accessElementInArray[2];
+
+ var removeElementFromBeginningOfArray =[1, 2, 3];
+    removeElementFromBeginningOfArray.slice(1);
+
+var removeElementFromEndOfArray = [1, 2, 3];
+   removeElementFromEndOfArray.slice(-1);
