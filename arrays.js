f8b1dd48833e740172b7ec1c2011119b4c246b25
Sadavath Sharma <sadavath@utexas.edu>
2017-02-05 21:46:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a["X","Y"],b1) {
+  var x = addElementToBeginningOfArray.unshift("Hello")
+  return x
+}
+
+function destructivelyAddElementToBeginningOfArray(b["X","Y"],b2) {
+  b["Hey",...b]
+  return b
+}
