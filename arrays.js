ac08fa1d320abc47769437264879e69b7f70826c
Joel Temple <joeltemple1@gmail.com>
2017-02-07 07:19:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+var array = [];
+
+function addElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+}
+function addElementToBeginningOfArray (array, element) {
+var array = [element, ...array]
+return array
+}
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array
+}
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+}
+function accessElementInArray (array, index) {
+  return array[index];
+}
+function removeElementFromBeginningOfArray (array) {
+  array.shift()
+  return array
+}
+function removeElementFromEndOfArray (array) {
+  array.pop()
+  return array
+}
