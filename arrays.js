e770a0ae21f91ea51edc2f73e4f234beabdc85cc
Ranjan Bajracharya <ranjanbajra@gmail.com>
2017-02-08 05:07:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array,element){
+array=[element, ...array]
+return  array
+
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+array.unshift(element)
+return array
+}
+function addElementToEndOfArray(array,element){
+  array=[ ...array, element]
+  return  array
+}
+function destructivelyAddElementToEndOfArray(array,element){
+array.push(element)
+return array
+}
+function accessElementInArray(array,index){
+  return array[index]
+}
+function removeElementFromEndOfArray(array){
+array.pop()
+return array
+}
+function removeElementFromBeginningOfArray(array){
+array.shift()
+return array
+}
