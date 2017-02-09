67271e5083c8819433a898813f123e49c1734c52
Curtis Greene <curtis.j.w.greene@gmail.com>
2017-02-09 17:27:07 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray('array', 'element') {
 }
