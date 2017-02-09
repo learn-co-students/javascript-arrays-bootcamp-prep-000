62623d5a019a36ad5a9ab82313df4ac26d67eeff
Heather Mason <heatem@gmail.com>
2017-02-09 22:12:27 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
+
+// element to add to the beginning of the array, and both functions should
+// add the element to the beginning of the array and then return the whole array.
+var addElementToBeginningOfArray = (array, element) => {
+  // should return a new array and not modify the original.
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
