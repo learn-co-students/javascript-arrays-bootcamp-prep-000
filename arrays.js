e2dc264538700a17ec800a692177eccf997b9b05
James Tabush <frozonorb@gmail.com>
2017-02-12 20:27:54 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,44 +1,40 @@
-var chocolateBars = [
-  'snickers', 
-  'hundred grand', 
-  'kitkat', 
-  'skittles'
-];
-
-function addElementToBeginningOfArray(array, element) {
+//create new array chocolateBars with items snickrs, jundre grand, kitkat and skittles  
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+//Add element to beginning of array without changing array
+function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
-
+//add element to beginning of array changing array forever
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
 
-function addElementToEndOfArray(array, element) {
+//add an element to the end of an array without changing array forever
+function addElementToEndOfArray(array, element){
   return [...array, element];
 }
-
+//add an element to end of array changing array forever
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
-
+//create an array that returns the array value at index
 function accessElementInArray(array, index){
   return array[index];
 }
-
-function removeElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+//Takes an array as its only argument and removes the first element then returns array
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
 }
-
-function removeElementFromBeginningOfArray(array) {
+//takes array as only arument and removes an element form the beginning of the array without changing it
+function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
+//remove element from end of array
 function removeElementFromEndOfArray(array){
-  array.pop(array);
-  return array;
+  return array.slice(0, array.length-1);
+  
 }
-
 
