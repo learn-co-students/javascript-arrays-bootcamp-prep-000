adffde74192e8724c54c4a3f65dbd237987ea418
Natalie Dean <ndean87@gmail.com>
2017-02-14 03:12:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var array = [element , ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var array = [...array, element];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0, array.length - 1);
+  return newArray;
+}
