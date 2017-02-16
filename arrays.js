dcde4b9260fffeb1c8cdeb61f9bb742793bb6163
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-02-16 19:43:05 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,26 +1,26 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-return [element,...array]
+    return [element, ...array]
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
       array.unshift(element)
 
       return array
 }
+
 function addElementToEndOfArray(array, element) {
-    array.push(element)
-
-    return array
-
+  return [...array, element]
 }
-function destructivelyAddElementToEndOfArray(array,element){
-  array.push(element)
 
-  return array
+function destructivelyAddElementToEndOfArray(array, element) {
+      array.push(element)
+
+      return array
 }
+
 function accessElementInArray(array, index) {
-  return array[index]
-
+    return array[index]
 }
 
 function removeElementFromBeginningOfArray (array) {
