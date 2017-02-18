2af4094d4c4441b1c2312c3891f9a64a9c8fc1a0
subash karki <skarki35205@hotmail.com>
2017-02-18 01:38:53 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,44 @@
 var chocolateBars = ["snickers", "hundres grand", "kitkat", "skittles"];
 
-function accessElementInArray(){
-    
+function accessElementInArray(array, element){
+    return [elements, array];
+
 }
+
+function destructivelyAddElementToEndOfArray(array, element){
+    array.unshift(element);
+
+}
+
+function addElementToEndOfArray(){
+    return[array, element];
+
+}
+function destructivelyAddElementToEndOfArray(){
+    array.push(element);
+    return array;
+
+}
+function accessElementInArray(array, index){
+    return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+   array.shift()
+
+   return array
+ }
+
+ function removeElementFromBeginningOfArray(array) {
+   return array.slice(1)
+ }
+
+ function destructivelyRemoveElementFromEndOfArray(array) {
+   array.pop()
+
+   return array
+ }
+
+ function removeElementFromEndOfArray(array) {
+   return array.slice(0, array.length - 1)
+ }
+ done();
