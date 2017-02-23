496323236a8e32092f627180f64b5216d4bf18b1
Steven Hui <stevenhhui@gmail.com>
2017-02-23 05:09:28 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,10 +6,23 @@ var chocolateBars = [
   "skittles"
 ]
 
+// This function takes two parameters:
+// 1) an array, 2) an element to add to the beginning of the array.
+// This function does not alter the original array that's passed in,
+// but rather return a new array and not modify the original.
+// When used in an array, the spread operator (...) creates a new array
+// in place, rather than modifying the original one.
+
 function addElementToBeginningOfArray(array, element){
   var addToArray = [element, ...array]
   return addToArray
 }
+
+// This function takes two parameters:
+// 1) an array, 2) an element to add to the beginning of the array.
+// This function alters/changes the original array that's passed in.
+// The push (pushes elements onto end of array) and unshift (adds element to
+// front of array) methods change/mutate the underlying array.
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
@@ -38,6 +51,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+
+// The slice method takes a slice from its array. The first argument specifies
+// where the slice starts, and the second argument specifies where it ends.
+// If there is no second argument, the slice goes from the first argument
+// (the start) to the end of the array.
 
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
