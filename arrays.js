ebb14e4a5012cb1f9a64bc00811703dc41e05240
Joseph Doyle <j.doyle345@gmail.com>
2017-02-24 23:40:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat". "skittles"];
+
+var addElementToBeginningOfArray function(array, element) {
+  newArray=[element, array];
+  return newArray;
+}
+
+var destructivelyAddElementToBeginningOfArray function(array, element) {
+  newArray=[element, ...array];
+  return newArray;
+}
+
+var addElementToEndOfArray function() {
+  newArray=[array, element];
+  return newArray;
+}
+
+var addElementToEndOfArray function() {
+  newArray=[...array, element];
+  return newArray;
+}
