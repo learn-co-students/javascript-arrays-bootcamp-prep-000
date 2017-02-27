e7e388ddd12fb34cfe8cf8bcfa3ef606d807e807
William  Martinez <willrmartinez7@gmail.com>
2017-02-27 15:40:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(array,element){
+  return [element, ...array];
+  return array;
+};
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return array.unshift(element);
+};
+
+
+
+function removeElementFromBeginningOfArray(){
+  
+};
