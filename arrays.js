63dbc460d25595c2a632738ccfd47ab56f1017e7
Josie Kelley <jcakelley@gmail.com>
2017-03-01 17:48:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var addElementToBeginningOfArray = ([1,2,3], 4) => {
+addElementToBeginningOfArray.push(4)
+return addElementToBeginningOfArray
+}
+
+var destructivelyAddElementToBeginningOfArray = function(){
+
+}
