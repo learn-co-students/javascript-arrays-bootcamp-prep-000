074b209b62fee094f5f286e90e2d52c4c5bd7a75
Josie Kelley <jcakelley@gmail.com>
2017-03-02 03:50:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,11 +6,42 @@ var ingredient5 = "tomato"
 var ingredient6 = "garlic"
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var addElementToBeginningOfArray = ([1,2,3], 4) => {
-addElementToBeginningOfArray.push(4)
-return addElementToBeginningOfArray
+
+function addElementToBeginningOfArray(array, element) {
+array = [element,...array]
+return array
 }
 
-var destructivelyAddElementToBeginningOfArray = function(){
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift(element)
+return array
+}
 
+function addElementToEndOfArray(array, element){
+  array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop()
+  return array
 }
