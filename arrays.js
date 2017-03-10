f3abad94bac6b00595f3d6b56cb8db6612fe6989
Kwadwo Ashong <ktashong@gmail.com>
2017-03-10 17:27:35 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars=["snickers", "hundred grand", "kit kat", "skittles"];
+function addElementToBeginningOfArray(array,element){
+  return ([element, ... array]);  
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return (array.unshift(element));
+}
+function accessElementInArray(array, index){
+  return (array[index]);
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return(array.shift());
+}
+function removeElementFromBeginningOfArray(array){
+  return (array.slice(1));
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return (array.pop());
+}
+function removeElementFromEndOfArray(array){
+  return (array.slice(0, array.length-1));
+}
