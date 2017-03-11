cc56f3fd0b5aa1332d38865dd7d454baf582dc20
Abdul Rahman Quwatli <aznq2005@yahoo.com>
2017-03-11 01:45:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,55 @@
+chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element)
+{
+  var newArray=[element, ...array]
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element)
+{
+  var newArray=[...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array;
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array)
+{
+  array.pop();
+  return array
+}
