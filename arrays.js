fe3697127af78279f899248206cb0e6259eb5a50
Narcita Hamilton <majesticwaterfall11@yahoo.com>
2017-03-13 18:41:28 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,59 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
+var name = chocolateBars
+function chocolate(){
+  return chocolate;
+}
+
+function addElementToBeginningOfArray(array,element) {
+array.unshift(element);
+array = array;
+return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element);
+return array;
+}
+
+function addElementToEndOfArray(array, element) {
+array.push (element);
+array = array;
+return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+array.push (element);
+return array;
+
+return array;
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+
+array = array.slice (1)
+return array
+}
+
+function  destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift(1);
+  return array;
+}
+function removeElementFromBeginningOfArray(array){
+array.shift();
+  return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array;
+}
+function removeElementFromEndOfArray(array){
+  var array = [1,2,3];
+  array.pop (-3);
+  return array;
+}
+
+function accessElementInArray(array, index){
+
+array.index = 3;
+    return array [index]
+}
