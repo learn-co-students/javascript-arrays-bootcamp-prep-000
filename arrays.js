d1bfb35886d5378ea16e667d023ec602733f4fcb
Stephanie Seib <sseib138@gmail.com>
2017-03-15 17:23:40 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,56 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (array, element){
+
+return['foo', ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element)
+{
+
+array.unshift ('foo')
+
+return array
+}
+
+
+function addElementToEndOfArray (array, element){
+
+return[...array, 'foo',]
+}
+
+function destructivelyAddElementToEndOfArray (array, element)
+{
+
+array.push ('foo')
+
+return array
+}
+
+function accessElementInArray (array, index){
+
+return array [index]
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array)
+{array.shift ()
+return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+
+  return array.slice(-2)
+
+}
+
+function destructivelyRemoveElementFromEndOfArray (array)
+{array.pop ()
+return array
+}
+
+function removeElementFromEndOfArray (array) {
+
+  return array.slice(0, array.length - 1)
+
+}
