1465c190f242b7e193b488e3c4b53a894a9ac669
Adriann Bracken <adriannbracken214@gmail.com>
2017-03-16 20:23:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,18 +1,41 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(chocolateBars, candy) {
-  chocolateBars.unshift(candy);
+  return [candy, ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candy) {
+  chocolateBars.unshift(candy)
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candy) {
+  return [...chocolateBars, candy]
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candy) {
+  chocolateBars.push(candy);
+  return chocolateBars;
+}
+
+function accessElementInArray(chocolateBars, index){
+  return chocolateBars[index]
+}
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift()
   return chocolateBars
 }
 
-var arrayOfNumbers = [2,3,4,5]
-
-function destructivelyAddElementToBeginningOfArray(arrayOfNumbers, number) {
-  arrayOfNumbers = [number, ...arrayOfNumbers]
-  return arrayOfNumbers
+function removeElementFromEndOfArray(chocolateBars) {
+return chocolateBars.slice(0, chocolateBars.length -1)
 }
 
-function destructivelyAddElementToEndOfArray(arrayOfNumbers, number) {
-  arrayOfNumbers = [arrayOfNumbers..., number]
-  return arrayOfNumbers
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  chocolateBars.pop()
+  return chocolateBars
 }
