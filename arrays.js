fa1411da64419305f5b46b66ca79edb80c14ecf8
Dustin Astrin <dustastrin@gmail.com>
2017-03-17 18:40:10 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var a = ["hello", 2]
+
+function addElementToBeginningOfArray(a,b) {
+  return [b, ...a]
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+a.unshift(b);
+return a
+}
+
+function addElementToEndOfArray(a,b) {
+  return [...a, b]
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  a.push(b)
+return a
+}
+
+function accessElementInArray(a,b) {
+return a[b]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a) {
+a.shift();
+return a
+}
+
+function removeElementFromBeginningOfArray(a) {
+return a.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(a) {
+a.pop()
+return a
+}
+
+function removeElementFromEndOfArray(a) {
+return a.slice(0, a.length - 1)
+}
