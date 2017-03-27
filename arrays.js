269a6194323cf4ed2fa3cea206cd06bb4f5edb73
Jacqueline Kukulski <jakukuls@mtu.edu>
2017-03-27 23:31:35 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
+function addElementToBeginningOfArray(){
+  var array = [];
+  array.unshift("element")
+}
+
+function destructivelyAddElementToBeginningOfArray (){
+  var array = [];
+  array[0]="element"
+}
