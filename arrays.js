a5823d25f96ccdb8d40156d307832fc82e0264c7
Dan Strong <dangaston@gmail.com>
2017-03-28 20:58:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ['snickers', 'hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var new_array = array.slice();
+  new_array.unshift(element);
+  return new_array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var new_array = array;
+  new_array.unshift(element);
+  return new_array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var new_array = array.slice();
+  new_array.push(element);
+  return new_array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
+
+function accessElementInArray() {
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+  
+}
+
+function removeElementFromEndOfArray(array) {
+
+}
