758debf2bc555e0fd69274517fd2b18f47d089f9
Robert Ciaccio <robjciaccio@gmail.com>
2017-03-30 17:42:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,52 @@
+
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element){
+  const secondArray = [element, ...array]
+  return secondArray
+
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  const secondArray = [...array, element]
+  return secondArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array = array.slice(0, array.length -1)
+  return array
+}
