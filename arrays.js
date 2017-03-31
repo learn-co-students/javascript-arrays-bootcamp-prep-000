4d6bf4bed950755b46d49156d06e444b1c106bfc
Roman Mulladzhanov <romanmullady@gmail.com>
2017-03-31 01:26:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+function addElementToBeginningOfArray(array,element){
+  return  [element,...array]
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array,element){
+  return [...array,element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push(element)
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
