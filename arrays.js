baaeb260d8c20f144e769e602de609a56c63bc98
David Ivory <davidmivory@gmail.com>
2017-04-01 23:55:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  //unshift adds to beginning of array
+  array.push(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
+
+function addElementToEndOfArray(array, element) {
+  //push allows adding to end of array
+  array.
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+
+}
+
+function accessElementInArray(array) {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+
+}
+
+function removeElementFromEndOfArray(array, element){
+
+}
