99381d8dc4543bf5957b3e4238caa6f971bf0a9d
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-06 18:39:17 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element){
+  var array = [];
+  return array.unshift(array, element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+
+function AddElementToEndOfArray(array, element){
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+
+}
