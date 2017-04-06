12adb052a817010153d7bec58bc5af3c7853abf5
Ellisa Shim <ellishim@gmail.com>
2017-04-06 13:43:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function addElementToBeginningOfArray(array, element) {
+  var oldArray = new Array(`${array}`);
+  return [`${element}`, ...oldArray];
+};
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var OGarray = new Array(`${array}`);
+  OGarray.unshift(`${element}`)
+  return OGarray
+}
