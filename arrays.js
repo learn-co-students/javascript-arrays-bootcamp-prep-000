9fde68669605f7d92551925d04d4f4559fc43898
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-06 19:05:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -22,3 +22,26 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array.push(element)
   return array
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.legnth - 1)
+}
