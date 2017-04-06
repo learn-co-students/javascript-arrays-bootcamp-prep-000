3f5f911d1c2ba00ab873c20613efa167df572c29
Ellisa Shim <ellishim@gmail.com>
2017-04-06 14:48:44 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,12 +6,41 @@ var chocolateBars = [
 ];
 
 function addElementToBeginningOfArray(array, element) {
-  var oldArray = `${array}`;
-  return [`${element}`, ...oldArray];
+  return [element, ...array];
 };
 
-/* function destructivelyAddElementToBeginningOfArray(array, element) {
-  var OGarray = new Array(`${array}`);
-  OGarray.unshift(`${element}`)
-  return OGarray
-} */
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+};
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+};
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+};
+
+function accessElementInArray(array, index) {
+  return array[index]
+};
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+};
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+};
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+};
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+};
