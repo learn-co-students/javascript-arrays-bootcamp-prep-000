362e392ea6a438be99d8cb6ead03289b6803376a
Leigh-Ayna Passamano <leighayna.josele@gmail.com>
2017-04-07 19:44:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray2 = [...array, element]
+  return newArray2
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArray3 = array.slice(1)
+  return newArray3
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray4 = array.slice(0, array.length-1)
+  return newArray4
+}
