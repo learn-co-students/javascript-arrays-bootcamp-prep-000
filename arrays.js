6fd491ae72cc2f624150e6244bc91056867a4ba4
Y J <yjachmann@gmail.com>
2017-04-09 19:34:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -21,3 +21,21 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1, array.length)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
