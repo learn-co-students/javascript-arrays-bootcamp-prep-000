951acd65fb2f527f65c839dfaa5c04817f4efd79
Mary Obray <maryobray@gmail.com>
2017-04-09 00:34:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray2 = [...array, element]
+  return newArray2
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return (array[index])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array = array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0, array.length - 1)
+  return newArray
+}
