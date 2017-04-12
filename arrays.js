e3dbf7c07b644b6e382e31beb21c8645ee7a0970
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-12 06:12:28 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,24 +1,26 @@
 //define an array
-var chocolateBars = [snickers, hundred grand, kitkat , skittles];
+var chocolateBars = ["snickers" , "hundred grand" , "kitkat" , "skittles"];
 
 //add element to the beginning of the array but not mutates it
 function addElementToBeginningOfArray(array,element) {
-          return [...element , array];
+          return [element , ...array];
 }
 
 //add element to the beginning of the array and mutates the original array
 function destructivelyAddElementToBeginningOfArray(array,element) {
-          return array.unshift(element);
+            array.unshift(element);
+          return array;
 }
 
 //add element to the end of the array but not mutates it
 function addElementToEndOfArray(array,element) {
-          return [array , ...element];
+          return [...array , element];
 }
 
 //add element to the end of the array and mutates the original array
 function destructivelyAddElementToEndOfArray(array,element) {
-          return array.push(element);
+            array.push(element);
+          return array;
 }
 
 //returns the element in the index place
@@ -28,7 +30,8 @@ function accessElementInArray(array,index) {
 
 //remove the first element in the array and mutates the original array
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-          return array.shift();
+            array.shift();
+          return array;
 }
 
 //remove the first element in the array but not mutates the original array
@@ -38,12 +41,11 @@ function removeElementFromBeginningOfArray(array) {
 
 //remove the last element in the array and mutates the original array
 function destructivelyRemoveElementFromEndOfArray(array) {
-          return array.pop();
+            array.pop();
+          return array;
 }
 
 //remove the last element in the array but not mutates the original array
 function removeElementFromEndOfArray(array) {
-          return array.slice(array.length-1);
+          return array.slice(0,array.length - 1);
 }
-
-
