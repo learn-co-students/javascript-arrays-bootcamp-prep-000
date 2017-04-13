ce62607a2c625aca040da5c3550edf82884676ac
Danny Vega <dannycvega@gmail.com>
2017-04-13 21:04:25 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
+
+function addElementToBeginningOfArray(array,newElement){
+        newArray = [newElement,...array]
+        return newArray
+
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array,newElement){
+            array.unshift(newElement)
+            return array
+}
+
+function addElementToEndOfArray(array, newElement){
+  return [...array, newElement]
+
+}
+
+function destructivelyAddElementToEndOfArray(array,newElement){
+  array.push(newElement)
+  return array
+
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array){
+  newArray =array.slice(1)
+  return newArray
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+return array
+
+}
+
+function removeElementFromEndOfArray(array){
+      newArray = array.slice(0, array.length - 1)
+  return newArray
+}
