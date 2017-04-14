9b574daad5a7a5fc05ed00b8466f211841c2a737
Matthew Van Dyke <matthewlvandyke@gmail.com>
2017-04-14 16:00:19 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function addElementToBeginningOfArray(array, el) {
+  return [el, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  arr.unshift(el);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, el) {
+  return [...arr, el];
+}
+
+function destructivelyAddElementToEndOfArray(arr, el) {
+  arr.push(el);
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+  return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length - 1);
+}
