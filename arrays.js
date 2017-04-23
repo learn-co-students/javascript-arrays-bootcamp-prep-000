ad6230f458a5599334dc61f15c2c648d8c7a68e6
Cristal Fernett <cristal@cristalfernett.com>
2017-04-23 08:08:11 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
+chocolateBars()
+
+function addElementToBeginningOfArray(array, foo) {
+  newArray = [foo , ...array]
+  return (newArray)
+}
+// function addElementToBeginningOfArray(array, foo) {
+//   var array = [1]
+//   array.unshift('foo')
+// }
+// addElementToBeginningOfArray()
+
+// function destructivelyAddElementToBeginningOfArray('foo', 1) {
+//
+// }
+//
+// function addElementToEndOfArray('foo', 1) {
+//
+// }
+//
+// function destructivelyAddElementToEndOfArray('foo', 1) {
+//
+// }
