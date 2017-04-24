eefa17e4d697279fd4c09dfa45192fdec4a328c3
Katie Larson <katmae83@gmail.com>
2017-04-24 03:04:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,3 +7,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {[array.unshi
 function addElementToEndOfArray(array, element) {return [...array, element];}
 function destructivelyAddElementToEndOfArray(array, element) {array.push(element);
 return array;}
+function accessElementInArray(array, index) {return (array[index])};
+function destructivelyRemoveElementFromBeginningOfArray(array) {array.shift(); return array};
+function removeElementFromBeginningOfArray(array) {return array.slice(1)}
+function destructivelyRemoveElementFromEndOfArray(array) {array.pop(); return array};
+function removeElementFromEndOfArray(array) {return array.slice(0,
+array.length - 1)};
