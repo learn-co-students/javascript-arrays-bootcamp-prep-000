4b49a4095755057e1845d17ee5940ecca8ad7093
Marlon DuPain <marlonsaddress@gmail.com>
2017-04-27 03:43:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -35,7 +35,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop()
   return array;
 }
-
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1);
 }
