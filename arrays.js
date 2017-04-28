6a0200143fa97c072529e75ccc2cf6bae5a858fc
Tyler Dejesus <tylerdejesus76@yahoo.com>
2017-04-28 17:58:09 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+array.unshift(element);
+
+
+
+return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
