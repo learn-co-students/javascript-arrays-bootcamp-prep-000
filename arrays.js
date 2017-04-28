5ea675287ee8b11ed74a97b4ab4223aa43bd81f7
Tyler Dejesus <tylerdejesus76@yahoo.com>
2017-04-28 19:31:00 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,11 +3,44 @@ var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
 array.unshift(element);
 
-
-
 return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(){
-  
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+
+  return array;
 }
+
+function addElementToEndOfArray(array, element){
+  array.push(element);
+
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+
+  return array;
+}
+
+function accessElementInArray(array, element){
+  return array[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice(1,1);
+
+  return array;
+}
+
+
+
+
+
