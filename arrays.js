a78b2c9fe49fc6a26335d3006c423676fcb89306
Alexander Lem <lem.alexander@gmail.com>
2017-05-03 16:12:34 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = [
+  "snickers",
+  "hundered grand",
+  "kitkat",
+  "skittles",
+]
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
