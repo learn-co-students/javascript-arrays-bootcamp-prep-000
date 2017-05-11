9ee4a8e1ed10c611971e4f13efeb244d55a7eb8f
tzvi seliger <tzviseliger@gmail.com>
2017-05-11 21:41:04 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,61 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element)
+{
+  [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element)
+{
+  [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+  return array
+}
+
+
+function removeElementFromEndOfArray(array)
+{
+  array.slice(-1)
+  return array
+}
+
+
+/*
+**BONUS**
+We can use `slice`, combined with the spread operator, 
+to make removing from the middle of an array much easier.*/
