7249e2bdcd2f8602d110e8a6efd7d0b37d57e8a3
Jonathan Ling <electric.pen@gmail.com>
2017-05-13 20:31:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray (array, element) {
@@ -13,7 +14,8 @@ function addElementToEndOfArray (array, element) {
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray (array, index) {
@@ -21,7 +23,8 @@ function accessElementInArray (array, index) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array) {
-  return array.shift()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray (array) {
@@ -29,7 +32,8 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
-  return array.pop()
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray (array) {
