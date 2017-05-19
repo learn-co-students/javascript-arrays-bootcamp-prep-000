9bf554f973557c18814f2c8a363ed81003ea18e2
Ryan Barley <rmbarley@gmail.com>
2017-05-19 02:43:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArr = [];
+  newArr = [element, ...array];
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArr = [];
+  array.push(element);
+  newArr = array;
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var newArr = array;
+  newArr.shift();
+  return newArr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArr = array;
+  newArr.pop();
+  return newArr
+}
