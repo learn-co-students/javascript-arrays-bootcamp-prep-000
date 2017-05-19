fa76c83eaea078f322fc94b6f28d4c207d6b3c08
Ian Rowe <ian.rowe.npt@live.com>
2017-05-19 04:39:11 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = [ snickers, hundred grand, kitkats, skittles]
+function addElementToBeginningOfArray(array,element){return
+(array = ["hks","isd"])
+["ajd", ...array]}
+function destructivelyAddElementToBeginningOfArray(array, element)
+{return
+  array.unshift("(element)")
+}
