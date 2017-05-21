4003a4c452057cb047844312323fe0491f8ad02f
Tacara Solomon <alleyesoncara@gmail.com>
2017-05-21 22:33:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,56 +1,43 @@
 
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
-function addElementToBeginningOfArray() {
-  var array =  ["foo"];
-  array.push(1);
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  var array = ["foo"];
-  array.push(1);
-  return array;
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
 
-function addElementToEndOfArray() {
-  var array = [1, 'foo'];
-  [...array, 2];
-  return array;
-}
-
-function destructivelyAddElementToEndOfArray() {
-  var array = [1];
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push('foo');
   return array;
 }
 
-function accessElementInArray() {
-  var array = [1, 2, 3];
-  return array[2];
+function accessElementInArray(array, index) {
+  return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-var array = [1, 2, 3];
+function destructivelyRemoveElementFromBeginningOfArray(array) {
 array.shift();
 return array;
 }
 
-function removeElementFromBeginningOfArray() {
-  var array = [1, 2, 3];
-  array.slice(1);
-  array = array.slice(1);
-  return array;
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+
 }
 
-function destructivelyRemoveElementFromEndOfArray() {
-  var array = [1, 2, 3];
+function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
 
-function removeElementFromEndOfArray() {
-  var array = [1, 2, 3];
-  array.slice(0, array.length - 2);
-  return array;
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
