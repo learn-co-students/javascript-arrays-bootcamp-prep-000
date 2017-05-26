a8fd9543b3fe9e6dec91a2889decfef69c4922a3
elle lee <eelzil77@gmail.com>
2017-05-26 00:24:59 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,15 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  return[element, ...array]
+  return[element,...array]
 }
-
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
-}
-function addElementToEndOfArray(array, element){
-  return[...array, element]
 }
 function addElementToEndOfArray(array, element){
   array.push(element)
@@ -28,12 +24,13 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
+
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
 function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length-1)
+  return array.slice(0, array.length - 1)
 
 }
