0ad7e7b5b6db03f0bd932f43efc7da6c8065fae7
Jarel Fryer <jcf0023@auburn.edu>
2017-05-27 01:14:04 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers","hundred grand", "kitkat","skittles"];
+
+function addElementToBeginningOfArray(arr,element) {
+  return [element, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,element) {
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr,element) {
+  return [...arr, element]
+}
+
+function destructivelyAddElementToEndOfArray(arr,element) {
+  arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr,index) {
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  arr = arr.slice(1);
+  return arr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  arr = arr.slice(0, arr.length -1);
+  return arr;
+}
