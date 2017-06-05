6b6220c70286f23e043b3719c54d6f1583a9fc6e
Brianna Ragsdale <mail@brianna.cc>
2017-06-05 00:30:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array,element) {
+  return [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index) {
+  if (array.isArray || index >= 0 || index < array.length) {
+    return array[index]
+  }
+}
+
+
+function removeElementFromBeginningOfArray(array,element) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array,element) {
+  array.shift()
+  return array;
+}
+
+function removeElementFromEndOfArray(array,element) {
+  return array.slice(0, array.length - 1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array,element) {
+  array.pop(element)
+  return array
+}
