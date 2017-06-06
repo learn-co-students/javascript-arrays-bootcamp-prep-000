6ea6513566d92fe1a81798a7ac1a31a42adaa3e1
Roslyn McKenna <roslynayers@gmail.com>
2017-06-06 19:53:44 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,38 +1,41 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-  return [array, ...element]
+  return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-	return array.shift()
-}
-
-function removeElementFromBeginningOfArray(array, element) {
-	return array.slice(1)
-}
-
-function destructivelyRemoveElementFromEndOfArray(array, element) {
-	array = array.pop();
-	return array
-}
-
-function removeElementFromEndOfArray(array, element) {
-	return array.pop()
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element);
+  return array
 }
 
 function accessElementInArray(array, index) {
-	return array[2]
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
