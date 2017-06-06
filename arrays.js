a2d153cf737b2bebbdb18f0e8b8315923e46d4be
Poke Sam <pokesam10031@yahoo.com>
2017-06-06 02:15:25 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+function addElementToBeginningOfArray(array, element){
+  var a = [array]
+  return a.unshift(element)
+  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+}
