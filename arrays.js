350b12183767c69dd3605b050db7ddc79e3047b0
Sam Crowder <samecrowder@gmail.com>
2017-06-06 19:02:38 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array;
+  newArray.unshift(element);
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = array;
+  newArray.push(element);
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array;
+  newArray.shift();
+  return newArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array;
+  newArray.pop();
+  return newArray;
+}
