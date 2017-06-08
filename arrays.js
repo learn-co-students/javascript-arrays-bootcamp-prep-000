4668ff9b146f4c6f58014bc579b9ff8092ff4986
Guesly  Germain <germain1211@outlook.com>
2017-06-08 16:03:38 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+   array.unshift(element)
+   return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){  
+  return  array.slice(1)
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+      array.pop()
+      return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
