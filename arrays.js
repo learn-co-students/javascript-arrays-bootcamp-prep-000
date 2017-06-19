58a2004101af84135482a0bcc52b3e81a655e5ea
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-06-19 22:42:47 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray () {
+  var add = ["B","C","D"]
+  return "A",...add
+};
+
+function addElementToBeginningOfArray () {
+  var add = ["B","C","D"]
+  var add1 = ["A",...add]
+  return add1
+};
