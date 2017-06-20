c8e11cf00a009ae94d535d102d2ea810a21a066b
Jonathan Magolnick <jonathanmagolnick@gmail.com>
2017-06-20 20:03:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,49 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToBeginningOfArray(array,element){
+  var newarray = array
+  newarray.unshift(element)
+  return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newarray = array
+  newarray.push(element)
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newarray = array
+  newarray.shift()
+  return newarray
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  var newarray = array
+  newarray.pop()
+  return newarray
+}
