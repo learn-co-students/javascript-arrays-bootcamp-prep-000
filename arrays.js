a8decd15a109089a4957f2453223bea1b8ada722
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-06-20 00:23:01 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,12 +1,46 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray () {
-  var add = ["B","C","D"]
-  return "A",...add
+array = ["B","C","D"]
+element = "A";
+Index = "1"
+
+function addElementToBeginningOfArray (array,element) {
+return [element, ...array]
 };
 
-function addElementToBeginningOfArray () {
-  var add = ["B","C","D"]
-  var add1 = ["A",...add]
-  return add1
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+  return array
 };
+
+function addElementToEndOfArray (array, element) {
+return [ ...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+};
+
+function accessElementInArray (array, index) {
+  return array [index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift ()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice (1)
+
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop ()
+  return array
+}
+
+function removeElementFromEndOfArray (array) {
+  return array.slice (0,2)
+}
