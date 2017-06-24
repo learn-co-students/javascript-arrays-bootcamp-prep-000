5924824bf396a0acbc70c5e660e6c2b25a09ca9b
Nick Pourmoradi <npourmoradi@gmail.com>
2017-06-24 07:41:10 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, word){
+  return [word, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, word){
+  array.unshift(word);
+  return array;
+}
+
+function addElementToEndOfArray(array, word){
+  return [...array, word]
+}
+
+function destructivelyAddElementToEndOfArray(array, word){
+  array.push(word);
+  return array;
+}
+
+function accessElementInArray(array, numba){
+  return array[numba];
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+ function destructivelyRemoveElementFromBeginningOfArray(array){
+   array.shift();
+   return array;
+ }
+
+ function removeElementFromEndOfArray(array){
+   return array.slice(0, array.length - 1);
+ }
+
+ function destructivelyRemoveElementFromEndOfArray(array){
+   array.pop();
+   return array;
+ }
