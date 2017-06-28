0e7b5ec356ec9c9f919abcc9d3f99142c680ecf5
Tyler MacDonald <tmacdonald1@gmail.com>
2017-06-28 04:50:19 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,60 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+
+
+function chocolateBars() {
+  return chocolateBars;
+}
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+  array.unshift(element);
+
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+
+  array.push(element);
+
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  //console.log(array[4]);
+
+  return array[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1);
+}
+//addElementToBeginningOfArray(array, element);
