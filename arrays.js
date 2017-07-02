d7d14be8e7978f47d2ef95e8bcb86e2a2b9d92fc
Jasmine Mixson <jmixson1179@gmail.com>
2017-07-02 19:57:49 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,32 +1,31 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-console.log(chocolateBars);
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element];
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
-  return array[index];
+  return array[index]
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
+  return array.slice(1)
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1);
+  return array.slice(0, array.length -1)
 }
