c00c09a96b5d7aa1fee29ff15067af15836cac3c
Michael Hardy <michaelmhardy@gmail.com>
2017-07-08 13:40:44 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -32,6 +32,35 @@ function accessElementInArray(golfClubs, Club){
 } // passed
 
 function destructivelyRemoveElementFromBeginningOfArray(golfClubs){
-  golfClubs.unshift()
+  golfClubs.shift()
   return golfClubs
+} // passed
+
+function removeElementFromBeginningOfArray(golfClubs){
+  return golfClubs.slice(1)
+} // passed --- see below...
+
+// START
+// in the Console
+
+// var golfClubs = ["Driver", "Putter", "Wedge"]
+
+// function removeElementFromBeginningOfArray(golfClubs){
+  // return golfClubs.slice(1)
+// }
+
+// golfClubs
+// (3) ["Driver", "Putter", "Wedge"]
+// golfClubs.slice(1)
+// (2) ["Putter", "Wedge"]
+
+// END
+
+function destructivelyRemoveElementFromEndOfArray(golfClubs){
+  golfClubs.pop()
+  return golfClubs
+} // passed
+
+function removeElementFromEndOfArray(golfClubs){
+  return golfClubs.slice(0, golfClubs.length - 1)
 }
