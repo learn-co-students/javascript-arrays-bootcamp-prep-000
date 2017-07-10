38d5f0829edc67280ff1f48f55c19729b2777139
Eric Lau <cireual510@gmail.com>
2017-07-10 21:57:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,48 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+/*function addElementToBeginningOfArray(array, element) {
+  var newarray = array;
+  newarray.unshift(element);
+  return newarray
+}
+*/
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
 }
