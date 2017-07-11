3340ff8970332a681e842609f528f28047990a5b
Dorothy  Eves <dorothyeves7@gmail.com>
2017-07-11 23:45:15 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 
 
 function removeElementFromBeginningOfArray(array){
-   array.slice(1);
+  array = array.slice(1);
    return array;
 }
 /*
