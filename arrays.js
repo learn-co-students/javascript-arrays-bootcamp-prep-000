23275d258653e2727c95e2510a1f3823a382ac08
Dorothy  Eves <dorothyeves7@gmail.com>
2017-07-11 22:33:11 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -16,7 +16,19 @@ function addElementToEndOfArray(array, element){
   return [...array, element];
 }
 
-
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
+}
+
+function accessElementInArray(array,index){
+ return  array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
 }
