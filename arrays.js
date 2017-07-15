ace211d9a4dc55d4e4e8a215413ee4e74d09d8d2
Bissan AbuSharar <bissansara@gmail.com>
2017-07-15 04:45:22 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = addElementToBeginningOfArray(array, element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var array = destructivelyAddElementToEndOfArray(array, element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  var array = array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  var array = array.slice(0, array.length - 1)
+  return array
+}
