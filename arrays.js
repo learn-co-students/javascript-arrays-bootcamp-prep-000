f07e68aaac5ac7b4eb3db95b4480402d50c3b4dd
zarah s <leechte@gmail.com>
2017-07-15 05:13:53 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+function chocolateBars();{
+
+function addElementToBeginningOfArray (){
+  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]; }
+  var bio = [" was one of the founding dancers of vogue dramatics";]
+console.log(chocolateBars[3] + bio)
+}
