545765b38a43e51bdbffaf0b7737205f90d86e06
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-20 16:47:59 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray([1],"foo") {
-  return "['foo',1]"
-}
+var addElementToBeginningOfArray = [1]
