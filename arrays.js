3249d9ae8fe20980786a90474bc95ff5ea1f7281
Kanav Kathuria <kkathuria93@gmail.com>
2017-07-21 04:19:50 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray() {
+  var addElementToBeginningOfArray = [1]
+  var addElementToBeginningOfArray = ['foo', ...addElementToBeginningOfArray]
+    return addElementToBeginningOfArray
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var destructivelyAddElementToBeginningOfArray = [1]
+    destructivelyAddElementToBeginningOfArray.unshift("foo")
+    return destructivelyAddElementToBeginningOfArray
+}
+
+function addElementToEndOfArray() {
+  var addElementToEndOfArray = [1]
+  var addElementToEndOfArray = [...addElementToEndOfArray, 'foo']
+    return addElementToEndOfArray
+}
+
+function destructivelyAddElementToEndOfArray() {
+  var destructivelyAddElementToEndOfArray = [1]
+    destructivelyAddElementToEndOfArray.push("foo")
+    return destructivelyAddElementToEndOfArray
+}
+
+function accessElementInArray() {
+  var accessElementInArray = [1, 2, 3]
+  console.log(accessElementInArray[2])
+}
