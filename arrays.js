5d88fefd760b6e0319cf2cdadbc535c249be1c6b
Donivan Gray <gray.donivan@yahoo.com>
2017-07-21 02:30:56 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,26 +1,39 @@
-var chocolateBars = ['snickers','hundred grand',  'kitkat', 'skittles',]
+var chocolateBars = ['snickers','hundred grand',  'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-(array,element).unshift()
+  array.unshift(element)
   return array
 }
 
-function destructivelyAddElementToEndOfArray(array){
-  array.unshift()
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
   return array
 }
 
 function addElementToEndOfArray(array,element){
   return [...array, element]
 }
-  function accessElementInArray(){
-    return (chocolateBars,3)
-  }
+
   function destructivelyRemoveElementFromBeginningOfArray(array) {
     array.shift()
     return array
   }
+  function accessElementInArray(array,index){
+        return array[index]
+}
+  function removeElementFromBeginningOfArray (array){
+        array=array.slice(1)
+      return array.slice()
+      }
+   function destructivelyRemoveElementFromEndOfArray(array) {
+         array.pop()
+         return array
+       }
+    function removeElementFromEndOfArray (array) {
+    array=array.slice(0, array.length - 1)
+      return array
+    }
