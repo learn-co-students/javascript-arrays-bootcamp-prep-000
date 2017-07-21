ff1a6141d3f443abfc9f1fcfb785271ae29fb7d1
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-21 02:04:52 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -8,3 +8,32 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
     return array
 }
+
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+    return array
+}
+function accessElementInArray(array, index) {
+  console.log(array[index])
+    return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift(array)
+    return array
+}
+function removeElementFromBeginningOfArray(array) {
+  array.shift(array)
+    return array
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop(array)
+    return array
+}
+function removeElementFromEndOfArray(array) {
+  array.pop(array)
+    return array
+}
