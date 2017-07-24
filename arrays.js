c5263a8a13df31ea2a729920f00d2ed2f26a2faa
beardshadow <beardshadow@gmail.com>
2017-07-24 22:15:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
@@ -11,8 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
@@ -30,8 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
@@ -40,6 +37,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.pop();
-  return array;
+  return array.slice(0, array.length -1);
 }
