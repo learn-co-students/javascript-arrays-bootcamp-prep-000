de8d43d97c80ebc0a6c2747e403d4b25275a6034
Katy Eng <katye610@gmail.com>
2017-07-24 14:02:08 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray (array, element) {
+  const array = [1]
+  return [`element`, ...array]
+}
