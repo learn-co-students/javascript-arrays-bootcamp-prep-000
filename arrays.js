3f5ae427e99b56aa4d3a7e391ba3a5770f3cf7e7
Brian Feibus <brianfeibus@gmail.com>
2017-07-27 01:53:27 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = new Array('snickers','hundred grand','kitkat', 'skittles')
+
+function addElementToBeginningOfArray(array, element) {
+  return chocolateBars.unshift = [ 'foo', 1 ]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return chocolateBars.unshift = [ 'foo', 1 ]
+}
+
+function addElementToEndOfArray(array, element) {
+  return chocolateBars.push = [ 1, 'foo' ]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  return chocolateBars.push = [1, 'foo']
+}
+function accessElementInArray(array, index) {
+  return chocolateBars.element = 3
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return chocolateBars.shift ([2, 3])
+}
+function removeElementFromBeginningOfArray(array) {
+  return chocolateBars.slice = [2, 3]
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+return chocolateBars.pop ([2, 3])
+}
+
+function removeElementFromEndOfArray(array) {
+  return chocolateBars.slice = [1, 2]
+}
