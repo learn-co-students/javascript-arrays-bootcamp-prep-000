36c2cd6d076346676b9d607fb7015df6e1505a49
Alexander Foster <afoster009@hotmail.ca>
2017-08-02 21:29:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,21 +1,34 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var newArray = [...array, 'kittens', 'dogs', 'cats' ];
-
-  return newArray;
-
+return [element, ...array];
 
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-
+  return array.unshift(element);
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-
+function addElementToEndOfArray(array, element){
+  return [array, ...element];
 }
 
-function addElementToBeginningOfArray(array, element){
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}
 
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop();
 }
