70eba6317888e01c727bb01e412667055ea39bb4
Alexander Foster <afoster009@hotmail.ca>
2017-08-02 21:21:21 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [...array, 'kittens', 'dogs', 'cats' ];
+
+  return newArray;
+
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+
+function addElementToBeginningOfArray(array, element){
+
+}
