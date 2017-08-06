900277b5dbff3b4e165a36966cb59d8351bb3531
omikolaj <oscar222@sbcglobal.net>
2017-08-06 21:25:20 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(myArray, element){
+  var myArrayTwo = [element, ...myArray]
+  return myArrayTwo
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, element){
+  myArray.unshift(element)
+  return myArray
+}
+function addElementToEndOfArray(myArray, element){
+  var myArrayTwo = [...myArray, element]
+  return myArrayTwo
+}
+
+function destructivelyAddElementToEndOfArray(myArray, element){
+  myArray.push(element)
+  return myArray
+}
+
+function accessElementInArray(myArray, index){
+  return myArray[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(myArray){
+  myArray.shift()
+  return myArray
+}
+
+function removeElementFromBeginningOfArray(myArray){
+  myArray = myArray.slice(1)
+  return myArray
+}
+function destructivelyRemoveElementFromEndOfArray(myArray){
+  myArray.pop()
+  return myArray
+}
+
+function removeElementFromEndOfArray(myArray){
+  myArray.pop()
+  return myArray
+}
