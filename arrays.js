5228156ca885e315c3cc4a3cf828bcaf262c25d6
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-11 16:45:35 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array;
+  var newArray = [element, ...newArray];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = array;
+  var newArray = [...newArray, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+
+function removeElementFromBeginningOfArray(array){
+  var array2 = array.slice(1)
+  return array2
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var array2 = array.slice(0, array.length - 1)
+  return array2
+}
