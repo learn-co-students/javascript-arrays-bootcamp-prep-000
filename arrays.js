cee12d9330dd46158d7154b422c587da65b91e0b
Chloe Mandel <chloemandel2@gmail.com>
2017-08-12 02:11:34 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  const array = [1]
+  const newArray = ['foo', ...array]
+  return newrray
+}
