f1dd7954a1ca8dfd6bdc007caa40898f35b8c70a
aheald25 <andrewheald25@gmail.com>
2017-08-15 00:16:18 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -25,3 +25,11 @@ function removeElementFromBeginningOfArray(array) {
   array = array.slice(1)
   return array
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  array = array.slice(0, array.length - 1)
+  return array
+}
