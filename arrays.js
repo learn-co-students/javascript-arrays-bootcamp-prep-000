568d043d44184c0012c33a446c07098b8292eb5f
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-08-16 02:11:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -29,3 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array, element){
   array.shift(element)
   return array
 }
+
+function removeElementFromBeginningOfArray (array){
+  return array.slice(1)
+}
