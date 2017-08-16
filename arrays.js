68a11f47ff514db01a3fb5149c588a52c9ff766c
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-08-16 01:13:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -24,3 +24,8 @@ function destructivelyAddElementToBeginningOfArray (array, element){
 function accessElementInArray (array, index){
 return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array, element){
+  array.shift(element)
+  return array
+}
