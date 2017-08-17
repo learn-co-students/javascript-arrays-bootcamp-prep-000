118dd95258b934e5b06568c194b701cfd943c2a9
Adrienne Yao <adrienne.yao@gmail.com>
2017-08-17 17:40:10 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ["snickers","hundred grand","kikkat","skittles"]
+
+// function addElementToBeginningOfArray( parameter1, paramenter2 ) {
+//   var newArray = [parameter2, ...parameter1];
+//   return newArray;
+// }
+//
+// function destructivelyAddElementToBeginningOfArray( parameter1, parameter2 ) {
+//   parameter1.unshift(parameter2);
+//   return parameter1;
+// }
+
+// function addElementToEndOfArray(parameter1, parameter2) {
+//   var newArray = [...parameter1, parameter2];
+//   return newArray;
+// }
+
+
+// function destructivelyAddElementToEndOfArray( parameter1, parameter2 ) {
+//   parameter1.push(parameter2);
+//   return  parameter1;
+// }
+
+// //array indexes are actually strings
+// function acessElemtnInArray(myArray,i) {
+//   return myArray[i];
+// }
+//
+// console.log(Object.keys([1,2,3]))
+//
+// function destructivelyRemoveElementFromBeginningOfArray (myArray) {
+//   myArray.shift();
+//   return myArray;
+// }
+//
+// function removeElementFromBeginningOfArray(myArray) {
+//   return myArray.slice(1);
+// }
+//
+// function destructivelyRemoveElementFromEndOfArray(myArray) {
+//   return myArray.pop();
+// }
+//
+// function removeElementFromEndOfArray(parameter1) {
+//   new myArray = parameter1.slice(0, parameter1.length - 1);
+//   return myArray;
+// }
