cff84df5c93123a44229e9772ca8a1590f113f32
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-08-17 00:28:01 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function destructivelyRemoveElementFromEndOfArray (array){
 array.pop(1)
 return array
 }
+
+function removeElementFromEndOfArray (array){
+array.slice(0, array.length-1)
+return array.slice(0, array.length-1)
+}
