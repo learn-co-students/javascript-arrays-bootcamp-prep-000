7f0e7f30bf9555e923c8f31e606ca807a2f3ef31
Ezekiel House <zekeman250@gmail.com>
2017-08-21 17:16:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
