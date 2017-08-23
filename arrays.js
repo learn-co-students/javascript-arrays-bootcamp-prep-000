baa83db5f3a497de2e5ddb5e58cd99e470aa0bfa
Paul Blake <paulblake_03@hotmail.com>
2017-08-23 04:38:18 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(dog, cat){
+  return [cat, ...dog]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  array.shift(0)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
+}
