66eca31159232e681c46591d2bb8e1973c20b8d4
doubleohjohn <d.john.oh@gmail.com>
2017-08-24 18:16:58 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(element, array){
+}
+
+function destructivelyAddElementToBeginningOfArray(element, array){
+  ${array}.unshift(${element})
+}
+
+function addElementToEndOfArray(element, array){}
+
+function destructivelyAddElementToEndOfArray(element, array){}
