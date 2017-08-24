0a026816677d892f5bf8ead2f8de83dd516569a7
Will Chen <will42874aj@gmail.com>
2017-08-24 18:53:47 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
