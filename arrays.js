5c8e3031026580156d5497e35036be018e17c011
Barbara Choy <barbchoy@gmail.com>
2017-08-25 15:35:39 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,56 @@
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array,element){
+
+  return [element, ...array];
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+
+  array.unshift(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementToBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function addElementToEndOfArray(array,element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
+  array.shift();
+  return array;
+}
+
+//function destructivelyRemoveElementToBeginningOfArray(array,element){
+//  array.pop();
+//  return array;
+//}
+
+function removeElementFromBeginningOfArray(array){
+  array=array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
