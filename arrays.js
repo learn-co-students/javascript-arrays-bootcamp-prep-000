a794c5232384efa3bb255e1473625c2d6b9a30ca
Barbara Choy <barbchoy@gmail.com>
2017-08-25 15:41:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -52,5 +52,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
 
-  return array.slice(array.length-1);
+  return array.slice(0,array.length-1);
 }
