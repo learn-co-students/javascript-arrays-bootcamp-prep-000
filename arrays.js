c0193c1021c625bdf6f802f7f2bc477b11105503
doubleohjohn <d.john.oh@gmail.com>
2017-08-26 18:46:08 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,12 +1,44 @@
-var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(element, array){
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(element, array){
-  ${array}.unshift(${element})
+function destructivelyAddElementToBeginningOfArray(array, element){
+ array.unshift(element);
+ return array
+ }
+
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
 }
 
-function addElementToEndOfArray(element, array){}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
 
-function destructivelyAddElementToEndOfArray(element, array){}
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1);
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array = array.slice(0, array.length -1);
+  return array
+}
