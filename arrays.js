b1648eb9d0618da8463fdcfaf9e2f3067448e39b
Sandy Yeung <sandynism@gmail.com>
2017-08-30 23:07:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+  console.log(chocolateBars)
+
+  function addElementToBeginningOfArray['()']
+
+  function destructivelyAddElementToBeginningOfArray['()']
