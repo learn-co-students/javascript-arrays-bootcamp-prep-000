3a1f91895887971ca04017472c425595200db37d
Sandy Yeung <sandynism@gmail.com>
2017-08-31 00:57:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,8 +1,41 @@
-
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-  console.log(chocolateBars)
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
 
-  function addElementToBeginningOfArray['()']
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
 
-  function destructivelyAddElementToBeginningOfArray['()']
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray (array, index) {
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
