f7eb22f85d7392454edce83810dc586eeaebfeca
Crista Cooper <aine3d@gmail.com>
2017-09-03 21:03:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,7 +6,39 @@ function addElementToBeginningOfArray(array, beginning) {
   return newarray
 }
 
-function destructivelyAddElementToBeginningOfArray() {
+function destructivelyAddElementToBeginningOfArray(array, beginning) {
   array.unshift(beginning)
   return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  var newarray = array[index]
+  return newarray
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
