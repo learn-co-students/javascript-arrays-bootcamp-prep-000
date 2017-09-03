c19946ca3babea196831e54b0e5667bb853fd8d1
Crista Cooper <aine3d@gmail.com>
2017-09-03 01:00:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, beginning) {
+  var array = [4, 5, 6, 7];
+  array.unshift(3)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
