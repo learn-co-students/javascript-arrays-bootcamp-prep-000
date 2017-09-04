dcfde616e44527fd390cc48e48a177b41c0192fb
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-04 15:51:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,12 +3,39 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var array = [];
 
 function addElementToBeginningOfArray(array, element) {
-  var array = new Array();
-  [element, ...array]
+  array = [element, ...array]
   return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element)
+  return array
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+  array = array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array = array.pop()
   return array
 }
