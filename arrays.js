b9a16a39d37b27ed1696ec87012ca69a394777b6
Nancy Tran <nanstran@gmail.com>
2017-09-05 07:07:39 -0400

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat", "skittles'];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
