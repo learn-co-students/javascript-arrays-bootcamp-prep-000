88fcdd44c1c31a96f0278e07bdb3a978a14976cc
Abdullahi Aliyu <abdoulfurya@gmail.com>
2017-09-14 22:34:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element){
+  chocolateBars.unshift(element);
+  return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
