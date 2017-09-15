3c37fc43c2c3c1dc074d746aba1197da20c711d1
Derek Haller <dhaller.personal@gmail.com>
2017-09-15 20:22:39 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, newElement) {
+  return [newElement, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, newElement) {
+ array.unshift(newElement)
+ return array;
+}
+
+function addElementToEndOfArray(array, newElement) {
+  return [...array, newElement];
+}
+
+function destructivelyAddElementToEndOfArray(array, newElement) {
+  array.push(newElement);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  newArray = array.slice(0, array.length - 1);
+  return newArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
