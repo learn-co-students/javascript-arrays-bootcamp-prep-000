26f51446d0d669b652db10fab75b1ffb72a781df
ciara hudson <cchudson33@gmail.com>
2017-09-16 00:07:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles" ];
+
+function addElementToBeginningOfArray = (array, element) {
+  chocolateBars.unshift ("reeses");
+  return ["reeses" , "snickers" , "hundred grand" , "kitkat" , "skittles"];
+};
