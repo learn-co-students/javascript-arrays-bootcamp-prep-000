65778ed8f69a67f6d1ef4aac8b15cd8ac02814d5
RenataCodes <psych.coaching@gmail.com>
2017-09-18 14:04:13 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -28,6 +28,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
   return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
 }
