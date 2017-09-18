a3de275a69ce4005da2364dac8882849f6ec52f5
Ghalyah Al-Ansari <gaa291@nyu.edu>
2017-09-18 23:47:38 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array,elemnt){
-  return [element,...array]
+function addElementToBeginningOfArray(array,element){
+  return [element,...array];
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(element)
@@ -13,4 +13,22 @@ function addElementToEndOfArray(array,element){
 function destructivelyAddElementToEndOfArray(array,element){
   array.push(element)
   return array
+}
+function accessElementInArray(array,index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
 }
