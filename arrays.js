0dfcaf4eab122311dc2e493a2ea48394cdf4d462
Quinn Dizon <qdizon@gmail.com>
2017-09-18 00:43:40 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(a, element){
+  return [element, ...a];
+}
+
+function destructivelyAddElementToBeginningOfArray(a, element){
+  a.unshift(element);
+  return a;
+}
+
+function addElementToEndOfArray(a, element){
+  return [...a, element];
+}
+
+function destructivelyAddElementToEndOfArray(a, element){
+  a.push(element);
+  return a;
+}
+
+function accessElementInArray(a, index){
+  return a[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a){
+  a.shift();
+  return a;
+}
+
+function removeElementFromBeginningOfArray(a){
+  var sliced = a.slice(1);
+  return sliced;
+}
+
+function destructivelyRemoveElementFromEndOfArray(a){
+  a.pop();
+  return a;
+}
+
+function removeElementFromEndOfArray(a){
+  var sliced = a.slice(0, a.length-1);
+  return sliced;
+}
