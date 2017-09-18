938d9ef9058ccb226b879e47bae6bd4eec4423e0
RenataCodes <psych.coaching@gmail.com>
2017-09-18 13:56:14 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,12 +1,33 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, element) =  {
-  var newArray = array.unshift(element)
-return newArray
+function addElementToBeginningOfArray(array, element) {
+return [element, ...array]
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
 
-function destructivelyAddElementToBeginningOfArray(array, element) = {
-  array = array.unshift(element)
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift([0])
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
   return array
 }
