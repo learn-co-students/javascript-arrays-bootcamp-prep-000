6fa174b2eea7c26bcc3d084fbb4b0f7dd8d9c92e
Jordan Rumeld <jordanrumeld@me.com>
2017-09-19 20:30:19 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,6 +4,22 @@ var chocolateBars = [
   "kitkat",
   "skittles"
 ]
-function addElementToBeginningOfArray(chocolateBars) = {
-  console.log (addElementToBeginningOfArray.push)
+function addElementToBeginningOfArray(chocolateBars, element) {
+  ["element", ...chocolateBars];
+  return addElementToBeginningOfArray
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
+  chocolateBars.unshift(element)
+  return destructivelyAddElementToBeginningOfArray
+}
+function addElementToEndOfArray(chocolateBars, element) {
+  [...chocolateBars, element]
+  return addElementToEndOfArray
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, element) {
+  return chocolateBars.push(element)
+}
+function accessElementInArray(chocolateBars, index) {
+  console.log(chocolateBars[3])
+  return accessElementInArray
 }
