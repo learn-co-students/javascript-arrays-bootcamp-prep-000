f2d5f3dcab1ccc337fd6450b17d51391cd01b0f3
Jordan Rumeld <jordanrumeld@me.com>
2017-09-19 16:19:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+function addElementToBeginningOfArray(chocolateBars) = {
+  console.log (addElementToBeginningOfArray.push)
+}
