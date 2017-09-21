36c28390af21aac845994f392577cb85132a9289
jsedlak65 <james.sedlak@gmail.com>
2017-09-21 04:15:13 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -23,14 +23,13 @@ function accessElementInArray(array, index){
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(1)
-  return array;
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
@@ -39,6 +38,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(0, array.length - 1)
-  return array;
+  return array.slice(0, array.length - 1);
 }
