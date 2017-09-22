ef6f103acf4ee4af44a8f6a9f5d76c4536f43b09
alexis101 <kondilisalexis@gmail.com>
2017-09-22 21:52:21 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,26 +4,31 @@ function addElementToBeginningOfArray(chocolateBars, element){
   return [element, ...chocolateBars ];
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
- return destructivelyAddElementToBeginningOfArray[...chocolateBars, element];
+ chocolateBars.unshift(element);
+ return chocolateBars
 }
 function addElementToEndOfArray(chocolateBars, element){
   return [...chocolateBars, element];
 }
 function destructivelyAddElementToEndOfArray(chocolateBars, element){
   chocolateBars.push(element);
+  return chocolateBars
 }
 
 function accessElementInArray(chocolateBars, index){
   return chocolateBars[2];
 }
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.shift();
+  chocolateBars.shift();
+  return chocolateBars
+
 }
 function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-  return chocolateBars.pop();
+  chocolateBars.pop();
+  return chocolateBars
 }
 function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length -1);
