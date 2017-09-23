da48b3634ce05abbe7aeeca53d27e8347a654ded
TigreLibre <rachelposey4@gmail.com>
2017-09-23 17:03:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+const array = [1]
+
+
+function addElementToBeginningOfArray([1], 'foo'){
+  array.unshift('foo');
+  return array;
+}
+ 
