f6ca3e6d34ac111be704d8df6d973d835bd7d691
apocalypse575 <apocalypse575@gmail.com>
2017-09-26 19:58:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementstoBeginningOfArray(array, element) {
+  [element, ...addElementstoBeginningOfArray]
+  return addElementstoBeginningOfArray(array, element)
+}
+
+function destructivelyAddElementToEndOfArray[]
