576f1e612aeb1241c85b2257f4e848283ca14c00
yash chaudhary <yash240196@gmail.com>
2017-09-30 11:47:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,5 +42,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0,a.length-1)
+  return array.slice(0,array.length-1)
 }
