256804e284332653fc81edb4314d5b4c84ba0406
jonathan Morton <jmorton4193@gmail.com>
2017-09-30 15:39:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars= ['snickers', 'hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray(array[], n){
+array.push('n')
+return array[]
+}
+ function destructivelyAddElementToBeginningOfArray(){}
