21a223a994b59604cb57bc486b31b76599ef7017
yash chaudhary <yash240196@gmail.com>
2017-09-30 11:08:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles' ]
+
+function addElementToBeginningOfArray(a,e) {
+  var a = [1,2]
+  var b = [e,...a]
+  return b
+}
+
+function destructivelyAddElementToBeginningOfArray(a,e) {
+  var a = [1,2]
+  a = [e,...a]
+  return a
+}
+
+function addElementToEndOfArray(a,e) {
+  var a = [1,2]
+  var b = [...a,e]
+  return b
+}
+
+function destructivelyAddElementToEndOfArray(a,e) {
+  var a = [1,2]
+  a = [...a,e]
+  return a
+}
