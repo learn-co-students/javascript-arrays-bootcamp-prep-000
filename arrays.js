4529f6b0361c20d72c5f794db95151ae5aed750d
Sen Tezeno <sen63092@yahoo.com>
2017-10-01 22:00:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array,element){
+
+    return [element,...array]
+};
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+
+    return array.unshift([element,array])
+
+};
+
+function addElementToEndOfArray(array,element){
+
+    return [...array,element]
+};
+
+function destructivelyAddElementToEndOfArray(array,element){
+
+    return array.push(element)
+};
+
+function accessElementInArray(array,index){
+
+    return array[index]
+};
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+
+      return array.shift()
+};
+
+function removeElementFromBeginningOfArray(array){
+
+    return array.slice(1)
+};
+
+function destructivelyRemoveElementFromEndOfArray(array){
+
+    return array.pop(3)
+};
+
+function removeElementFromEndOfArray(array){
+
+    return array.slice(0, array.length - 1)
+};
