5d54191082db4ff977323699788a8e017a6cc2dc
savita madray <savitamadray01@gmail.com>
2017-10-03 00:58:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -23,6 +23,7 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
     array.shift()
+    return array
 }
 
 function removeElementFromBeginningOfArray(array){
