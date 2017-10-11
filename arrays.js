3e6b7213c0b44a4fd158f218745744d444c3bbad
Joanne Basa <joannegbasa@gmail.com>
2017-10-11 19:30:40 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -40,6 +40,3 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length - 1)
 }
-function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
-}
