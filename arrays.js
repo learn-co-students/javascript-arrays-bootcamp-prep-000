0e7426bb9da7334a6eec0e3e78ebfddb85962fb6
Noelle <noelle.legrain@gmail.com>
2017-10-13 10:09:05 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,36 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, element){
+  array = [element, ...array]
+  return array
+};
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  array = [...array, element]
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index) {
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  array = array.slice(0, array.length - 1)
+  return array
+}
