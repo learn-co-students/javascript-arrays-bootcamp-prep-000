02f062136c2d14f19638f5c02763b99e26ccb193
Karen G <kgraves0701@gmail.com>
2017-10-14 13:42:23 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,10 +10,43 @@ The destructive function, destructivelyAddElementToBeginningOfArray, should alte
 addElementToBeginningOfArray, on the other hand, should return a new array and not modify the original.*/
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = array.unshift(element)
+  var newArray = [element, ...array]
   return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray2 = [...array, element]
+  return newArray2
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+    return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+    return array.slice(0,array.length-1)
 }
