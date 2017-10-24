a68982e26260e22abe4b1123bd1f98fe72684199
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-24 11:45:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+const array =[1]
+element = "foo"
+function addElementToBeginningOfArray(array, element) {
+  return (["foo", 1])
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift("foo")
+}
