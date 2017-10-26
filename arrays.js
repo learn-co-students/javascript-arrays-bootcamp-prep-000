871b01a8e46c80a10b2e21642d6c360772eaec01
Julien Tregoat <jtregoatlearn@gmail.com>
2017-10-26 08:07:50 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,5 +10,38 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array = array.unshift(element)
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+  return newArray = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return newArray = array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  newArray = array.slice(0, array.length - 1)
+  return newArray
 }
