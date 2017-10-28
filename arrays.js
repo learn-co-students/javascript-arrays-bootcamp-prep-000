cc1f69b7ed9f3f0dc405af8d5bad7b4510fc3eb1
Jeremy Williams <jeremy.bet.williams@gmail.com>
2017-10-28 17:28:23 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(array,item){
+ var newArray = [item,...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, item){
+  array.unshift(item);
+  return array;
+}
+
+function addElementToEndOfArray(array,item){
+  var newArray = [...array,item];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array,item){
+  array.push(item);
+  return array;
+}
+
+function accessElementInArray(array,index){
+  return(array[index]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return(array);
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArray = array.slice(1);
+  return newArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0,array.length-1)
+  return newArray;
+}
