93b63f70db78e66f091171fa0b8cbbaed09c172f
Lalita Desouza <lmdesou@yahoo.com>
2017-10-28 22:12:20 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,38 +1,49 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var array = [1, 2, 3]
-function addElementToBeginningOfArray(array, element) {
-  return [element,...array];
-    const newArray = [element, ...array]
+//should return a new array
+function addElementToBeginningOfArray(array,item){
+ var newArray = [item,...array];
+  return newArray;
 }
+
+//should modify the original array
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   array.unshift("foo")
-   return array
-}
-
-function addElementToEndOfArray (array, element) {
-  return [...array, element];
-    const newArray = [...array, element]
-}
-function destructivelyAddElementToEndOfArray(array, element) {
-    array.push("foo")
+  array.unshift(element);
   return array
 }
-function accessElementInArray(array, index){
+
+//should return new array
+function addElementToEndOfArray(array, element){
+var newArray = [...array, element]
+  return newArray
+}
+
+// should modify the original array
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
   return array[index];
 }
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-    array.shift()
+//should mutate original array
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
   return array
 }
-function  removeElementFromBeginningOfArray(array) {
-     array.slice(1)
-     return sliceArray = array.slice(1)
+//should make a new array
+function removeElementFromBeginningOfArray(array) {
+var newArray = array.slice(1)
+return newArray
 }
-function destructivelyRemoveElementFromEndOfArray (array) {
-    array.pop()
-  return array
+//changes original array
+function destructivelyRemoveElementFromEndOfArray(array) {
+   array.pop()
+   return array
 }
+//makes a new array
 function removeElementFromEndOfArray (array) {
-  return array.slice(0, -1)
+  var newArray = array.slice(0, -1);
+  return newArray
 }
