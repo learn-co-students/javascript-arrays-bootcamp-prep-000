25930c986079b059f63c85be085cf6f7f1a388ef
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2017-11-03 22:50:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ['snickers', 'hunderd grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, el) {
+  return [el, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  arr.unshift(el);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, el) {
+  return [...arr, el];
+}
+
+function destructivelyAddElementToEndOfArray(arr, el) {
+  arr.push(el);
+  return arr;
+}
+
+function accessElementInArray(arr, idx) {
+  return arr[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length-1)
+}
