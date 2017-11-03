679d3d0314c3193442b101081264ac68b450d6d6
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-11-03 02:48:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,32 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-chocolateBars.push("reeses")
-chocolateBars.unshift("Milkduds")
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
