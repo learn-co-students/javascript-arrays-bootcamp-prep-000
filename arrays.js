d4d3945237255da567d7f040dcf9ff2af82a5187
Jay G <playerjayho@gmail.com>
2017-11-15 20:19:26 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function addElementToEndOfArray (array,element) {
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
 
-    return array.unshift(element)
+    return array.unshift("element")
 }
 
 
