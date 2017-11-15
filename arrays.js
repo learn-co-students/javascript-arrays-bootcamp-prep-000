e08efe2a919911ddc0162ba2eb606c27f4309a75
Jay G <playerjayho@gmail.com>
2017-11-15 20:49:07 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,6 +5,26 @@ var chocolateBars = [
     'skittles'
 ];
 
+function removeElementFromEndOfArray(array) {
+      var fix = array.slice(0, array.length - 1)
+      return fix
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+        array.unshift(element)
+        return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+   array.pop()
+   return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+       array.shift()
+       return array
+}
+
 function addElementToBeginningOfArray (array,element) {
       return [element, ...array]
 }
@@ -14,19 +34,16 @@ function addElementToEndOfArray (array,element) {
       return [...array, element]
 }
 
-
-
-function destructivelyAddElementToBeginningOfArray(array,element) {
-
-    return array.unshift("element")
-}
-
-
-
 function destructivelyAddElementToEndOfArray (array,element) {
-    return array.push(element)
+   array.push(element)
+   return array
 }
 
-function accessElementInArray() {
+function removeElementFromBeginningOfArray(array) {
+      var fix = array.slice(1)
+      return fix
+}
 
+function accessElementInArray(array, n) {
+      return array[n]
 }
