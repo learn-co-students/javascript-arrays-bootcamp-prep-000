37299f0c51dd2ca8578f7f5132c08f14c26f4be8
Cas <lydiahamil@yahoo.com>
2017-11-24 05:43:37 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+///function addElementToBeginningOfArray(array, element) {
+   ///return addElementToBeginningOfArray = ['foo', 1]
+   
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return addElementToEndOfArray.push = [1, 'foo']
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return destructivelyAddElementToBeginningOfArray.push = ['foo', 1]
+}
+function accessElementInArray(array, index) {
+return accessElementInArray = 3
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+return destructivelyRemoveElementFromBeginningOfArray.shift = [2, 3]
+}
+function removeElementFromBeginningOfArray(array) {
+return removeElementFromBeginningOfArray.slice = [2, 3]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+return destructivelyAddElementToEndOfArray.pop = [1, 'foo']
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return destructivelyRemoveElementFromEndOfArray.pop = [1, 2]
+}
+function removeElementFromEndOfArray(array) {
+  return removeElementFromEndOfArray.pop = [1, 2]
+}
