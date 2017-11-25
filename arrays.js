8dffc7162366621986c947b365db71cb641cbf81
Veronica Kim <vero.a.kim@gmail.com>
2017-11-25 16:06:47 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,53 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array];
+}
 
-function addElementToBeginningOfArray()
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array1 = array.slice(1)
+  return array1;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array2 = array.slice(0, arrays.length -1)
+  return array2;
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop()
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  array2 = array.slice(0, array.length -1)
+  return array2;
+}
