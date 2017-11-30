16342ae7fc365e6effbccf9757e0d8a864b15d98
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-11-30 00:30:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -18,3 +18,44 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  var arr2 = [...array, element]
+  return arr2
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  var arr2 = [array]
+  return arr2[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var arr2 = array.slice(1)
+  return arr2
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  var arr2 = array.slice(0, array.length - 1)
+  return arr2
+}
+
+function accessElementInArray(array, index) {
+  array
+  return array[index]
+}
