6d8e29eae4a19649c1da512b0041c92071468e76
Lance Patrick <patricle@miamioh.edu>
2017-12-06 22:20:36 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var cholocateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
+
+function addElementToBegginingofArray(array, element) {
+  var array = [1];
+  console.log([foo, ...array]);
+  return array
+}
