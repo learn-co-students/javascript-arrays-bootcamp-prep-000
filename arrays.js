8769db32e247f3b16acd28bbcdce97118655ba8d
ARYA WIRASANDI <aryawirasandi7153@gmail.com>
2017-12-07 15:15:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = [
+    'snickers',
+    'hundred grand',
+    'kitkat',
+    'skittles'
+]
+function addElementToBeginningOfArray(myarray){
+  myarray = [
+    "foo",
+    1
+  ];
+  return myarray;
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  myarray = [
+    'foo',
+    1
+  ];
+  return myarray;
+}
+
+function addElementToEndOfArray(myarray){
+  myarray = [
+  1,
+  'foo'
+  ];
+}
+function destructiveAddElementToEndOFArray(){
+
+}
+function accessElementInArray(){
+
+}
+function destructivelyRemoveElementFromBeginningOfArray(){
+
+}
+function removeElementFromBeginningOfArray(){
+
+}
+function destructivelyRemoveElementFromEndOfArray(){
+
+}
+function removeElementFromEndOfArray(){
+
+}
