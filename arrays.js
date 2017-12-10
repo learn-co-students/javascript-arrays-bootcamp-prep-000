6478960f247fa0669642df43c09215b900892b06
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-10 17:30:15 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+/*
 var chocolateBars = [
   'snickers',
   'hundred grand',
@@ -43,4 +44,48 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
+}
+*/
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (array, element){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element);
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+  array = array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop(element);
+  return array;
+}
+
+function removeElementFromEndOfArray(array, element) {
+  array = array.slice(0, array.length-1);
+  return array;  
 }
