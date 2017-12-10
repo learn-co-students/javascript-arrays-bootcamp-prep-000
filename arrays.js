2780f970702ea501e0cc0a34ee1444b35ddf35a2
scottburack <itsmescottb123@gmail.com>
2017-12-10 22:22:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, ele) {
+return [ele, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+  arr.unshift(ele);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, ele) {
+  return [...arr, ele]
+}
+
+function destructivelyAddElementToEndOfArray(arr, ele) {
+  arr.push(ele);
+  return arr;
+}
+
+function accessElementInArray(arr, idx) {
+  return arr[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop()
+  return arr
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length - 1)
+}
