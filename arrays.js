092594ed2de677a3efd65fcb802590ba5f707007
Zach Vary <zach.vary@gmail.com>
2017-12-14 20:30:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,37 +5,37 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array.unshift(element);
   return array
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
-  return array[index]
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+  array.shift();
   return array
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
+  return array.slice(0, array.length - 1);
 }
