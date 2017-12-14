491c3e8f630230a40d76a680532443ca817f681f
Jinwoo Bae <bbae7050@gmail.com>
2017-12-14 18:07:40 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = () => {
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles']
+}
+
+var addElementToBeginningOfArray = (array, element) => {
+  return [element, ...array];
+}
+var descructivelyAddElementToBeginningOfArray = (array, element) => {
+  array.unshift(element);
+  return array;
+}
+
+var addElementToEndOfArray = (array, element) => {
+  return [...array, element];
+}
+
+var destructivelyAddElementToEndOfArray = (array, element) => {
+  array.push(element);
+  return array;
+}
+
+var accessElementInArray = (array, index) => {
+  return array[index];
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = array => {
+  array.shift;
+  return array;
+}
+
+var removeElementFromBeginningOfArray = array => {
+  return array.slice(1);
+}
+
+var removeElementFromEndOfArray = array => {
+  return array.slice(0, array.length - 1);
+}
