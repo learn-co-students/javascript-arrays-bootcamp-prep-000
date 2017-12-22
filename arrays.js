4e103b7ab42dcae51764de0792da38368f436c19
Orlando Wethered <orlandowethered@gmail.com>
2017-12-22 23:56:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray() {
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray() {
+  console.log(chocolateBars[2])
+}
