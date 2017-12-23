c147d7273cf29c0cc63419876807c0b6f318f048
Sayaka Tamura <arccycle@gmail.com>
2017-12-23 22:57:56 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = ["snickers","hundred grand","kitkat", "skittles"]
+
+function addElementToBeginningOfArray (chocolateBars, element)
+{
+  return [element, ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray (chocolateBars, element)
+{
+  chocolateBars.unshift(element);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray  (chocolateBars, element)
+{
+  return [...chocolateBars, element] 
+}
+
+function destructivelyAddElementToEndOfArray (chocolateBars, element)
+{
+  chocolateBars.push(element) 
+  return chocolateBars
+}
+
+function accessElementInArray(array,num)
+{
+  return array[num]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
+{
+  chocolateBars.shift()
+  return chocolateBars
+}
+
+function removeElementFromBeginningOfArray (chocolateBars)
+{
+  return chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray (chocolateBars)
+{
+  chocolateBars.pop()
+  return chocolateBars
+}
+
+function removeElementFromEndOfArray  (chocolateBars)
+{
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
