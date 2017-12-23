6c6c5eedb57b54e7abb502fa9ba83c796c83815f
Rehan Ayoub <123rehan@live.com>
2017-12-23 02:03:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,15 @@
+function chocolateBars(){
+var chocolateBars= ["snickers","hundred grand","kitkat","skittles"];
+return chocolateBars;
+}
+//var array = new Array();
+//var addElement;
+function addElementToBeginningOfArray(array, addElement){
+  //array = new array();
+  array = [addElement, ...array];
+  return newArray;
+}
+
+//function destructivelyAddElementToBeginningOfArray(array, addElement){
+
+//
