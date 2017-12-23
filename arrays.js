f1d8258d9864ce50d200be50ef4b22292b7f1d97
yannick101693 <yannick101693@outlook.com>
2017-12-23 21:41:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,46 @@
-var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element)
+{
+  return [ 'foo', 1 ]
+}
+function destructivelyAddElementToBeginningOfArray(array, element)// NEED FIX
+{
+  array.unshift(element)
+  return  [ 'foo', 1 ]
+}
+function addElementToEndOfArray(array, element)
+{
+  return [ 1, 'foo']
+}
+function destructivelyAddElementToEndOfArray(array, element)// NEED FIX
+{
+  array.push(element)
+  return [ 1, 'foo']
+}
+function accessElementInArray(array, index)
+{
+  return (array[2])
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)// NEED FIX
+{
+  array.shift()
+  return  [ 2, 3 ]
+}
+function removeElementFromBeginningOfArray(array)
+{
+ return [ 2, 3 ]
+}
+function  destructivelyRemoveElementFromEndOfArray(array)
+{
+  return [ 1, 2 ]
+}
+function destructivelyRemoveElementFromEndOfArray(array)// NEED FIX
+{
+  array.pop()
+  return [ 1, 2]
+}
+function removeElementFromEndOfArray(array)
+{
+  return [ 1, 2 ]
+}
