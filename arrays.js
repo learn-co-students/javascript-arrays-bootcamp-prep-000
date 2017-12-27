e9cfd191a3512486947ea8c5af2c6c9d2d88556b
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-27 04:36:14 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var element = 'foo'
+function addElementToBeginningOfArray(chocolateBars, element) {
+  return [element, ...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
+  chocolateBars.unshift(element)
+  return chocolateBars
+}
+function addElementToEndOfArray(chocolateBars, element) {
+  return [...chocolateBars, element]
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, element) {
+  chocolateBars.push(element)
+  return chocolateBars
+}
+function accessElementInArray(chocolateBars, index) {
+  return chocolateBars[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift()
+  return chocolateBars
+}
+function removeElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  chocolateBars.pop()
+  return chocolateBars
+}
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(0, chocolateBars.length - 1)
+}
+
+
+
+
+
+
+
