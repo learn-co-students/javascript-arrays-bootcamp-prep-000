7e4fb92179302904b3f7c9c1bd1c1d88c5f20b3c
Isaias Pomales <impomales@gmail.com>
2017-12-28 18:53:46 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, e) {
+  return [e, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, e) {
+  arr.unshift(e);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, e) {
+  return [...arr, e];
+}
+
+function destructivelyAddElementToEndOfArray(arr, e) {
+  arr.push(e);
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+  return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length - 1);
+}
