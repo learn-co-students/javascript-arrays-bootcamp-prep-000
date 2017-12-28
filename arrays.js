65b7f19688bd28ba6accafc51f7f7f65bd003f98
Mariel Ríos <marielbrios09@gmail.com>
2017-12-28 21:05:38 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  console.log(array[index]);
+}
