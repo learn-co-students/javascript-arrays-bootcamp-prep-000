3d435c0911f03537ac25c549096f7d8404756c07
Rhys Williams <rwillcolumbia@gmail.com>
2017-12-28 18:31:53 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var choclateBars = [
+  'Snickers',
+  'Hundred Grand',
+  'KitKat',
+  'Skittles'
+  ]
+  
+function addElementToBeginningOfArray ()[
+  ["Hersheys...choclateBars"]
+   choclateBars = ["Hersheys",...choclateBars]
+  ]
+  
+  function destructivelyAddElementToBeginningOfArray()[
+    choclateBars.unshift("Hersheys")
+    return choclateBars;
+    ]
