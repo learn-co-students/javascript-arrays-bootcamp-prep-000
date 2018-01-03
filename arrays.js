0f9a66ee1914fb89bad8981b3d979fd36386af0b
Samantha Gibson <sgibson47@gmail.com>
2018-01-03 17:54:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
