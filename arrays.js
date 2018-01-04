6ce53dd64fff16fd0f5f4625eeaecaf2783dc1ab
Jeewon Kim <jki00129@gmail.com>
2018-01-04 17:11:54 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var superheroines = ["catwoman", "she-hulk", "mystique"]
+superheroines.push("wonder woman");
