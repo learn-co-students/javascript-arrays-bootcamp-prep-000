f21a2944495a04b8d60363e32cc2ffe998aee698
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-04 02:58:27 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,2 +1,3 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
-var addElementToBeginningOfArray=[...chocolateBars,"foo"];
+var addElementToBeginningOfArray=[chocolateBars];
+addElementToBeginningOfArray.push('foo');
