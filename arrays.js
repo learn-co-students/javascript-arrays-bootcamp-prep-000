cc5665cfa8a515bd751348cd57b6c8d2e14e8538
Samantha Gibson <sgibson47@gmail.com>
2018-01-04 16:05:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -17,3 +17,25 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1)
+}
