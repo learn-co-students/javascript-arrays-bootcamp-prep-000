a1985c2775c5ae897f4705993b093c96d26b553c
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-04 04:00:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
-var addElementToBeginningOfArray=[chocolateBars];
-addElementToBeginningOfArray.push('foo');
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return [element,...array]
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return (array)
+}
+function addElementToEndOfArray(array,element){
+  array.push(element)
+  return (array)
+}
