7fb6570c6e60d36f82f69d3d1decc6d1637f1d0a
Rosie Wang <rosiedwang@yahoo.com>
2018-01-08 21:53:11 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,42 @@
-
-
-function addElementToEndOf(array, element)
+var chocolateBars = ["snickers","hundred grand", "kitkat","skittles"]
+function addElementToBeginningOfArray(array, element)
 {
-   array.pop(element)
+  array = [element,...array];
+  return array
 }
-function destructivelyAddElement(array,element)            
+function destructivelyAddElementToBeginningOfArray(array,element)
 {
-  
+  array.unshift(element);
+  return array
+}
+function addElementToEndOfArray(array, element)
+{
+   array = [...array,element]
+   return array
+}
+function destructivelyAddElementToEndOfArray(array,element)            
+{
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array)
+{
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+}
+function removeElementFromEndOfArray(array)
+{
+  return array.slice(0,array.length-1)
 }
