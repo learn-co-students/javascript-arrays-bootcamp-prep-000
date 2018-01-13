1527adf24a9a9ccea1e0b056eb9245302ecbdd2c
butch cosio <butchcosio@yahoo.com>
2018-01-13 20:17:27 +0000

    Done.

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -13,13 +13,13 @@ describe('arrays', () => {
   describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
       expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
-    })
-  })
+    });
+  });
 
   describe('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
-      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-    })
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1]);
+    });
 
     it('does not alter the original array', () => {
       const array = [1]
