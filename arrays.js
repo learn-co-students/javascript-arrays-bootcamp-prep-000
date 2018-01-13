627f416553d21554b301952439e7b7af80581b8e
SDS 800 <jeremyhyman@gmail.com>
2018-01-13 01:03:56 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+ function addElementToBeginningOfArray (array,foo) {
+   
+    return ["foo", ...array]
+  
+}
+
+  function destructivelyAddElementToBeginningOfArray(array, foo){
+    array.unshift("foo")
+   return array
+    
+  }
+function addElementToEndOfArray(array, foo){
+  
+  return [...array, "foo"]
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, foo) {
+  array.push("foo")
+  return array
+}
+
+function accessElementInArray(array, index) {
+return array[2]
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+  
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  
+  return array.slice(0, array.length - 1)
+}
