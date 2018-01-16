396ca8312519c3965f8b3f76c96175bc14dd6ff0
Mark Grant <mark.rd.grant@gmail.com>
2018-01-16 02:08:28 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray([1], 'foo') {
+  const array = [1]
+}
+
+function destructivelyAddElementToBeginningOfArray([1], 'foo') {
+
+}
