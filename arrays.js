9a324a46f2f8f009f5d88393026f7e1b16267ad3
Mark Grant <mark.rd.grant@gmail.com>
2018-01-16 18:50:34 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,13 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, 'foo') {
-  return ['foo', ...array]
+  return [array..., element]
 }
