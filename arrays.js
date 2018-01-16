33a23672d94a68bc2c0f0117e4b0f9b0f24969fc
Mark Grant <mark.rd.grant@gmail.com>
2018-01-16 20:36:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
