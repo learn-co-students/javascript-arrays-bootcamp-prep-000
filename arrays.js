710c0f82c3f8ab95e06db416630d27f5b58db5b6
Douglas Craig <dcraig112@gmail.com>
2018-01-19 01:02:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,55 +1,27 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-/*return a new array and not modify the original.*/
-function addElementToBeginningOfArray(inputArray, elementToUnshift) {
-	var differentArray = inputArray
-	differentArray.unshift(elementToUnshift)
-	return differentArray
+function addElementToBeginningOfArray(array, element) {
+	return [element, ...array]
 }
-
-/* add to beginning of array and return altered array */
-function destructivelyAddElementToBeginningOfArray(inputArray, elementToUnshift) {
-	inputArray.unshift(elementToUnshift)
-	return inputArray
+function destructivelyAddElementToBeginningOfArray(array, element) {
+	array.unshift(element)
+	return array
 }
-
-function addElementToEndOfArray(inputArray, elementToPush) {
-	var differentArray = inputArray
-	differentArray.push(elementToPush)
-	return differentArray
+function addElementToEndOfArray(array, element) {
+	return [...array, element]
 }
-
-function destructivelyAddElementToEndOfArray(inputArray, elementToPush) {
-	inputArray.push(elementToPush)
-	return inputArray
+function destructivelyAddElementToEndOfArray(array, element) {
+	array.push(element)
+	return array
 }
-
-function accessElementInArray(inputArray, index) {
-	return inputArray[index]
+function accessElementInArray(array, index) {
+	return array[index]
 }
-
-/*Define a function in arrays.js called destructivelyRemoveElementFromBeginningOfArray that takes an array as its only argument and removes the first element. Your function should then return the entire array, and it should mutate the array.*/
-function destructivelyRemoveElementFromBeginningOfArray(inputArray) {
-	inputArray.shift()
-	return inputArray
+function removeElementFromBeginningOfArray(array) {
+	array.shift()
+	return array
 }
-
-/*	Define a function in arrays.js called removeElementFromBeginningOfArray that takes an array as its only argument and removes the first element. Your function should then return the entire array, and it should not mutate the underlying array. */
-function removeElementFromBeginningOfArray(inputArray) {
-	var newArray = inputArray.slice(1)
-	return newArray
-}
-
-/*Define a function in arrays.js called destructivelyRemoveElementFromEndOfArray that takes an array as its only argument and removes the last element. Your function should return the entire array, and it should mutate the array.*/
-
-function destructivelyRemoveElementFromEndOfArray(inputArray) {
-	var newArray = inputArray.pop()
-	return inputArray
-}
-
-/* Define a function in arrays.js called removeElementFromEndOfArray that takes an array as its only argument and removes the last element. Your function should return the entire array, and it should not mutate the array.*/
-
-function removeElementFromEndOfArray(inputArray) {
-	var newArray = inputArray.slice(0, inputArray.length -1)
-	return newArray
+function removeElementFromEndOfArray(array) {
+	array.pop()
+	return array
 }
