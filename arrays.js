b35bc31e3a2ba27492ffb55a8a532ce7071c1c1e
Devin DiNapoli <devindinapoli@gmail.com>
2018-01-31 19:04:11 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,6 +4,22 @@ var chocolateBars = ["snickers",
   "skittles"];
   
   function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
   
-    
   } 
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element)
+    
+    return array
+  }
+  
+  function addEelementToEndOfArray (array, element){
+    array.push(element)
+    return array
+    
+  }
+  
+  function destructivelyAddElementToEndOfArray(array, element) {
+    return [...array, element]
+  }
