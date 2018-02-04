507b07a48a6b67f1d797df365e346dbe0a2079b8
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-02-04 23:03:49 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,12 +7,53 @@ chocolateBars.unshift("Mounds");
 
 
 
-var chocolateBarsYum = 
-["snickers", "hundred grand", "kitkat", "skittles"];
-
 function addElementToBeginningOfArray(array, element){
-  
-  return array.push(element);
+  var arrayNew = [element, ...array];
+  return arrayNew;
 }
-addElementToBeginningOfArray(chocolateBarsYum,"Mounds");
 
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var arrayNew = [...array, element];
+  return arrayNew;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(array, element){
+    array.shift(element);
+    return array;
+  }
+  
+  function removeElementFromBeginningOfArray(array, element){
+   array.shift(element);
+    return array;
+    }
+    
+    function destructivelyRemoveElementFromEndOfArray(array, element){
+      array.pop(element);
+      return array;
+    }
+    
+    function removeElementFromEndOfArray(array, element){
+      return array.slice(0, array.length - 1);
+    }
+      
+      
+      function accessElementInArray(array, index){
+       return array[index];
+      }
+      
+      function removeElementFromBeginningOfArray(array, element){
+        return array.slice(1);
+        
+      }
+      
+     
