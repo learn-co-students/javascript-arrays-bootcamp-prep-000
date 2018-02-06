66a883658bad6d82390d2233f35f10289277e136
Devon Kincaid <tenaciousdk86@aol.com>
2018-02-06 20:36:25 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,47 +1,43 @@
-var chocolateBars = ['snickers', 'hundred grand' , 'kitkat', 'skittles']
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element){
-  return [...array, element]
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index){
-  return array[index]
-}
+  
+return array[index];
+  }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
-  return array
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length - 1)
+  return array.slice(0, array.length -1);
 }
-
-
-
-
-
 
