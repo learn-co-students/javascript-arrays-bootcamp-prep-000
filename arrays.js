2426dbfe11c0f3e76c8d2191a8eae846b6337626
Brittany Tinnin <bntinnin7@gmail.com>
2018-02-23 16:24:30 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -24,17 +24,34 @@ var tomatoSauceIngredients = [
     
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(element, array) {
-   return [array, ...element]
+   return [array, ...element];
 }
 function destructivelyAddElementToBeginningOfArray(array, element)
 {
-  array.unshift(element)
-  return array 
+  array.unshift(element);
+  return array; 
 }
 function addElementToEndOfArray(element, array) {
-  return [...element, array]
+  return [...element, array];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
