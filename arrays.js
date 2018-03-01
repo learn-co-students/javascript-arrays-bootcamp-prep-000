419f6f3c7991df77ba2303956763e1d42b19e3a2
max behling <elmachomax123@gmail.com>
2018-03-01 01:20:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,14 +3,67 @@ chocolateBars.push('heroin')
 chocolateBars.unshift('skittles')
 
 function addElementToBeginningOfArray(yeet, boii) {
-  
+  let addElementToBeginningOfArray = ['ah', 'ahh', 'ahhh', 'ahhhh!']
+  addElementToBeginningOfArray.unshift('hows about another one?')
+  return addElementToBeginningOfArray
 }
 
 function destructivelyAddElementToBeginningOfArray(hey, kid) {
-  
+  let destructivelyAddElementToBeginningOfArray = ['yes', 'sir', 'my dude']
+  destructivelyAddElementToBeginningOfArray.unshift('another one')
+  return destructivelyAddElementToBeginningOfArray
 }
 
-function accessElementInArray() {
-  
+function addElementToTheEndOfArray(parameter1, parameter2) {
+ let addElementToTheEndOfArray = ['pickles', 'ice', 'vanilla']
+ addElementToTheEndOfArray.push('more vanilla')
 }
+
+function destructivelyAddElementToEndOfArray() {
+  let destructivelyAddElementToEndOfArray = ['seether', 'metallica', 'Offspring']
+  destructivelyAddElementToEndOfArray.push('smashmouth')
+}
+
+
+
+function accessElementInArray() {
+  let accessElementInArray = ['something', 'something else', 'another one']
+  accessElementInArray.shift([3])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  let destructivelyRemoveElementFromBeginningOfArray = ['ho', 'hoo', 'hooo']
+  destructivelyRemoveElementFromBeginningOfArray.slice(0)
+}
+
+function removeElementFromBeginningOfArray() {
+  let removeElementFromBeginningOfArray = ['pirates', 'gay pirates', 'slutty bartenders']
+  removeElementsFromBeginningOfArray.pop()
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+  let destructivelyRemoveElementFromEndOfArray = ['more', 'even more', 'naked jellybeans']
+  destructivelyRemoveElementFromEndOfArray.pop()
+}
+
+function removeElementFromEndOfArray() {
+  let removeElementFromEndOfArray = ['dad', 'padre', 'daddy']
+  removeElementFromEndOfArray.slice()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
