3d92a2d653eb3488027122fef80326e7f2c9fdd2
max behling <elmachomax123@gmail.com>
2018-03-01 00:41:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+chocolateBars.push('heroin')
+chocolateBars.unshift('skittles')
+
+function addElementToBeginningOfArray(yeet, boii) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(hey, kid) {
+  
+}
+
+function accessElementInArray() {
+  
+}
+
