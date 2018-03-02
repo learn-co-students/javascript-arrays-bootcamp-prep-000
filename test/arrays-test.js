3004ee4ccc1a3ff313657fc0e2942454bd9f99cc
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 04:18:19 +0000

    Done.

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,5 +1,8 @@
 /*global describe, it */
-
+describe('arrays', () => {
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
+  })
 const expect = require('chai').expect
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
