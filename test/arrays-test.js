6f78517e484f4b01eae7975d8aee1e6db50c8cd1
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 03:53:16 +0000

    Done.

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -5,6 +5,63 @@ const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
 
+var grilledCheeseIngridents = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic' ]
+var tomatoSauceIngridents = ['tomato', 'garlic', 'olive oil', 'basil', 'oreagno']
+var myArray = ['element 0', 'element 1', 'element 2, ..., elementN'];
+
+var primeNumbers = ['2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37'];
+ 
+var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+ 
+var weirdGreeting = [ "he", '110', "w", '0', "r", '1', "d" ];
+
+var superheroines = ["catwoman", "she-hulk", "mystique"];
+ 
+superheroines.push("wonder woman");
+
+var chocolateBars = 
+  ["hundred grand", "kitkat", "skittles", "snickers"];
+  function()addElementToBeginningOfArray {
+    it ('add the element to the beginning of array');
+    'returns("the whole array");'
+  }
+function()destructivelyAddElementToBeginningOfArray {
+  it ('add the elementto  to the beginning of array, alters the original array')
+ 'returns(the new array and not modifiy the original);'
+}
+function()addElementToEndOfArray {
+  it ('takes two arguments, an array and an element to add to the end of the array, without altering the original');
+  'returns (without altering the original array);'
+}
+function()accessElementInArray {
+  it ('accepts an array and an index') 
+  returns('the element at that index');
+}
+function()destructivelyRemoveElementFromBeginningOfArray {
+  it ('takes an array as an argument and removes the first element');
+  'returns(the entire array and mutates the array);'
+}
+function()removeElementFromBeginningOfArray {
+  it ('takes an array as the only argument and removes the first element');
+  'returns(the entire array and should not mutate underlying array);'
+}
+function()destructivelyRemoveElementFromEndOfArray {
+  it ('takes an array as its only argument and removes the last element');
+  'returns(the entire array, and mutate the array);'
+}
+function()removeElementFromEndOfArray {
+  it ('takes an array as its only argument and removes the last elementy')
+  'returns(the array without the last element and should not mutate the original array);'
+}
+
+}
+
+}
+var cities = ["New York", "San Fransisco", "...cities", "Philidelphia"]
+ 
+var empty = [];
+
+
 describe('arrays', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
@@ -12,7 +69,7 @@ describe('arrays', () => {
 
   describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
+      expect(chocolateBars).to.eql["hundred grand", "kitkat", "skittles", "snickerslearn"]
     })
   })
 
