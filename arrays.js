e9e12e17373a020c3cfaf62fc7a056b15e83259d
Sean F <sean.flanagan@flatironschool.com>
2018-03-04 02:18:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
