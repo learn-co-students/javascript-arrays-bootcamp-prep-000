5979feaa02b3ab5efdeb3a61c0c15c5456c1edc7
max behling <elmachomax123@gmail.com>
2018-03-05 02:03:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,58 +1,59 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-chocolateBars.push('heroin')
-chocolateBars.unshift('skittles')
+//disregard code above this line
 
-function addElementToBeginningOfArray(yeet, boii) {
-  let addElementToBeginningOfArray = ['ah', 'ahh', 'ahhh', 'ahhhh!']
-  addElementToBeginningOfArray.unshift('hows about another one?')
-  return addElementToBeginningOfArray
+function addElementToBeginningOfArray(parameter1, parameter2) {
+  ['yea', 'boi']
+}
+['ahhahaha',...addElementToBeginningOfArray]
+//end of function 1 
+
+function destructivelyAddElementToBeginningOfArray(parameter3, parameter4) {
+  ['yes', 'sir']
+}
+destructivelyAddElementToBeginningOfArray.unshift('more variables')
+//end of function 2 
+
+function addElementToEndOfArray(parameter5, parameter6) {
+  ['more','even more']
 }
 
-function destructivelyAddElementToBeginningOfArray(hey, kid) {
-  let destructivelyAddElementToBeginningOfArray = ['yes', 'sir', 'my dude']
-  destructivelyAddElementToBeginningOfArray.unshift('another one')
-  return destructivelyAddElementToBeginningOfArray
+//end of function 3
+function destructivelyAddElementToEndOfArray(parameter7, parameter8) {
 }
-
-function addElementToTheEndOfArray(parameter1, parameter2) {
- let addElementToTheEndOfArray = ['pickles', 'ice', 'vanilla']
- addElementToTheEndOfArray.push('more vanilla')
-}
-
-function destructivelyAddElementToEndOfArray() {
-  let destructivelyAddElementToEndOfArray = ['seether', 'metallica', 'Offspring']
-  destructivelyAddElementToEndOfArray.push('smashmouth')
-}
-
-
+destructivelyAddElementToEndOfArray.push('more stupid variables')
+//end of function 4
 
 function accessElementInArray() {
-  let accessElementInArray = ['something', 'something else', 'another one']
-  accessElementInArray.shift([3])
+  ['this is an array','content', 'more content']
 }
+//end of function 5
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-  let destructivelyRemoveElementFromBeginningOfArray = ['ho', 'hoo', 'hooo']
-  destructivelyRemoveElementFromBeginningOfArray.slice(0)
+function destructivelyRemoveElementFromBeginningOf() {
+  ['number 1', 'number 2', 'number 3']
 }
+destructivelyRemoveElementFromBeginningOf.shift()
+
+// end of function 6
 
 function removeElementFromBeginningOfArray() {
-  let removeElementFromBeginningOfArray = ['pirates', 'gay pirates', 'slutty bartenders']
-  removeElementsFromBeginningOfArray.pop()
+  ['here', 'is', 'another', 'array']
 }
+removeElementFromBeginningOfArray.slice(0)
+
+// end of function 7
 
 function destructivelyRemoveElementFromEndOfArray() {
-  let destructivelyRemoveElementFromEndOfArray = ['more', 'even more', 'naked jellybeans']
-  destructivelyRemoveElementFromEndOfArray.pop()
+  ['im', 'tired', 'of', 'typing', 'arrays']
 }
+destructivelyRemoveElementFromEndOfArray.pop()
+
+// end of function 8
 
 function removeElementFromEndOfArray() {
-  let removeElementFromEndOfArray = ['dad', 'padre', 'daddy']
-  removeElementFromEndOfArray.slice()
+  ['hop', 'hippity', 'hop']
 }
-
-
-
+removeElementFromEndOfArray.slice(0,
+removeElementFromEndOfArray.length - 1)
 
 
 
