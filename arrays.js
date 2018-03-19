5123e6254a69c2364f91c41d5272a76f663c653a
Christopher <christopherccm@msn.com>
2018-03-19 23:52:01 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,22 @@
+
+let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+var rockTypes = ['igneous', 'basalt', 'granite']
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(${element});
+  return array;
+}
+addElementToBeginningOfArray(rockTypes, 'sedimentary'}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  ${array}.unshift(${element});
+  return array;
+}
+destructivelyAddElementToBeginningOfArray(rockTypes, 'sedimentary'}
+
+function accessElementInArray(array, indexNumber) {
+  console.log(array[indexNumber])
+}
