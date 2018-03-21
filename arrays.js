4e2a033a63ffedd221719cf4f348ec9735209733
Christiana Vandermale <christianavdm@gmail.com>
2018-03-21 00:38:50 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,7 +3,5 @@ var addElementToBeginningOfArray = function(array, element) {
   return [element, ...array]
 }
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
-  var array = new Array(element, ...array)
-  array
-  return [array]
+  return [array.unshift(element)]
 }
