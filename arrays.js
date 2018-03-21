e469a4c2bc0bd3a2f323b46b030af80c1b4ffb27
James Ryan Nielcen <rnielcen@hotmail.com>
2018-03-21 16:43:22 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+ 
+ function addElementToBeginningOfArray(chocolateBars, foo){
+return ['foo', ...chocolateBars]
+ }
+ 
+ function destructivelyAddElementToBeginningOfArray(chocolateBars, foo){
+ [chocolateBars.unshift("foo")]
+ return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, foo){
+  return [...chocolateBars, 'foo']
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, foo){
+  [chocolateBars.push('foo')]
+  return chocolateBars
+}
+
+function accessElementInArray(chocolateBars, index){
+return chocolateBars[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+[chocolateBars.shift()]
+return chocolateBars
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+return chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+[chocolateBars.pop()]
+return chocolateBars
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+return chocolateBars.slice(0, chocolateBars.length - 1)
+}
