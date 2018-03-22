0173fe51db4981900b35e472680ebc7044b2a300
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-22 11:40:46 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -35,3 +35,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length - 1);
 }
+function accessElementInArray(array, index){
+  return array[index];
+}
