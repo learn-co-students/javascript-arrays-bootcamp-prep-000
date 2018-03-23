78fd2467c379b457961d0d2fcc6e824122736c55
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-23 12:34:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  const newArray = array.unshift(element);
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return newArray = array.push(element)
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+
+function accessElementInArray() {
+  
+}
