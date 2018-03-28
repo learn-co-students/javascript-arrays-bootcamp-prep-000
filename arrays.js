718d4b81d476009b896f95a16319365e55e6b2d7
piotrkulak@outlook.com <piotrkulak@outlook.com>
2018-03-28 22:45:00 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var addElementToBeginningOfArray = [1,2];
+addElementToBeginningOfArray.push(3);
+
+var destructivelyAddElementToBeginningOfArray = [1,2]
+destructivelyAddElementToBeginningOfArray[0] = 0
+destructivelyAddElementToBeginningOfArray
+
+var addElementToEndOfArray()
+var destructivelyAddElementToEndOfArray()
+
+var removeElementFromEndOfArray = [1,2,3]
+removeElementFromEndOfArray = removeElementFromEndOfArray.slice(1)
