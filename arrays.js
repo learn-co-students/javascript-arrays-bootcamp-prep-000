ce14af577b325c841f7a97309ef8a86d8e1ec392
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 07:32:06 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,39 +1,48 @@
+chocolateBars=[ "snickers", "hundred grand", "kitkat", "skittles"]
+
 function addElementToBeginningOfArray(array, element){
   const moreAr = [element,...array];
   return moreAr;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+
+function destructivelyAddElementToBeginningOfArray(xc, bv){
+   
+   xc.unshift(bv);
+   return xc
 }
 
 function addElementToEndOfArray (array, element){
-  return array.push(element)
+  const moreArr = [...array, element];
+  return moreArr;
+
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  const moreArr = [...array, element]
-  return moreArr
-}
+  array.push(element);
+  return array;}
 
 function accessElementInArray(array, index){
   return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift();
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop();
+function destructivelyRemoveElementFromEndOfArray(lal){
+  
+  lal.pop();
+  return lal;
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(array.length(-1));
+  return array.slice(0, array.length-1);
 }
 
 
