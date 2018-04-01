3ec68ca4442ca60f7d0ea09f6ea4c48d096fa999
richiebonilla01@gmail.com Bonilla <richiebonilla01@gmail.com>
2018-04-01 23:55:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+var addElementToBeginningOfArray = function (array, element) {
+  return [element, ...array]
+}
+
+var destructivelyAddElementToBeginningOfArray = function (array, element) {
+  array.unshift(element)
+  return array
+}
+
+var addElementToEndOfArray = function (array, element) {
+  return [...array, element]
+}
+
+var destructivelyAddElementToEndOfArray = function (array, element) {
+  array.push(element)
+  return array
+}
+
+var accessElementInArray = (array, index) => {
+  return array[index]
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = (array) => {
+  array.shift()
+  return array
+}
+
+var removeElementFromBeginningOfArray (array) => {
+  return array.slice(1)
+}
