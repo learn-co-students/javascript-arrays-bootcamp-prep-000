cdaeebf844d9c94af17b6a09929165cf66a14dbe
Taryn Boni <taryn04@live.com>
2018-04-03 19:34:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+function addElementToBeginningOfArray(array, element){
+  const addElementToBeginningOfArray = [element,...array]
+  return addElementToBeginningOfArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  const addElementToEndOfArray = [...array,element]
+  return addElementToEndOfArray
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
