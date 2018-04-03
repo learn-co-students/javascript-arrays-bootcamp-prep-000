51f26ca39069462231f9bd90cac89fc715731890
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-03 02:19:01 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,2 +1,26 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+
+function accessElementInArray(array, index) {
+ return array[index];
+}
+
+function destructivelyRemoveElementToEndOfArray(array) {
+  return array.shift();
+}
 
