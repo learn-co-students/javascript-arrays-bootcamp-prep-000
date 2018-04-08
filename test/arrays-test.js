ca9c378aa2a359d0c169d08225189a5e9c0e5927
Marcello Lindo <marcellolindo34@gmail.com>
2018-04-08 19:15:26 +0000

    Done.

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -3,30 +3,30 @@
 const expect = require('chai').expect
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
-const path = require('path')
+const path = require('path');
 
 describe('arrays', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
-  })
+  });
 
   describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
-    })
-  })
+      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles'];
+    });
+  });
 
   describe('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
-      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-    })
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1]);
+    });
 
     it('does not alter the original array', () => {
-      const array = [1]
+      const array = [1];
 
-      addElementToBeginningOfArray(array, 'foo')
+      addElementToBeginningOfArray(array, 'foo');
 
-      expect(array).to.eql([1])
+      expect(array).to.eql([1]);
     })
   })
 
