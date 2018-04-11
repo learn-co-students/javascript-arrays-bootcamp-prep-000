386e2a4a9618865e8cb93d8980a84d3d5f06ed05
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-11 20:02:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+var addElementToBeginningOfArray = ['hello', 'car', 'pet', 'job', 'computer']
+addElementToBeginningOfArray.unshift("Cow")
