955e4dd4d8bd485396826d258a28b0469e8ab1ca
Jessica Green <jgreen629@yahoo.com>
2018-04-12 17:31:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+/* describe('addElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+
+    it('does not alter the original array', () => {
+      const array = [1]
+
+      addElementToBeginningOfArray(array, 'foo')
+
+      expect(array).to.eql([1])
+    })
+  }) */
+  
+
+function addElementToBeginningOfArray(element, array) {
+  addElementToBeginningOfArray('foo')
+}
+
+
+function destructivelyAddElementToBeginningOfArray(element, array) {
+  destructivelyAddElementToBeginningOfArray('foo')
+}
