07c682b850b8917139abcea1f83664fda75bacbd
Jessica Green <jgreen629@yahoo.com>
2018-04-12 22:44:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,22 +1,32 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
   
-function addElementToBeginningOfArray(element, array) {
-  element.unshift()
+
+function addElementToBeginningOfArray(array, element) {
+  ["foo"...array]
+  
+  addElementToBeginningOfArray("foo")
 }
 
 
-function destructivelyAddElementToBeginningOfArray(element, array) {
-   element.unshift('foo')
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+ 
 }
+
+
+
+
 
 
 function addElementToEndOfArray(array, element) {
-  element.push()
+  
 }
 
+
 function destructivelyAddElementToEndOfArray(array, element) {
-  element.push('foo')
+ 
 }
+
 
 
