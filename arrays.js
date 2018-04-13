62b2b0fde2c7e284edc3875b6929bc427f4a9c13
angelica winkler <caw400@nyu.edu>
2018-04-13 20:22:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,30 +1,22 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat','skittles' ]
+
 function addElementToBeginningOfArray (array, element) {
-  return [element, ...testArray]
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  array.shift(element);
+  array.unshift(element);
   return array;
 }
 
-
-
-var testArray = [1, 2, 3]
-
-
-
 function addElementToEndOfArray (array, element) {
-  return [...testArray, element]
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element);
   return array;
 }
-
-
-
-console.log(destructivelyAddElementToEndOfArray(testArray, 4));
 
 
 
