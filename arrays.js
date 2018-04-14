0018f6a40b101b39602ac2312092f5f3b1d15565
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-14 22:01:53 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var c = "sup";
+
+function addElementToBeginningOfArray (chocolateBars, c) {
+  
+  return [c, ...chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, c){
+  chocolateBars.unshift(c);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, c){
+  return [...chocolateBars, c];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, c){
+  chocolateBars.push(c);
+  return chocolateBars;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
