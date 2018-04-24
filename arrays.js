ebe1e7f363f8d828d8df5bbafcc7e9d03f4ca492
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-24 02:43:58 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  const newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  const newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
