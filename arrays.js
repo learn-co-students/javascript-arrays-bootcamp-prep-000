b4a319c01581d68ef2138d5625b4b77104947864
April Kiser <denise626@hotmail.com>
2018-04-30 02:22:42 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  array = [1];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [1];
+}
