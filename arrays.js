8281ea2d2a461d1f8f1df2797a29686abddfabd7
emy garcia <emygarcia1920@gmail.com>
2018-05-02 00:00:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars= ['snickers','hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray(array,element){
+  return [element,...array]
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array 
+}
+
+function addElementToEndOfArray(array,element){
+  return[...array,element]
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array,index){
+  array [1,2,3]
+  return 3
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+ return array.shift()
+ 
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(-2)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length-1)
+}
