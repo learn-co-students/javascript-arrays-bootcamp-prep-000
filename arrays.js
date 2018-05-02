306bcb90a6e94c0e1360a08d8156ae7aa178e164
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 10:54:07 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,48 @@
+
+
+var chocolateBars=['snickers','hundred grand', 'kitkat', 'skittles'];
+var candyString='foo';
+
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+
+function accessElementInArray(chocolateBars, index){
+  return (chocolateBars[2]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift(candyString);
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  chocolateBars=chocolateBars.slice(1);
+  return chocolateBars;
+} 
+  function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+    chocolateBars.pop()
+    return chocolateBars;
+  }
+
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars=chocolateBars.slice(0, chocolateBars.length-1);
+  return chocolateBars;
+}
+
+
+
