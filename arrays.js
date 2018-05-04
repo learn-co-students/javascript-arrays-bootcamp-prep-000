7e7725667fce2c2ac8191f018baae94c7590e72a
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-04 17:24:12 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+function addElementToBeginningOfArray(array,element) {
+  return array.unshift()
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  return array.unshift(element)
+}
+  
+function accessElementInArray(array,index) {
+  
+}
