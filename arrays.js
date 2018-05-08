2cb124d1e0b4c02399108cd45b5b33d0eb989994
Cristina Lugo <cmlugoce@syr.edu>
2018-05-08 21:28:35 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,31 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var num = [1, 2, 3, 4]
-console.log(num[2]);
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array]
+}
+
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+return array.unshift(element)
+}
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array, element)
+{
+  return array.shift(element)
+}
+
+function removeElementFromBeginningOfArray (array, element) {
+  return array.slice(element)
+}
+
+function destructivelyRemoveElementFromEndOfArray (array, element) {
+  return array.pop(element)
+}
+
+function removeElementFromEndOfArray (array, element) {
+  return array.slice(0, array.length-1)
+}
