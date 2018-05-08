7145d206005b4c00e9be5af19a4440524e9f156d
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-08 13:40:54 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,18 +7,40 @@ var chocolateBars = [
   
 function addElementToBeginningOfArray(array,element) {
 return [element,...array]
-
-}
-
-function addElementToBeginningOfArray(array,element) {
-  array.unshift()
-  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
   
+ function addElementToEndOfArray(array,element) {
+    return [...array,element]
+  }
+  
+  function destructivelyAddElementToEndOfArray(array,element) {
+    array.push(element)
+    return array
+  }
+
 function accessElementInArray(array,index) {
-  return console.log(index)
+return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray (array) {
+return array.slice(0, array.length - 1)
+  }
