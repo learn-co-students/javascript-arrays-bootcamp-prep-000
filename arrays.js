c2ce808a19525abdee10bce6848684f81d031130
stefan.vicentijevic@gmail.com <stefan.vicentijevic@gmail.com>
2018-05-13 20:25:29 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,8 +1,45 @@
-var chocolateBars = ["snickers", "hundred", "grand", "kitkat", "skittles"];
-console.log(chocolateBars);
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
 
 function addElementToBeginningOfArray(array, element) {
-    var array = ['foo', 1];
-    element.string = "twix";
-    console.log(element, ...array);
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
