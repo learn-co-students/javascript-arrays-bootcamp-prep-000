07a8b1acbf27414c0c53e6a4070640b2f9f4a403
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 00:16:09 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(string,string2){
+ var newArray = [string2, ...string]
+ return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+  var newArray = [...array,element]
+  return newArray
+}
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
+}
+function accessElementInArray(array,index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  newArray = array.slice(1)
+  return newArray
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  newArray = array.slice(0, array.length - 1)
+  return newArray
+}
