7e16e4d45a99dcff57c73151985926fa38b491eb
Rayan Dutta <rondutta23@gmail.com>
2018-05-15 14:47:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+var addElementToBeginningOfArray = function(array,value){
+  array = [value,...array];
+  return array;
+};
+
+var destructivelyAddElementToBeginningOfArray = function(array,value){
+  array[0]=value;
+  return array;
+};
+
+var addElementToEndOfArray = function(array,value){
+  array = [...array,value];
+  return array;
+};
+
+var destructivelyaddElementToEndOfArray = function(array,value){
+  array[array.length]=value;
+  return array;
+};
