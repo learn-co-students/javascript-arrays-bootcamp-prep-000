3368dc40cce971972f3666ffd77a039bad86e3fe
Linzi Moore <linzimoore643@gmail.com>
2018-05-19 05:48:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ];
+    
+function addElementToBeginningOfArray(array,element) {
+  var catsPajamas = [ 
+    'Macy',
+    'Paca',
+    'Marissa'
+    ];
+  [...catsPajamas,
+    'Kendall']  
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  var timmyTim = [
+    'Tim',
+    'Timmy',
+    'Lil Timmy T',
+    'Lil Timmy Tim'
+    ];
+    timmyTim.unshift('Timothee');
+}
