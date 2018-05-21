ae6dd0faaf70ba17612e24ef066cd103bc9c5c59
Kevin Joseph Abalos <kevinabalos@gmail.com>
2018-05-21 05:37:46 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array,element) {
+
+  array = []
+  array.unshift(element)
+  return array
+}
+
