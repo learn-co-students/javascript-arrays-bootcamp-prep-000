7fdf62f627175711bafca12673f8fc27b5c3669e
Bryan Ochoa-Flores <ochoafloresb@yahoo.com>
2018-05-22 19:32:29 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -12,12 +12,10 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 function addElementToEndOfArray(array, element){
   return [...array, element];
 }
-
-function destructivelyAddElementToBeginningOfArray(array, element){
+function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
-
 function accessElementInArray(array, index){
   return array[index];
 }
