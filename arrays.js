0acc8512747a2152c6f54c2dbc1e5eb2b1528cd1
Bryan Ochoa-Flores <ochoafloresb@yahoo.com>
2018-05-22 19:14:33 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,31 +3,39 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
 function addElementToEndOfArray(array, element){
   return [...array, element];
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.push(element);
   return array;
 }
+
 function accessElementInArray(array, index){
   return array[index];
 }
+
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array;
 }
+
 function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
-function destructivelyRemoveElementFromEndOfArray(array){
+
+function destructivelyRemoveElementFromEndOfArray(array, element){
   array.pop();
   return array;
 }
+
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length - 1);
 }
