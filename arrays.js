9abdc7fa0f75ecdf819a9b8c9b4a9ae40fbc57d7
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-23 02:04:21 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,15 +1,18 @@
 var chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles'
-  ];
-
-function addElementToBeginningOfArray(element, array) {
+  "snicker",
+  "hundred grand",
+  "skittles"];
+  
+  function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
-}
+  
+  function destructivelyAddElementToBeginningOfArray(array,element){
+    array.unshift(element)
+    return array
+  }
+  
+  function addElementToEndOfArray(array, element) {
+    array.push(element)
+    return array
+  }
