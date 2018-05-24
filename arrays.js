4a372d1fe2d95bac19dc3fb42b302db94433d466
Rosa Tu <rosalina.tu@gmail.com>
2018-05-24 19:44:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,21 +1,5 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var  chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
- 
-var addElementToBeginningOfArray = [a]
 function addElementToBeginningOfArray(a, b) {
-  function()
-  {
-return `a.unshift(b)`}
-const a = [b, ...a]
-return [a] 
-  }
-  
-  
-  var = destructivelyAddElementToBeginningOfArray(a, b)
-  {
-      function()
-  {
-return [b,...a]}
-return [a] 
-   return  [b,...a]
+ return `a.unshift(b)`
   }
