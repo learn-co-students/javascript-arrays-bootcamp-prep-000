d198de59b6e33e6c19e1daa3298bdc29db25ca3d
Lige <lige.williamson@gmail.com>
2018-05-25 13:24:46 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray() {
+  var food = [
+    "pizza",
+    "broccoli",
+    "sandwich"
+  ]
+  var salad = "salad"
+  return foods = [salad,...food]
+}
+
+
+function destructivelyAddElementToBeginningOfArray() {
+  var cars = [
+    "truck",
+    "van"
+  var suv = "SUV"
+  return cars = [suv, ...cars]
+  ]
+}
