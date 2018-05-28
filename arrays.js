1d5600f247d43191f3431ef5b9853296818f5c2f
Jack Yeung <jyeung.se@gmail.com>
2018-05-28 21:56:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,6 +7,42 @@ var addElementToBeginningOfArray = function(array,element) {
 }
 
 var destructivelyAddElementToBeginningOfArray = function(array,element) {
-  array() = [array.unshift(element)];
+  array.unshift(element);
   return array;
 }
+
+var addElementToEndOfArray = function(array,element) {
+  var newArray = [...array,element];
+  return newArray;
+}
+
+var destructivelyAddElementToEndOfArray = function(array,element) {
+  array.push(element);
+  return array;
+}
+
+var accessElementInArray = function(array,index) {
+  return array[index];
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = function(array) {
+  array.shift();
+  return array;
+}
+
+var removeElementFromBeginningOfArray = function(array) {
+  var newArray = array.slice(1);
+  return newArray;
+}
+
+var destructivelyRemoveElementFromEndOfArray = function(array) {
+  array.pop();
+  return array;
+}
+
+var removeElementFromEndOfArray = function(array) {
+  var newArray = array.slice(0,array.length - 1);
+  return newArray;
+}
+
+
