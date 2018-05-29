c34e2262021e19ce33fe043e735eacdb2c8d2bf5
Lige <lige.williamson@gmail.com>
2018-05-29 12:56:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,22 +5,42 @@ var chocolateBars = [
   "skittles"
 ]
 
-function addElementToBeginningOfArray() {
-  var food = [
-    "pizza",
-    "broccoli",
-    "sandwich"
-  ]
-  var salad = "salad"
-  return foods = [salad,...food]
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array]
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
 
-function destructivelyAddElementToBeginningOfArray() {
-  var cars = [
-    "truck",
-    "van"
-  var suv = "SUV"
-  return cars = [suv, ...cars]
-  ]
+function addElementToEndOfArray(array,element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift(1)
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array = array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array = array.slice(0, array.length -1)
 }
