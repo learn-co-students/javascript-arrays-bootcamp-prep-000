04181620132094d53d824b85b6d510c726509a3e
Sterling Moss <mtsilvertm@gmail.com>
2018-05-30 18:28:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [1];
+  [...array, element];
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+  return array;
+}
+  
+  function addElementToEndOfArray(array, element) {
+  var array = [1];
+  [element, ...array,];
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  var array = [1, 2, 3, 4, 5]
+  return (array[index]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  var array = array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+ return array;
+}
+
+function removeElementFromEndOfArray(array) {
+
+  return array.slice(0, array.length - 1)
+}
