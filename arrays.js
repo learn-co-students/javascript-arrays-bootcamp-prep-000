8bbe64dec3946a50925f97fa2a1653dd54dba159
Shafi Khalisdar <shafi.khalisdar@gmail.com>
2018-05-30 01:46:20 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return(...array, element);
+  element.unshift();
 };
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+  return (element, ...array);
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
