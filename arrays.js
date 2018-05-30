ab09882cedfdeca291134cf63b5a077d24db687c
Sterling Moss <mtsilvertm@gmail.com>
2018-05-30 20:36:17 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   var array = [1];
   [...array, element];
+<<<<<<< HEAD
   return [element, ...array];
 }
 
@@ -47,3 +48,11 @@ function removeElementFromEndOfArray(array) {
 
   return array.slice(0, array.length - 1)
 }
+=======
+  return array;
+}
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+  return array;
+  }
+>>>>>>> 33ee8ab4cf9ad871a70477bf96860ccf33e7bd66
