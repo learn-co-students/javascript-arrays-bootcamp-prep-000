83cab310ef5cb2590c8adbe2b5b97d419acfcfe5
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-03 23:45:18 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+chocolateBars.unshift("welcome")
+
+/*addElementToBeginningOfArray(array, element){
+  return array.unshift()
+  //addElementToBeginningOfArray([1], 'foo'
+  
+}*/
+
