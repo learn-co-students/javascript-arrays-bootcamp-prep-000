bbc46f618297195a87c58386fe9ab49241c35083
Michael Carlos <michaelpcarlos@gmail.com>
2018-06-04 21:32:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -11,5 +11,41 @@ var chocolateBars =
   }
   
   function destructivelyAddElementToBeginningOfArray(array, someElement) {
-    return [someElement, array];
+    array.unshift(someElement);
+    return array;
   }
+  
+  function addElementToEndOfArray(array, someElement) {
+    return [...array, someElement];
+  }
+  
+  function destructivelyAddElementToEndOfArray(array, someElement) {
+    array.push(someElement);
+    return array;
+  }
+  
+  function accessElementInArray(array, index) {
+    return array[2];
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.shift();
+    return array;
+  }
+  
+  function removeElementFromBeginningOfArray(array) {
+    array.slice(1);
+    return array.slice(1);
+  }
+  
+  function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop();
+    return array;
+  }
+  
+  function removeElementFromEndOfArray(array) {
+    array.slice(0, array.length - 1);
+    return array.slice(0, array.length - 1);
+  }
+  
+  
