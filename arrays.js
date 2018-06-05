9bb3b6bbb2995330c4725520c1336723d21e051f
John Higgins <johnhiggins@outlook.ie>
2018-06-05 21:28:55 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+  var a  = [1]
+  var b = 'foo'
+  var c = []
+  c.concat(b,a) 
+}
