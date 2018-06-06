fe0e0ed8faf96778f5700aaa0fa7a0e26ecab9a9
heii70 <williamgao70@yahoo.com>
2018-06-06 07:20:20 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = [ 
+  "snickers", 
+  "hundred grand", 
+  "kitkat", 
+  "skittles" 
+];
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
