c4bbdf1a3754b3375d4378777ca28ed145a57cce
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 22:25:03 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,32 +1,32 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 function addElementToBeginningOfArray(array, element) {
-  var newarray = [element, ...array]
-  return newarray
+  return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 function addElementToEndOfArray(array, element) {
-  var newarray = [...array, element]
-  return newarray
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
+  return array
 }
 function removeElementFromBeginningOfArray(array) {
-  var newarray = array.slice(1)
-  return newarray
+  return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
 function removeElementFromEndOfArray(array) {
-  var newarray = array.slice(0, array.length - 1)
-  return newarray
+  return array.slice(0, array.length - 1)
 }
