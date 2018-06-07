edfc7cd849bd8fc0756974e55d5c2dd556479d3d
Tom De Vries <tdevries60@gmail.com>
2018-06-07 20:53:46 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,32 +1,45 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array,newElement) {
-  array.unshift(newElement)
-  print(array)
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array,newElement) {
-
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
 }
 
-function accessElementInArray(array,index) {
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
 
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
 
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
-  console.log(array)
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+
+  return array
 }
 
-function(array) {
-  array.slice(-1)
-  console.log(array);
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
