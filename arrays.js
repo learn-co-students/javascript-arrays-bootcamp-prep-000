22acbca50f323ae2a3471f532a7a29c5e722b186
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-07 20:02:51 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray (element, array) {}
-
+function addElementToBeginningOfArray (array, element) {}
+addElementToBeginningOfArray[1]
