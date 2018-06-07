1177ed7b62222a3ca35622c170676dc311e95dbc
Tom De Vries <tdevries60@gmail.com>
2018-06-07 20:52:17 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,newElement) {
+  array.unshift(newElement)
+  print(array)
+}
+
+function destructivelyAddElementToBeginningOfArray(array,newElement) {
+
+}
+
+function accessElementInArray(array,index) {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(0)
+  console.log(array)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+}
+
+function(array) {
+  array.slice(-1)
+  console.log(array);
+}
