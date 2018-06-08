f10d7fab68d1735c20a06938640d83189fe1733f
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-08 16:45:27 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  return [element, ...array];
+  array.unshift(element);
+  return array
 }
