07bdaa4a26849ff22cc042f1303e1e78ae00545d
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-10 00:07:26 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var array
+var addition
+function addElementToBeginningOfArray(array, addition) {
+  var temp = [addition,...array]
+return temp
+}
+
