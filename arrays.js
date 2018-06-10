ca82c39f1c02e88360efac8d14cf29c099559581
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-10 03:00:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var array
 var addition
 var index
+=======
+var chocolateBars ["snickers", "hundred grand", "kitkat", "skittles"];
+var array
+var addition
+>>>>>>> 023e14d20aacff96274d03abbba0a5399c175a27
 function addElementToBeginningOfArray(array, addition) {
   var temp = [addition,...array]
 return temp
 }
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(array, addition) {
   array.unshift(addition)
   return array
@@ -19,5 +26,5 @@ function destructivelyAddElementToEndOfArray(array, addition) {
   return array
 }
 function accessElementInArray(array, index) {
- return console.log(array[index]);
+ return array[index]
 }
