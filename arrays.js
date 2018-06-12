3433cccb91b58e5c617d8301a84815e3784d709b
Eric Shiels <ericshiels2@gmail.com>
2018-06-12 15:03:21 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBards = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  return array.push(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  return [array,... element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
