d646b45b087853fb46c97e389ad2c06800d7af49
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-12 15:34:32 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(Array,element) {
+  var newarray = [element, ...Array];
+  return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(Array,element) {
+  Array.unshift(element);
+  return Array
+}
+
+function addElementToEndOfArray(Array,element) {
+ var newarray = [...Array,element];
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(Array,element) {
+  Array.push(element);
+  return Array
+}
+
+function accessElementInArray(Array,index) {
+  console.log(Array[index])
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(Array){
+  Array.shift();
+  return Array
+}
+
+function removeElementFromBeginningOfArray (Array){
+  var newarray = Array.slice(1);
+  return newarray
+}
