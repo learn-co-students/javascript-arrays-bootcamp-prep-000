a022ade801c37dcf0d2f1b2938bfcfaddb6ab04e
Karen Pinilla <pinillakaren@yahoo.com>
2018-06-12 04:07:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,61 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  
+  array.unshift();
+  
+  return addElementToBeginningOfArray;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  
+  array = [element...array];
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  
+  array.push();
+  
+  return addElementToEndOfArray
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+  [element...array]
+
+}
+
+function accessElementInArray(array, index) {
+  
+  return(array[index]);
+  
+}
+
+function removeElementFromBeginningOfArray(array) {
+  
+  array.slice(0)
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+  array.push()
+
+}
+
+function removeElementFromEndOfArray(array) {
+  
+  array.slice()
+  
+}
+
+
+
+
+
+
+
+
