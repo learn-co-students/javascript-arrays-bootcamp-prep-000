6ae36145aa00fb405e45b261c3a14165adedcc1d
Jorlost19 <jorlost19@gmail.com>
2018-06-14 05:41:03 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let chocolateBars=['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  array=['Jorge'];
+  element=10;
+  array.unshift(element);
+  return array;
+}
