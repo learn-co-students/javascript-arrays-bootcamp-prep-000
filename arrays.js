2b5640ec515611a7a266a1083283aa1ad8ac69e1
John Higgins <johnhiggins@outlook.ie>
2018-06-16 12:06:10 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,8 +1,71 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
-  var a  = [1]
-  var b = 'foo'
-  var c = []
-  c.concat(b,a) 
+  var myArray = [1];
+  return ["foo", ...myArray];
+}  
+
+function destructivelyAddElementToBeginningOfArray (){
+  var myArray = [1];
+  x = 'foo'
+  myArray = [x, ...myArray];
+  return myArray
 }
+
+function addElementToEndOfArray (){
+  var myArray = [1];
+  return [...myArray,"foo"];
+}
+
+function destructivelyAddElementToEndOfArray (){
+  var myArray = [1];
+  myArray = [...myArray,"foo"];
+  return myArray
+}
+
+function accessElementInArray (){
+  myArray = [1,2,3]
+  return (myArray [2])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+   myArray = [1,2,3]
+   myArray.shift()
+  return myArray
+}
+
+function removeElementFromBeginningOfArray(){
+  myArray = [1,2,3]
+  return myArray.slice (1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+  myArray = [1,2,3]
+  myArray.pop()
+  return myArray
+}
+
+function removeElementFromEndOfArray(){
+   myArray = [1,2,3]
+   return myArray.slice (0, myArray.length - 1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
