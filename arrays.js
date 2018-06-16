556439d63d26e7f6f23adb3b1508be8b1a98a71c
Dylan <dylan@jumponweb.com.au>
2018-06-16 10:16:22 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "kittles"]
+function addElementToBeginningOfArray(imArray,element) {
+  return ("element",...imArray)
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(newArray,anotherElement) {
+  return newArray.unshift(anotherElement)
+}
