bb988bdf107c1ea789c2009881ec261c38c99a01
Amanda S <shottsan@dukes.jmu.edu>
2018-06-17 21:52:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var addElementToBeginningOfArray function(a, [b]) {
+    var pushed = function(a) {
+        array.push("input");
+        return
+
+    }
+}
+
+var destructivelyAddElementToBeginningOfArray function([array], element) {
+
+}
