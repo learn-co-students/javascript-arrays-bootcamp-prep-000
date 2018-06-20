7638e5cc5526e535bb12c02336b6c9e096390c06
Andrei Sevostianov <sevost.andrey@gmail.com>
2018-06-20 04:04:13 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (chocolateBars) {
+  Bars = chocolateBars.unshift("foo")
+  return Bars
+}
