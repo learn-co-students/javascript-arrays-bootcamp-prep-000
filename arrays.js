04db34e7b9fdbc70cf59dca882065ee1db2bb2bd
Jacob Boodry <jake.boodry@live.com>
2018-06-20 23:37:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", " hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a,e){
+  return [e, ...a]
+}
+function destructivelyAddElementToBeginningOfArray(a,e){
+  a.unshift(e)
+  return a
+}
+function addElementToEndOfArray(a,e){
+  return [...a, e]
+}
+function destructivelyAddElementToEndOfArray(a,e){
+  a.push(e)
+  return a
+}
+function accessElementInArray(a,i){
+  return a[i]
+}
+function destructivelyRemoveElementFromBeginningOfArray(a){
+  a.shift()
+  return a
+}
+function removeElementFromBeginningOfArray(a){
+ var A = a.slice(1)
+ return A
+}
+function destructivelyRemoveElementFromEndOfArray(a){
+  a.pop()
+  return a
+}
+function removeElementFromEndOfArray(a){
+  var A = a.slice(0, a.length - 1)
+  return A
+ }
+ 
+ // YEET
