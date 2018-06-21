25d53b00925189755342b9cf60ab393b2892377e
Susann Weissova <suzi.weissova@gmail.com>
2018-06-21 06:47:56 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; 
+
+function addElementToBeginningofArray([], foo ){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
