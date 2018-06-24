b07f0c330ff30c37b93749343ce3b8f1b2c391ca
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-24 18:24:38 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
