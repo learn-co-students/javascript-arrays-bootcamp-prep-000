6a9fc03b78fcbc6051c4a97ce887238caef7d608
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-24 21:05:58 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,3 +7,22 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
