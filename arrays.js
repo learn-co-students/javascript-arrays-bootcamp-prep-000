bbafac6dbc09c161faa00c551efecbcc553c0a29
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-26 03:04:38 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,14 +10,45 @@ var grilledCheeseIngredients = [ "bread", "mild cheese", "sharp cheese", "butter
 var tomatoSauceIngredientes = ["tomato", "garlic", "olive oil", "basil", "organo"]
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var propio1 = [1, 2, 3, 4, 5]
-var foo = ["uno", "dos", "tres"]
-
-var addElementToBeginningOfArray = function(array, element){
- propio1 = [foo, 1, ... propio1]
-  return propio1
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var candyString = "foo"
+ 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-var destructivelyAddElementToBeginningOfArray = function(){
-  propio3 = [10, 40]
-  return propio3
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
 }
