ef7ddb13958959ab29f395f96d1e869b25eb5a52
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-26 03:00:34 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [ "bread", "mild cheese", "sharp cheese", "butter", "tomato", "garlic"]
+
+var tomatoSauceIngredientes = ["tomato", "garlic", "olive oil", "basil", "organo"]
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var propio1 = [1, 2, 3, 4, 5]
+var foo = ["uno", "dos", "tres"]
+
+var addElementToBeginningOfArray = function(array, element){
+ propio1 = [foo, 1, ... propio1]
+  return propio1
+}
+var destructivelyAddElementToBeginningOfArray = function(){
+  propio3 = [10, 40]
+  return propio3
+}
