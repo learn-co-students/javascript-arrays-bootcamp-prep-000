2079126daf8df0d92689a104f42d0b3eabde8691
Sean Wu <seanwu20@gmail.com>
2018-06-28 23:12:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -11,12 +11,37 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array2 =[array,...element]
+  array2 = [...array,element]
   return array2
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array2 = array.slice(1)
+  return array2
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array2 = array.slice(0, array.length - 1)
+  return array2
 }
 
 
