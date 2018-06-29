ae46fde2335dcfb2ea24d140bf614fbe1f3c4e59
masontang@aol.com <masontang@aol.com>
2018-06-29 15:17:24 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+addElementToBeginningOfArray(chocolateBars, "hershey")
