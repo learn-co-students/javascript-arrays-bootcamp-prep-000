920a98f64d72a2a825179b7864193f2b98d0bb31
jvpink200 <jvillarr20@gmail.com>
2018-06-29 01:19:13 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,3 +3,11 @@ var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 function addElementToBeginningOfArray(){
 
 }
+
+function addElementToEndOfArray(){
+
+}
+
+function destructivelyAddElementToEndOfArray(){
+
+}
