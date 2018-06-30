af8a8bec86ae56dededfc00554d85e45c2a190a1
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-06-30 21:15:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = [
+  "snickers"
+  "hundred grand"
+  "kitkat"
+  "skittles"
+]
+
+function addElementToBeginningOfArray([array], "element") {
+  return array.unshift("element")
+  constant array = [array]
+}
+
+function destructivelyAddElementToBeginningOfArray([array],"element") {
+  return array.unshift("element")
+}
+
+function addElementToEndOfArray([array], 'element') {
+  return array.push('element')
+  const array = [array]
+}
+
+function destructivelyAddElementToEndOfArray([array], 'element') {
+  return array.push('element')
+}
