bcf39f12c2bda10b3d9bad6af962e6c739cfe4ce
Cheryl Kelson <cherylkelson@icloud.com>
2018-06-30 17:10:54 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,9 +1,34 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-const addElementToEndOfArray = (array, element);
-  addElementtoEndOfArray.push ();
-  
-const destructivelyAddElementToEndOfArray = (array, element);
-  destructivelyAddElementToEndOfArray.push ();
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']; 
+ function addElementToBeginningOfArray (array, element) {
+  return {element,...array];
+  }
+ function destructivelyAddElementToBeginninfOfArray (array, element){
+   array.unshift(element);
+   return array;
+   
+ function addElementToEndfArray(array, element){
+   return [...array, element];
+   function destructively AddElementToEndOfArray(array, element) {
+     array.push(element);
+     return array;
+     accessElementInArray(array,index){
+       return [index];
+      function destructivelyRemoveElementFromBeginningOfArray(array){
+        array.shift();
+        return array;
+        function removeElementFromBeginningOfArray(array){
+          return array.shift;
+   }
+   function destructivelyRemoveElementFromEndOfArray(array){
+     return array.slice(1);
+     function destructivelyRemoveElementFromEndOfArray(array)
+     {
+       array.pop();
+       return array;
+     }
+   }
+
+
     
 
 
