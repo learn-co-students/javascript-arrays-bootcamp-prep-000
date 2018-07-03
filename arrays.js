94d62e4d0c2cf2022c840869c606c731bfacc1d9
Tesalon Felicien <felicien.tesalon@gmail.com>
2018-07-03 00:48:28 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,20 @@
+
+var chocolateBars =["snickers","hundred grand","kitkat","skittles"]
+
+var cake =["chocolate","vanilla","marshmellow","lemon"]
+function addElementToBeginningOfArray(a,b){
+console.log( a =[b, ...a])
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){ 
+console.log(a.unshift(b));
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a){
+console.log(a.shift());
+} 
+
+function accessElementInArray(a,b){
+  
+}
+
