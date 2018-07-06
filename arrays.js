e2bd5797d5b75d20b09317248feedc48725994c4
jvpink200 <jvillarr20@gmail.com>
2018-07-06 04:22:44 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,16 +1,46 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
-function addElementToBeginningOfArray(array,element){
+function addElementToBeginningOfArray(array, element){
   var new_array = [element,array[0]];
   return new_array;
 }
 
-function destructivelyAddElementToBeginningOfArray(array,element){
+function addElementToEndOfArray(array,element){
+    var new_array = [element, array[3]];
+    return new_array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
     array.unshift(element);
     return array;
 }
 
-function addElementToEndOfArray(array,element){
-    var new_array = [element,array[3]];
+function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element);
+    return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function accessElementInArray(array, index){
+  var new_array = array[index];
+  return new_array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+    var new_array = array.slice(0, array.length -1);
     return new_array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var new_array = array.slice(1);
+  return new_array;
 }
