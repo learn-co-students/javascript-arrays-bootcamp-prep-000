b5cd8f25b0acd93852f007193480c3266e8024a1
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-08 23:01:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -20,3 +20,25 @@ function destructivelyAddElementToEndOfArray (array, element){
   array.push(element);
   return array
 }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.shift(0)
+  return array
+}
+
+function removeElementFromBeginningOfArray (array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop(-1)
+  return array
+}
+
+function removeElementFromEndOfArray (array){
+  return array.slice(0, array.length- 1)
+}
