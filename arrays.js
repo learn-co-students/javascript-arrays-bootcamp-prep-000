f352aad39e4e77695a7cf6dcfe4f9f757ace5526
Jesi Loredo <jesi.thegreat@gmail.com>
2018-07-10 03:54:57 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var foo = [2, 3]
+
+function addElementToBeginningOfArray (foo) {
+
+foo.unshift(1)
+  return foo
+}
+
+function destructivelyAddElementToBeginningOfArray () {
+  
+}
+
+
+function addElementToEndOfArray () {
+  
+}
+
+function destructivelyAddElementToEndOfArray () {
+  
+}
+
+
+function accessElementInArray () {
+  
+}
