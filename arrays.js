31f86cf9a1f8cb18f6568a7d13ae5e0a7b736d22
Tiffany Choong <t.choong@yahoo.com>
2018-07-16 22:24:05 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,10 +2,11 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var arraytest1 = [1];
 var arraytest2 = [1];
+var arraytest3 = [1];
 
 
 function addElementToBeginningOfArray(array, element){
-  return ["foo",...arraytest1]
+  return ["foo",...arraytest1];
 }
 
 function destructivelyAddElementToBeginningOfArray(arraytest2, unshift){
@@ -13,3 +14,11 @@ function destructivelyAddElementToBeginningOfArray(arraytest2, unshift){
  return arraytest2;
 }
 
+function addElementToEndOfArray(array, element){
+  return [...arraytest1, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(arraytest3, push){
+  arraytest3.push("foo");
+  return arraytest3;
+}
