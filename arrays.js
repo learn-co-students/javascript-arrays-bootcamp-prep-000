1f0d99273151be1666c5faafd173f920f88007a7
elaitman1 <elaitman1@gmail.com>
2018-07-17 01:21:54 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+function addElementToBeginningofArray(element, array){
+  array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) { 
+  array.unshift(element);
+}
