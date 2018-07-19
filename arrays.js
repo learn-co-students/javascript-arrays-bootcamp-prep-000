6c5d43a0689a8897d3e626eeec46d086ae5a8819
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-19 01:20:33 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,16 +1,16 @@
 var chocolateBars = ['snickers', 'hundred grand','kitkat', 'skittles'];
 function addElementToBeginningOfArray(a, b) { return [ b, ...a ] }
-function destructivelyAddElementToBeginningOfArray(a, b) { return a.unshift(b) }
-function addElementToEndOfArray(a, b) { return [ b, ...a ] }
-function destructivelyAddElementToEndOfArray(a, b) { return a.push(b) }
+function destructivelyAddElementToBeginningOfArray(a, b) { a.unshift(b); return a }
+function addElementToEndOfArray(a, b) { return [...a, b ] }
+function destructivelyAddElementToEndOfArray(a, b) { a.push(b); return a }
 function accessElementInArray(a, b) { return a[b] }
-function destructivelyRemoveElementFromBeginningOfArray(a, b) { return a.shift(b) }
-function removeElementFromBegnningOfArray(a) { return a.slice(1) }
-function destructivelyRemoveElementFromEndOfArray(a) { return a.pop() }
-function removeElementFromEndOfArray(a) { return a.slice(0, a.length, -1) }
+function destructivelyRemoveElementFromBeginningOfArray(a, b) { a.shift(b); return a }
+function removeElementFromBeginningOfArray(a) { return a.slice(1) }
+function destructivelyRemoveElementFromEndOfArray(a) { a.pop(); return a }
+function removeElementFromEndOfArray(a) { return a.slice(0, a.length - 1) }
 
-destructivelyAddElementToBeginningOfArray(chocolateBars, 'starburst');
 addElementToBeginningOfArray(chocolateBars, 'm&ms');
+destructivelyAddElementToBeginningOfArray(chocolateBars, 'starburst');
 addElementToEndOfArray(chocolateBars, 'milky way');
 destructivelyAddElementToEndOfArray(chocolateBars, 'mamba');
 accessElementInArray(chocolateBars, 0);
