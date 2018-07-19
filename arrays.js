508ec596a0dbba49b9e2d613e89debbb189d31fa
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-19 01:24:17 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function accessElementInArray(a, b) { return a[b] }
 function destructivelyRemoveElementFromBeginningOfArray(a, b) { a.shift(b); return a }
 function removeElementFromBeginningOfArray(a) { return a.slice(1) }
 function destructivelyRemoveElementFromEndOfArray(a) { a.pop(); return a }
-function removeElementFromEndOfArray(a) { return a.slice(0, a.length - 1) }
+function removeElementFromEndOfArray(a) { return a.slice(0, a.length) }
 
 addElementToBeginningOfArray(chocolateBars, 'm&ms');
 destructivelyAddElementToBeginningOfArray(chocolateBars, 'starburst');
