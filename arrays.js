7bd17ba3e0325b52c7f86ecd28df37ab39c4b027
Hanna Strezh <hannastrezh@gmail.com>
2018-07-23 23:46:49 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,50 +1,37 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+  return[element,...array];
 }
+// element goes first because we want to add the element to the //beginning of the array
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
-/*
-}
-//function destructivelyAddElementToBeginningOfArray(array, element){
- array.unshift(element);
- return array;
-}*/
-
 function addElementToEndOfArray(array, element){
-  return [...array, element];
+  return[...array, element];
+  
 }
-
 function destructivelyAddElementToEndOfArray(array, element){
- array.push(element);
+  array.push(element);
   return array;
 }
-
-function accessElementInArray(array, index){
-  return array[index];
+function accessElementInArray(array,index){
+  return array [index];
 }
-
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array;
   
 }
 function removeElementFromBeginningOfArray(array){
-  let firstVal = array.shift(); //modif the array 
-  let result = [...array];// used it to create new array
- array.unshift(firstVal); // reverse modification
- return result;// return the new created array
-}
-function removeElementFromEndOfArray(array){
-  let firstVal = array.pop(); //modif the array 
-  let result = [...array];// used it to create new array
- array.push(firstVal); // reverse modification
- return result;
+ array = array.slice(1);
+  return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
-
+function removeElementFromEndOfArray(array){
+  array = array.slice(0, array.length-1);
+  return array;
+}
