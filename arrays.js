0f3713da8998894cc3660d29a7c0b8c2b0f26c2f
charlesokwechime@gmail.com Okwechime <charlesokwechime@gmail.com>
2018-07-24 22:55:27 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [array, ...elements];
+}
+
+ function destructivelyAddElementToBeginningOfArray(arr, element) {
+    return array.unshift(element);
+  
+ }
+
