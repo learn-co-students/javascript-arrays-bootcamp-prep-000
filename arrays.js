b78e01ff74c0997dd9eedd04a84e3726ad512efb
Daniel Schoch <d.schoch90@gmail.com>
2018-07-26 23:03:12 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -31,3 +31,11 @@ function removeElementFromBeginningOfArray (array) {
   return array.slice(1);
 }
 
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length - 1);
+}
