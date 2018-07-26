c80f972ec79d52f7b43403aedf2f51ef064e883e
Amir Safoev <amirsafoev0@gmail.com>
2018-07-26 19:52:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element){
 
-["element", ...array];
+function addElementToBeginningOfArray(array, element){
+return   [element, ...array];
 }
-function destructivelyAddElementToBeginningOfArray(){
-  var array = [];
-  
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+array.unshift(element) ;
+return array; 
 }
