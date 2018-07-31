df00c1608e8e219bf14f045e37978059f7f046f6
Brenda  Almonte <breal0811@gmail.com>
2018-07-31 18:46:21 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,15 +6,13 @@ function addElementToBeginningOfArray() {
 }
 //GOOD ABOVE 
 
-
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);	  
   return array;	
-  
 }	
  function addElementToEndOfArray(array, element)  {
   return [...array, element];	 
- 	}
+}
  	
 function destructivelyAddElementToEndOfArray(array, element) {	
   array.push(element);
@@ -22,28 +20,32 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 //GOOD ABOVE
 
+function accessElementInArray(array, index) {
+  return array[index];
+}
+//GOOD ABOVE
 
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift(1);
+  return array;
+}
+//GOOD ABOVE
 
+function removeElementFromBeginningOfArray(array) {
+ return array.slice(1);
+}
+//GOOD ABOVE
 
-function accessElementInArray() {
-  
+function destructivelyRemoveElementFromEndOfArray(array) {
+ array.pop();
+ return array;
+}
+//GOOD ABOVE
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-  
-}
-
-function removeElementFromBeginningOfArray() {
-  
-}
-
-function destructivelyRemoveElementFromEndOfArray() {
-  
-}
-
-function removeElementFromEndOfArray() {
-  
-}
 
 
 
