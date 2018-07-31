360f4b51327e274620ea28194983a17b07b42aa1
Hank Liu <hanke.liu@gmail.com>
2018-07-31 20:17:47 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -38,8 +38,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array;
+  return array.pop();
+ 
 }
 
 function removeElementFromEndOfArray(array) {
