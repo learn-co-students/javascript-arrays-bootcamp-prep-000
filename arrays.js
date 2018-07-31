7be552ae7a08c3e00f63460c18b56d59507dec94
Hank Liu <hanke.liu@gmail.com>
2018-07-31 20:11:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
-var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, and `skittles`]; 
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]; 
 
 var array=[];
 
 function addElementToBeginningOfArray(array, element) { 
   var newArray = [element, ...array];   //spread operator
-  return newArray
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
@@ -14,11 +14,38 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 function addElementToEndOfArray(array, element) {
   var newArray = [...array, element];
-  return newArray
+  return newArray;
 }
 
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push[element];  //push operator
+  array.push(element);  //push operator
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, index) {
+  array.shift();  //destructive method
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1); //slice method
+  return newArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0, array.length-1);
+  return newArray;
+}
+
+
+
