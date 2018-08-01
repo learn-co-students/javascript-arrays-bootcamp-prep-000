e7e9325b02ee4b534652a78ddb4916f79faa31f1
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-08-01 13:09:58 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+/*var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"*/
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var cities = ["New York" , "San Francisco"];
+//["philadelphia". ...cities];
+/*cities = ["Philadelphia", ...cities];
+const cats = ["Milo" , "Garfield"];
+const moreCats = ["Felix" , ...cats];*/
+function addElementToBeginningOfArray (array, element){
+  return [element,...array];
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array,index){
+  return array[2];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop();
+  return array
+}
+function removeElementFromEndOfArray (array){
+  return array.slice(0, array.length - 1);
+}
