1ba005d1881296688ae415c06c20189e16787928
ajay <ajay.kumar@unicodesystems.org>
2018-08-01 11:52:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers", "hundredgrand", "kitkat", "skittles"];
+ var a =1;
+function addElementToBeginningOfArray(a, foo){
+  
+  const v = ['a', 'foo' ]
+  
+  const c = ['foo', ...v ]
+  
+}
+
+function  destructivelyAddElementToBeginningOfArray(d,b){
+  
+  
+}
