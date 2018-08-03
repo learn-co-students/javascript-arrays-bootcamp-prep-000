90abda5af426c96e8e786abbdcf0d65dcc047099
Phat Doan <phatdoan@yahoo.com>
2018-08-03 21:25:14 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -35,6 +35,7 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
