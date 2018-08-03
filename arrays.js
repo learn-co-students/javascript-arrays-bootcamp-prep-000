8ac4d24a82523079fe27d69def0b2f27064239f7
Phat Doan <phatdoan@yahoo.com>
2018-08-03 21:22:25 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element);
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  return element = [index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
