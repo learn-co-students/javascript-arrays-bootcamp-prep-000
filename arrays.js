56192f07412374803e05798ba89265d4d0742f30
Michael Barbone <michael.barbone@gmail.com>
2018-08-03 21:37:19 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,47 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var candyString='foo';
 
-function addElementToBeginningOfArray (list, 'item') {
-  list.unshift ('item');
+function addElementToBeginningOfArray (chocolateBars, candyString) {
+  return [candyString, ...candyBars];
 }
+
+function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray (chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray (chocolateBars, candyString) {
+  chocolateBars.push (candyString);
+  return chocolateBars;
+}
+
+function accessElementInArray (chocolateBars, index) {
+  return chocolateBars([2]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars=chocolateBars.slice(1);
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
+  chocolateBars.pop();
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray (chocolateBars) {
+  chocolateBars.slice(0, chocolateBars.length-1);
+  return chocolateBars;
+}
+
+
+
