0c7aeb5c26fe0f4777d7131e2d9411884ee97e34
Michael Barbone <michael.barbone@gmail.com>
2018-08-03 20:11:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray ()
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
