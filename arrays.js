0a3491e20c2238592dc2a99cccc9e85357f07ad3
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-05 18:30:20 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return array.unshift(element)
 }
 /*
 function addElementToEndOfArray(array, element) {
