0635e776d02ea71fccfa7b50c58326422324819d
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-05 18:20:46 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var chocolateBars = [
   ]
  
 function addElementToBeginningOfArray(array, element) {
-  var newArray = array.unshift(element)
+  var newArray = [element, ...array]
   return newArray
 }
 /*
