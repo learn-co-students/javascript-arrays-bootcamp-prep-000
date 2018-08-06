36c0f4187d4e397b95ed5e87612592c15765eb31
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-06 06:39:27 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,36 +10,36 @@ function addElementToBeginningOfArray(array, element) {
   const newArray = [element, ...array]
   return newArray
 }
-
+/*
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element)
 }
-
+ */
 function addElementToEndOfArray(array, element) {
   const newArray = [...array, element]
   return newArray
 }
-
+/*
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
 } 
-
+ */
 function accessElementInArray(array, index) {
   return array[index]
 }
-
+/*
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.unshift()
 }
-
+ */
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
-
+/*
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.push(-1)
 }
-
+ */
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
