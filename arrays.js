8747a3ae4f569d41131a1907a36872beaef5218d
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-06 17:27:27 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.unshift
+  return array.unshift()
 }
 
 function removeElementFromBeginningOfArray(array) {
