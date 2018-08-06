0034ad9de8e4c9b801746cd5c6b5065f081fa9d1
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-06 19:48:10 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -25,9 +25,9 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = array.push(element)
 //  return array.push(element)
-//  array = [...array, element]
+//  array = array.push(element)
+  array = [...array, element]
   return array
 } 
 
