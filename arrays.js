d387b539a5ee59dd286d011e310991938487ef83
shanicunningham@aol.com <shanicunningham@aol.com>
2018-08-07 18:05:42 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(a,b) {
+  var c = [b, ...a]
+  return c
+}
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  a.unshift(b)
+  return a
+}
+//failed
+function addElementToEndOfArray(a,b) {
+  var c = [...a,b]
+  return c
+}
+function destructivelyAddElementToEndOfArray(a,b) {
+  a.push(b)
+  return a
+}
+function accessElementInArray(a,b) {
+  return a[b]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a) {
+  a.shift()
+  return a
+}
+function removeElementFromBeginningOfArray(a) {
+  var c = a.slice(1)
+  return c
+}
+
+function destructivelyRemoveElementFromEndOfArray(a) {
+  a.pop()
+  return a
+}
+function removeElementFromEndOfArray(a) {
+  var c = a.slice(0, a.length -1)
+  return c
+}
