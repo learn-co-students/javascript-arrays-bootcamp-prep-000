12fe1d8ad42c3262e1fbad4a5e36de774bc613a6
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-08 01:00:26 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function removeElementFromBeginningOfArray(array) {
  */
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
+  array.slice(array.length)
   return array
 }
 
