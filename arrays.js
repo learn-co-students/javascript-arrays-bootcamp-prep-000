6565f1b5ca785c675559a21a3898068637d6299e
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-08 01:37:07 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,59 +1,62 @@
 // My arrays.js
-var chocolateBars = [
+
+
+var chocolateBars =[
   'snickers',
   'hundred grand',
   'kitkat',
   'skittles'
   ]
-/*
+
 function addElementToBeginningOfArray(array, element) {
   const newArray = [element, ...array]
   return newArray
 }
- */
-/* passed
+
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
- */
-/*
+
+
 function addElementToEndOfArray(array, element) {
   const newArray = [...array, element]
   return newArray
 }
- */
+
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 } 
 
-/*
+
 function accessElementInArray(array, index) {
   return array[index]
 }
- */
-/* passed
+
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
- */
-/*
+
+
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1, array.length)
 }
- */
-/* passes
+
+
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
- */
-/*
+
+
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
- */
+
+
 // End my arrays.js
