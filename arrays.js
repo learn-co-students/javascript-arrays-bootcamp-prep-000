92cdae64bea518fcc124929a01010b69bc0810ff
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-12 19:03:19 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -7,11 +7,31 @@ function addElementToBeginningOfArray(array,element){
   // return  newArray
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-  return array.unshift(element)
+ array.unshift(element)
+ return array
 }
 function addElementToEndOfArray(array,element){
   return [...array,element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
+}
+function accessElementInArray(array,index) {
+  var newArray = array;
+  return newArray[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+ return array.slice(1, array.length)
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,array.length-1)
 }
