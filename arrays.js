5a4da00f3da028f06cbfb0b3e8b7beac2755ce02
rayzafir@aol.com <rayzafir@aol.com>
2018-08-12 21:05:45 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars=["snickers"," hundred grand", "kitkat", "skittles"]{
+}
+var addElementTobeginningOfArray= ["1","2","3"]{
+addElementTobeginningOfArray.unshift("array","element")
+}
