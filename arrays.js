8beaa4d082a1b66e2bec53f778d1fa284fcd6252
Fathima1997 <fathimareeza9@yahoo.com>
2018-08-12 04:31:41 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,52 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittle" ]
+  
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice()
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
