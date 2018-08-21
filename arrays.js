dd0d269584e8982dcd20297dc4ad56735f6551b5
jonroger3@gmail.com <jonroger3@gmail.com>
2018-08-21 19:06:31 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array];
+  array = [element, ...array];
   return array;
   
 }
