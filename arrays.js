51fb43894ce5ab1478a5ee4b42fa893d6a3ecad4
Dallas <dallasbille@gmail.com>
2018-08-25 02:13:53 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,8 +2,9 @@
 var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,element){
+  ...array,element 
+ }
  
-}
 function destructivelyAddElementToBeginningOfArray(array,element){
   
 }
