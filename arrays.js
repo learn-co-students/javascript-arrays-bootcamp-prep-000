77fe5e11aa7c715858e71c341953033193c65758
demian sims <demiansims@gmail.com>
2018-08-28 00:14:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+  }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+ array.unshift(element);
+ return array; 
+}
+function addElementToBeginningOfArray(array, element) {
+  return [...array, element]; 
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
