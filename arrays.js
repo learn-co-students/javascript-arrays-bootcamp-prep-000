296e5d4a86ba65bbf0ff46f8791e341e31515ab5
demian sims <demiansims@gmail.com>
2018-08-28 02:07:49 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -6,18 +6,32 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
  array.unshift(element);
  return array; 
 }
-function addElementToBeginningOfArray(array, element) {
+function addElementToEndOfArray(array, element) {
   return [...array, element]; 
 }
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
 }
-  
-
-  
-
-
+function accessElementInArray(array, index) {
+  return array[index]
+} 
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element);
+  return array 
+}
+function removeElementFromBeginningOfArray(array, element) {
+  arrays = array.slice(1);
+  return arrays
+}
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop(element);
+  return array 
+}
+function removeElementFromEndOfArray(array, element) {
+  arrays = array.slice(0, array.length - 1);
+  return arrays
+}
 
 
 
