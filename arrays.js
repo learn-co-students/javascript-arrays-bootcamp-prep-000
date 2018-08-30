6f9db0e6c766f7342efdbcb1e7289fb40ce49a5f
Alisa McCormick <muntananuchat@gmail.com>
2018-08-30 16:54:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  chocolateBars
+  ["Hersey", ...chocolateBars]
+  return chocolateBars
+}
