3360885148903cefefbabf8405b57ac94ccf094c
Taylor Watson <taylorwatsonb@gmail.com>
2018-09-02 19:36:17 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,36 +1,41 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array,element) {
-  return[element,...array];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
-  return array 
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop(2)
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
 
 function accessElementInArray(array, index) {
   return array[index]
-}
-
-function accessElementToEndArray(array, element) {
-  return [...array, element]
-}
-
-function destrctivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
-  return array
-}
- function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
-}
- function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
-}
- function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length - 1)
-}
-function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
 }
