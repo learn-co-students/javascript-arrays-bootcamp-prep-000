58b973e1a6f77d20337f91a8f7824bb4ed587d83
Dorothy Davenport <dorothympdx@gmail.com>
2018-09-12 03:15:56 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  [element, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  [...array, element];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[0];
+}
