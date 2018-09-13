a3d3b037d51edc9507adeb7b3bbdc9fcb3c3ba41
Virginia Dooley <vcdooley@gmail.com>
2018-09-13 15:15:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -8,3 +8,49 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element);
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+   array.unshift(element);
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
