6f2e37e9911be7a8d8cf34b6b0ab8a5cab8bdc61
Virginia Dooley <vcdooley@gmail.com>
2018-09-13 13:08:40 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
-var candyBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray () {
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array]
 }
 
-function destructivelyAddElementToBeginningOfArray () {
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
 }
