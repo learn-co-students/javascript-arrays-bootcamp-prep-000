6a69586545aff1d836c0b704850cc32c79c52896
Joshua Chinyama <chinyamajosh@gmail.com>
2018-09-13 11:06:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -13,4 +13,30 @@ function  destructivelyAddElementToBeginningOfArray(array) {
   return [array.unshift('foo')];
 }
 
-  
+function addElementToEndOfArray(array) {
+  return [...array, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(array) {
+  return [array.push('foo')];
+}
+
+function accessElementInArray(array) {
+  return (array[2]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
+
+ function removeElementFromBeginningOfArray(array) { 
+   return array.slice(1)
+ }
+
+ function destructivelyRemoveElementFromEndOfArray(array) {
+   return array.pop()
+ }
+ 
+ function removeElementFromEndOfArray(array) {
+   return array.slice(0, array.length - 1)
+ }
