6655cc10f1c31b09eb8438307ad1c7d9c5030c80
Jeff Carter <jeff@pepperfly.com>
2018-09-14 14:39:20 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,48 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = array;
-  return newArray.unshift(element);
+  newArray = Array.from(array);
+  newArray.unshift(element);
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  newArray = Array.from(array);
+  newArray.push(element);
+  return newArray;
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+  
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
