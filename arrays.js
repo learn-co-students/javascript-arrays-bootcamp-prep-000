a89a91e38430818d938461b4dd2ab323135a5596
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2018-09-13 20:42:41 -0400

    Done.

---
+++ arrays.js
@@ -0,0 +1,54 @@
+var chocolateBars = [
+    "snickers",
+    "hundred grand",
+    "kitkat",
+    "skittles"
+]
+
+function addElementToBeginningOfArray(array, element){
+    var newArray = array.slice()
+    newArray.unshift(element)
+    return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+    array.unshift(element)
+    return array
+}
+
+function addElementToEndOfArray(array, element){
+    var newArray = array.slice()
+    newArray.push(element)
+    return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element)
+    return array
+}
+
+function accessElementInArray(array, index){
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift()
+    return array
+}
+
+function removeElementFromBeginningOfArray(array){
+    var newArray = array.slice()
+    newArray.shift()
+    return newArray
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+    array.pop()
+    return array
+}
+
+function removeElementFromEndOfArray(array){
+    var newArray = array.slice()
+    newArray.pop()
+    return newArray
+}
