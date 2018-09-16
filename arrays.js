38149fb59df57a4a7d87d0f3a9ed4a55fd860970
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-16 17:36:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,4 +4,17 @@ function addElementToBeginningOfArray (array, element) {
   return [element, ...array]
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element){
+ array.unshift(element)
+  return [element, array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return [array, ...element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return [array, element]
+}
 
