7ec633926fb160779e0035dcb17c222bccd2f181
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-16 15:21:10 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,11 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 function addElementToBeginningOfArray (array, element) {
   return [element, ...array]
 }
 
 
-function myName(name) {
-  return `Hello ${name}`
-}
-
-What kind of data am I working with?
