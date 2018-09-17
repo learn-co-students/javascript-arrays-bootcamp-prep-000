a25da11d42724063d79f6e2d797f3752c5ce8332
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-17 21:07:00 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return [array.slice()]
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
