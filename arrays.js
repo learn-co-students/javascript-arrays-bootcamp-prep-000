9a8d8ea116eaf7af2935a5ae3c34b41eb007b798
Brian <briandsalemi@gmail.com>
2018-09-18 01:19:09 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -27,3 +27,18 @@ function accessElementInArray(array, index) {
     return array[index];
   }
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array; 
+}
