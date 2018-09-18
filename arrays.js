8869d96d20f5df4fe05be39bba49cdacc37206a6
Austin Puckett <puckett_austin@yahoo.com>
2018-09-18 05:17:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, elem) {
+  // var new_arr = [elem, arr[0]];
+  // return new_arr;
+  return [elem, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr.unshift(elem);
+  return arr;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
+
