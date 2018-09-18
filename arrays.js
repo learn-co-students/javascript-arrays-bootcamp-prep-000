c1f4d9ca843abb1cc5c3fc967095bc5c48f215c6
Brian <briandsalemi@gmail.com>
2018-09-18 01:43:40 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -42,3 +42,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array; 
 }
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1);
+  return array;
+}
