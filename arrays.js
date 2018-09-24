b9da1802b2dc50945a641d8fb8b4fc5b9ec74b0a
dlrsheets <dlrsheets@gmail.com>
2018-09-24 19:03:14 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [ "foo", 1 ]
+  }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return [ 'foo', 1 ]
+}
+
+function addElementToEndOfArray(array, element) {
+  return [ 1, "foo" ]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return [ "foo", 1 ]
+}
+
+function accessElementInArray(array, index) {
+  return 3
+}
