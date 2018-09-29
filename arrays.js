f8b1e0306db9c499e7f13d5f78da4024e54a0fb4
Mariel Westervelt <elle.westervelt@gmail.com>
2018-09-29 19:16:51 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array,element) {
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift('array','element')
+  return 'array'
+}
+
+function addElementToEndOfArray(array,element) {
+
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+
+}
+
+function accessElementInArray(array,index) {
+  return (array[index])
+}
