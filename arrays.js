677ec580d44ac7197b855c833cad14d4b02d9a59
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-01 17:18:34 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,31 +2,35 @@ var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 
 function addElementToBeginningOfArray(array, element) {
 array = [element,...array];
+return(array);
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return(array);
 }
 
 function addElementToEndOfArray(array, element) {
   array = [array,...element];
-  array;
+ array;
 }
 
-function destrctivelyAddElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
 }
 
 function accessElementInArray(array, index) {
-  console.log(array, index);
+  console.log(array[index]);
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
+  return(array);
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array.slice(-1);
+  return(array);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
