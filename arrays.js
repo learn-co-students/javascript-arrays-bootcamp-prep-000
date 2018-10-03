292fd493cd03816861b10db77c375dabef8d662a
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-03 01:46:28 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,43 +1,56 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(fruits, otherFruits) {
- 
-return [otherFruits,...fruits]
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(arr, el) { 
-  arr.unshift(el)
-  return arr
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element); 
+  return array;
 }
 
-function addElementToEndOfArray(arr, el) {
-  return [...arr, el]  
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(arr, el) {
-  arr.push(el)
-  return arr
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
 }
 
-function accessElementInArray(arr, ind) {
-  return arr[ind]
+function accessElementInArray(array, index) {
+ return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(arr) {
- arr.shift()
- return arr
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 }
 
-function removeElementFromBeginningOfArray(arr) {
-  return arr.slice(1)
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
 
-function destructivelyRemoveElementFromEndOfArray(arr) {
-  arr.pop()
-  return arr
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 }
 
-function removeElementFromEndOfArray(arr) {
- return arr.slice(0, arr.length - 1)
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
 
+/*
+describe('removeElementFromEndOfArray(array)', () => {
+    it('removes the last element from the array', () => {
+      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('does not alter the original array', () => {
+      const array = [1, 2, 3];
+      removeElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2, 3]);
+    })
+  })
+})
+*/
