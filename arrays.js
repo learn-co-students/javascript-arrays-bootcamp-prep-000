d41ad3e424a022c873d7dd03f04d237ba8236926
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-03 01:46:56 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -39,18 +39,3 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1);
 }
-
-/*
-describe('removeElementFromEndOfArray(array)', () => {
-    it('removes the last element from the array', () => {
-      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
-    })
-    
-    it('does not alter the original array', () => {
-      const array = [1, 2, 3];
-      removeElementFromEndOfArray(array);
-      expect(array).to.eql([1, 2, 3]);
-    })
-  })
-})
-*/
