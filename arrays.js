3bd473f03f7511d3e8e58689cb1357ecabd9b64b
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 20:12:19 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var ingredient1 = "bread";
+var ingredient2 = "mild cheese";
+var ingredient3 = "sharp cheese";
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, element) {
+  return [...arr, element];
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr, index) {
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length-1);
+}
+
