0c6f589b41c4cd5ecd422eac13b29c01208a61af
Victoria L Sumner <vicsumner@gmail.com>
2018-10-15 20:05:47 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element){
+return addElementToBeginningOfArray()
+foo = [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+function addElementToEndOfArray(array,element){
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+
+}
