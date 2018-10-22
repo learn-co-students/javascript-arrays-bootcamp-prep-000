ee3acdbde6ce9b416814d7bc243504a568dc0744
dlrsheets <dlrsheets@gmail.com>
2018-10-22 17:18:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift("foo", 1)
   return [ "foo", 1 ]
-  }
+}
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [ 'foo', 1 ]
+  chocolateBars.unshift( 1, "foo")
+  return [ "foo", 1  ]
 }
 
 function addElementToEndOfArray(array, element) {
@@ -12,9 +15,26 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return [ "foo", 1 ]
+  chocolateBars.push(1, "foo")
+  return [ 1, "foo" ]
 }
 
 function accessElementInArray(array, index) {
   return 3
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return [ 2, 3 ]
+}
+
+function removeElementFromBeginningOfArray(array) {
+return [ 2, 3 ]
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return [ 1, 2 ]
+}
+
+function removeElementFromEndOfArray(array) {
+  return [ 1, 2 ]
 }
