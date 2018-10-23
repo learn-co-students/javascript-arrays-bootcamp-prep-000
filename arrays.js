87da8ea5e288082a935d55a442ba9d348828d843
wise-procedure-3206 <aaronkim662@gmail.com>
2018-10-23 05:37:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var addElementToBeginningOfArray = new addElementToBeginningOfArray()
+function addElementToBeginningOfArray(addElementToBeginningOfArray,"something"){
+  return addElementToBeginningOfArray()
+}
