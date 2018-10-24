040c057fe4417efde895af1f98fc4c1205b44d16
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-24 16:06:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+const HouseMates = ["Mani","Ange","Ash"];
+function addElementToBeginningOfArray (name, Array) {
+  return [name, ...Array]
+}
+function destructivelyAddElementToBeginningOfArray (name, Array) {
+  return [name, Array]
+}
+
+
+
