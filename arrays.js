1ff3cdf6a21517d09082052a3d487c12ea8cc1d7
Timothy Lim <timothyws.lim@gmail.com>
2018-10-26 22:41:33 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, candy){
+  return [candy, ...chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candy){
+  chocolateBars.unshift(candy);
+  return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, candy){
+  return [...chocolateBars, candy];
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, candy){
+  chocolateBars.push(candy);
+  return chocolateBars;
+}
+function accessElementInArray(array, index){
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1)
+}
