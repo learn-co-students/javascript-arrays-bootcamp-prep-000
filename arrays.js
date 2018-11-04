08eaea07ebbe0862d16e29e04069e171bf0b4d26
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-04 02:54:38 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
-} 
+}
 
 function accessElementInArray(array, index) {
   return array[index];
@@ -41,6 +41,18 @@ function removeElementFromEndOfArray(array) {
 }
 
 /*
+  describe('destructivelyRemoveElementFromEndOfArray(array)', () => {
+    it('returns the array with the last element removed', () => {
+      expect(destructivelyRemoveElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('alters the original array', ()=>{
+      const array = [1, 2, 3];
+      destructivelyRemoveElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2]);
+    })
+  })
+  
   describe('removeElementFromEndOfArray(array)', () => {
     it('removes the last element from the array', () => {
       expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
@@ -52,5 +64,5 @@ function removeElementFromEndOfArray(array) {
       expect(array).to.eql([1, 2, 3]);
     })
   })
-})
+})  
 */
