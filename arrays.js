6d39a83d62876d4eb2057f8d4358908257f428c5
Naenae L <oneprincesslea@gmail.com>
2018-11-13 00:54:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["godiva", "hershey", "moonstruck"] ;
+console.log(chocolateBars);
+chocolateBars.push("bridgewater");
+console.log (chocolateBars);
+chocolateBars.unshift("chocolove");
+["devries", ...chocolateBars];
+console.log(chocolateBars);
