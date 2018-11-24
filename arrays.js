fe96025e3489b1a3d6eca6d3457c6f8474436e8c
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-24 16:05:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -87,5 +87,4 @@ function removeElementFromEndOfArray (array) {
 //***************************************************************
 function removeElementFromEndOfArray (array) {
   return array.slice(0, array.length - 1);
-  return array;
 }
