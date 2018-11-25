5e18348426c7fcd9c2c271cd100526e6be8dfe76
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-25 01:00:14 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  var array = [1]
+  array.unshift("foo")
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  var array = [1]
+}
