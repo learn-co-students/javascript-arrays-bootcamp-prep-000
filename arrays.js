13e2010bd1015a337a2bf1324cec5ddf1632f8f3
Elaina <elainaseki@gmail.com>
2018-11-26 05:30:00 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+const array = [1];
+const element = "foo";
+function addElementToBeginningOfArray(array, element){
+  return ["foo", ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift("foo");
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return[...array, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push("foo");
+  return array;
+}
+
+function accessElementInArray(array, index){
+   array = [1,2,3];
+   return array[2];
+}
+
+ var array1 = [1,2,3];
+function destructivelyRemoveElementFromBeginningOfArray(array1){
+  array1.shift();
+  return array1;
+}
