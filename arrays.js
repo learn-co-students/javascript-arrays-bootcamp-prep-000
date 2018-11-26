7ce16469d3cd3e65a06345c8067c3404539b8ea9
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-26 17:32:01 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,10 +1,41 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var array = [1]
-  array.unshift("foo")
+  return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(){
-  var array = [1]
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return (array[index]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
 }
