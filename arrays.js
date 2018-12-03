795e5a02d327c01e55f07d172c2fd9a25ce21c9f
Paris Dolfman <parisdolfman@gmail.com>
2018-12-03 19:30:19 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,6 +5,7 @@ var chocolateBars = [
   'skittles'
 ]
 
+
  function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element)
     return array
@@ -18,4 +19,29 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element)
   return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop(element)
+  return array
+}
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array = array.slice(1)
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
 }
