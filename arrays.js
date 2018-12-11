a6150ac91b9bdd1a63de7827ec3d26a06ea07d30
Charles Treloar <cwtreloar@gmail.com>
2018-12-11 20:37:49 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,3 +4,25 @@ var chocolateBars = [
   'kit kat',
   'skittles'
   ];
+  
+function addElementToBeginningOfArray(array, element){
+  var myArray = [array];
+  myArray = [element, ...array];
+  return myArray;
+  }
+  
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+  }
+
+function addElementToEndOfArray (array, element){
+  var myArray = [array];
+  myArray = [...array, element];
+  return myArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
