816230b242deb9f0e890a7af9038cad4cc37d1d9
Charles Treloar <cwtreloar@gmail.com>
2018-12-11 21:28:20 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -26,3 +26,27 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  myArray = array.slice(1);
+  return myArray;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  myArray = array.slice(0, array.length - 1)
+  return myArray
+}
