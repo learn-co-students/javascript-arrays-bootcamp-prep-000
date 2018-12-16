776a93e44ebe470c5f520a5a84de7786ce7d8425
Reid Boudreau <rtboudreau@gmail.com>
2018-12-16 22:18:50 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 var gummys = "foo";
 
 function addElementToBeginningOfArray(chocolateBars, gummys) {
@@ -16,8 +17,29 @@ function addElementToEndOfArray(chocolateBars, gummys) {
 
 function destructivelyAddElementToEndOfArray(chocolateBars, gummys) {
   chocolateBars.push(gummys);
+  return chocolateBars;
 }
 
 function accessElementInArray(chocolateBars){
   return chocolateBars[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  chocolateBars = chocolateBars.slice(1)
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop(-1);
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars = chocolateBars.slice(0, chocolateBars.length -1);
+  return chocolateBars;
 }
