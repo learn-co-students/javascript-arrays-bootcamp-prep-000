d1f52681746918cbd33b667ec60cbf12d2887a75
Juan Alorro <jralorro93@gmail.com>
2018-12-17 22:08:43 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,7 +10,43 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 
-function destructivelyAddElementsToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   var x = array.unshift(element);
+  console.log("Result:",x);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var x = [...array];
+  console.log(x.push(element));
+  return x;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var x = array.push(element);
+  console.log("Result:", x);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  console.log(array.shift());
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  console.log(array.pop());
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  var x = array.slice(0,array.length - 1);
   return x;
 }
