8af53200297bb6487abff0f21ced0926c2287463
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 23:28:11 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,62 +1,43 @@
 
-var ingredient1 = 'bread'
-var ingredient2 = 'mild cheese'
-var ingredient3 = 'sharp cheese'
-var ingredient4 = 'butter'
-var ingredient5 = 'tomato'
-var ingredient6 = 'garlic'
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-
-var superheroines = ['catwoman', 'she-hulk', 'mystique']
-superheroines.push('wonder woman')
-
-var cities = ['New York', 'San Francisco']
-cities.unshift('Philadelphia')
-
-var cities = ['New York', 'Miami']
-function addElementToBeginningOfArray(){
-  return ['Houston', ...cities]
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
 }
 
-var states = ['Texas', 'Florida']
-function destructivelyAddElementToBeginningOfArray(){
-  states.unshift('Florida')
-  return states
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
 }
 
-var colors = ['red', 'green']
-function addElementToEndOfArray(){
-  return [...colors, 'blue']
+function addElementToEndOfArray(array, element){
+  return [...array, element]
 }
 
-var planets = ['Mercury', 'Venus']
-function destructivelyAddElementToEndOfArray(){
-  planets.push('Earth')
-  return planets
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
 }
 
-var entrepreneurs = ['Oprah Winfrey', 'Laurene Powell Jobs', 'Arianna Huggington']
-function accessElementInArray(){
-  return entrepreneurs[1]
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
 }
 
-var days = ['Monday', 'Tuesday', 'Wednesday']
-function destructivelyRemoveElementFromBeginningOfArray(){
-  days.shift()
-  return days
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+  return array.slice(1)
 }
 
-var cats = ['Milo', 'Garfield', 'Otis']
-function removeElementFromBeginningOfArray(){
-  return cats.slice(1)
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
 }
 
-var iceCreams ['chocolate', 'vanilla', 'raspberry']
-function destructivelyRemoveElementFromEndOfArray(){
-  return iceCreams.pop()
-}
-
-function removeElementFromEndOfArray(){
-  return iceCreams.slice(0, .length - 1)
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length-1)
+  return array.slice(0, array.length-1)
 }
