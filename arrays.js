09adb7f25e52f97362d7b2f1ab45119a0a4b55bd
Stuart Sewell <sewelly@gmail.com>
2018-12-20 22:43:12 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,47 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+// var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//
+// function addElementToBeginningOfArray(array, element){
+//   return [element, ...array]
+// }
+//
+// function destructivelyAddElementToBeginningOfArray(array, element){
+//   array.unshift(element)
+//   return array
+// }
+//
+// function addElementToEndOfArray(array, element){
+//   return [...array, element]
+// }
+//
+// function destructivelyAddElementToEndOfArray(array, element){
+//   array.push(element)
+//   return array
+// }
+//
+// function accessElementInArray(array, index){
+//   return array[index]
+// }
+//
+// function destructivelyRemoveElementFromBeginningOfArray(array){
+//   array.shift()
+//   return array
+// }
+//
+// function removeElementFromBeginningOfArray(array){
+//   return array.slice(1)
+// }
+//
+// function destructivelyRemoveElementFromEndOfArray(array){
+//   array.pop()
+//   return array
+// }
+//
+// function removeElementFromEndOfArray(array){
+//   return array.slice(0, array.length -1)
+// }
+
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittle"]
 
 function addElementToBeginningOfArray(array, element){
   return [element, ...array]
@@ -28,8 +71,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
-}
+   return array.slice(1)
+ }
 
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
