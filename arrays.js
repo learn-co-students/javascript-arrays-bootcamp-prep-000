c5a5a0bba65e6c0b7eff1418655967a5642db69e
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-21 04:57:25 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,1 +1,56 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
+/*
+ describe('removeElementFromEndOfArray(array)', () => {
+    it('removes the last element from the array', () => {
+      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('does not alter the original array', () => {
+      const array = [1, 2, 3];
+      removeElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2, 3]);
+    })
+  })
+})
+*/
