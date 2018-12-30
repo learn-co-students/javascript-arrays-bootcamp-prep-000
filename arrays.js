618563d7313f090ae45e9f56546a63c036942c1f
Ranae Rodgers <ranae.rodgers@icloud.com>
2018-12-30 15:24:28 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,26 +1,27 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
+  var newArray = [element,...array];
+  return newArray;
+}
+function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element);
-  return array
+  return array;
 }
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array
+function addElementToEndOfArray(array,element)  {
+  var newArray = [...array, element];
+  return newArray;
 }
-function addElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element);
-  return array
-}
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array
+  return array;
 }
 function accessElementInArray(array, index) {
   return array[index];
 }
-function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+function removeElementFromBeginningOfArray(array,element) {
+  array.shift(element);
+  return array;
 }
-function removeElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array, element) {
   return array.slice(0, array.length -1)
 }
