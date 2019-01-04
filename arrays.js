3a8decf1b0edd23c9bcd8315525d1ef700cbce83
William Saffran <willh.saffran@gmail.com>
2019-01-04 22:21:53 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,44 @@
+
+//Creating Arrays
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+// Adding Elements
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+// Accessing an element
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+// Removing an element
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0, array.length-1)
+  return newArray
+}
