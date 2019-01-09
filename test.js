f583338b04b8a244c97dbc216b49ce8b042ef218
Simon Mei <nagaromo@gmail.com>
2019-01-09 20:13:48 +0000

    Done.

---
+++ test.js
@@ -0,0 +1,11 @@
+var stuff = [1, 2, 3, 4, 5];
+var ayy1 = stuff.slice(0, stuff.length)
+var ayy2 = stuff.slice(0, stuff.length-1)
+var ayy3 = stuff.slice(0, stuff.length-2)
+var ayy4 = stuff.slice(0, 1)
+
+console.log(stuff.length);
+console.log(ayy1);
+console.log(ayy2);
+console.log(ayy3);
+console.log(ayy4);
