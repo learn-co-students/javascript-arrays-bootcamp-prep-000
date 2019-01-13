d0ec0787cf2ba9cfa822c8e76d3a1cbe50508f65
Dennis Chung <chungdennis6@gmail.com>
2019-01-13 19:50:15 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,22 +1,41 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
-function destructiveltAddElementToBeginningOfArray(chocolateBars, candyString){
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   return chocolateBars;
 }
+
 function addElementToEndOfArray(chocolateBars, candyString){
   return[... chocolateBars, candyString];
 }
+
 function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
   chocolateBars.push(candyString);
 return ChocolateBars;
 }
+
 function accessElementinArray(array, index){
   return array[index];
 }
-function destructivelyRemoveElementFromBeginningOfArray(chcolateBars){
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   chocolateBars.shift();
   return chocolateBars;
 }
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolatebars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
+
