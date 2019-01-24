8ae8559b88983571e62863113eb7aec62f8a6318
Jason Velez <jvelez20@yahoo.com>
2019-01-24 13:57:10 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars =['snickers','hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+array.unshift(element);
+return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element);
+return array;
+}
+function addElementToEndOfArray(array, element) {
+array.push(element);
+return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+array.push(element);
+return array;
+}
+
+function removeElementFromEndOfArray(array, element) {
+    array = array.slice
+return array; 
+}
+
+function accessElementInArray(array, index) {
+array.push(element);
+return array;
+}
+addElementToBeginningOfArray(array, element)
+
+
+
+
+
+
+
