d30df9ea78c10de5569703e69e615d68593dc641
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-28 04:41:32 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
 /*
  describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
