0d67fb517c086af002abe99df0dc719cd63d4ab2
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-12 04:07:40 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, ele) {
+  new_array = [ele, ...array]
+  return new_array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, ele) {
+  array.unshift(ele)
+  return array
+}
+
+function addElementToEndOfArray(array, ele) {
+  new_array = [...array, ele]
+  return new_array
+}
+
+function destructivelyAddElementToEndOfArray(array, ele) {
+  array.push(ele)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
