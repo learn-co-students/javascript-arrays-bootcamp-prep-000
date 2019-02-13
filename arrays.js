762ba346e1c2cc58aaf24c64bd9f7575d4106274
Constantine Chutis <constantinechutis@gmail.com>
2019-02-13 21:08:37 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  [...array, element];
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  [element, ...array];
+  return array;
+}
+
+function accessElementInArray(array, num) {
+  return array[num];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice();
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
+
+
+
+
+
+
+
