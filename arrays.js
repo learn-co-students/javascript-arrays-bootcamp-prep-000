c44c609ae36d3381657da1d04b2d6b889735568b
L D <tapircode@gmail.com>
2019-02-16 02:45:25 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,49 @@
+chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element){
+  let newArr = [element, ...array] ;
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  array.unshift(element);
+  return array;
+}
+
+
+function addElementToEndOfArray(array, element){
+  let newArr = [...array, element] ;
+  return newArr;
+  
+}
+
+function destructivelyAddElementToEndOfArray (array, element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array,  index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  let newArr = array.slice(1);
+  return newArr;
+}
+
+function removeElementFromEndOfArray(array){
+  let newArr = array.slice(0, array.length -1)
+  return newArr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array;
+}
+
+
