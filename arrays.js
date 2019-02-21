0e90b6f91c25871f74164d305ff6dc33cb84327a
Denzel Archie <archie4denzel@gmail.com>
2019-02-21 21:57:56 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,50 +1,41 @@
-var ingredient1 = "bread"
-var ingredient2 = " mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var chocolateBars = ['snicker', 'hundred grand', 'kitkat', 'skittles']
-
-function addElementToBeginningOfArray(array, n) {
-  return [n, ... array]
+ function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, n) {
-  array.unshift(n);
-  return array;
-}
-
-function addElementToEndOfArray(array, n) {
-  return [...array, n]
-}
-
-function destructivelyAddelementToEndOfArray(array, n) {
-  return array.push(n)
+ function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
   return array
 }
 
-function accessElementInArray(array, n) {
-  array[n]
+ function addElementToEndOfArray(array, element) {
+  return [...array, element]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+ function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
   return array
 }
 
-function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array 
+ function destructivelyRemoveElementFromBeginningOfArray(array) {
+   array.shift()
+   return array
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
+ function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
-  return array 
+  return array
 }
 
-function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  return array
+ function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+ function accessElementInArray(array, index) {
+  return array[index]
 }
