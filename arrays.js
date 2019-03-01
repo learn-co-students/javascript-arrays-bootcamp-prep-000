59f62583edc0defb322a0c80dbc24ecd8142e937
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:40:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,41 +1,41 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(a, b) {
-  return [b, ...a]
+  return [b, ...a];
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b) {
-  a.unshift(b)
-  return a
+  a.unshift(b);
+  return a;
 }
 
 function addElementToEndOfArray(a, b) {
-  return [...a, b]
+  return [...a, b];
 }
 
 function destructivelyAddElementToEndOfArray(a, b) {
-  a.push(b)
-  return a
+  a.push(b);
+  return a;
 }
 
 function accessElementInArray(a, b) {
-  return a[b]
+  return a[b];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(a) {
-  a.shift()
-  return a
+  a.shift();
+  return a;
 }
 
 function removeElementFromBeginningOfArray(a) {
-  return a.slice(1)
+  return a.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(a) {
-  a.pop()
-  return a
+  a.pop();
+  return a;
 }
 
 function removeElementFromEndOfArray(a) {
-  return a.slice(0, a.length - 1)
+  return a.slice(0, a.length - 1);
 }
