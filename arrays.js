e861ea324e577b9fdff8d236c3fc387f1bd7e3d6
Annia Forney <anniaforney@gmail.com>
2019-03-05 19:56:50 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element) ; 
+  return array
+}
+
+function addElementToEndOfArray (array, element) {
+  console.log (array)
+  return[...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element) ;
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray (array, element) {
+array.pop()
+array 
+}
+
+function removeElementFromEndOfArray (array) {
+  array.slice (0,
+  array.length - 1)
+  array 
+}
+
+function accessElementInArray(array, index)
+
+ myArray = [index]
+ 
+ myArray
