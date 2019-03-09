1ee870eba0c73cdefb9a3fdead509e7ffe73d5e6
Andrew Waller <andrewhwaller@gmail.com>
2019-03-09 21:41:56 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray()
+
+function destructivelyAddElementToBeginningOfArray() {
+array.unshift("element")
+array = ["element",...array]
+return array
+}
+
+function addElementToEndOfArray() {
+array.push("element");
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray() {
+
+}
