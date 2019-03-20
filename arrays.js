dfd049b3542a07c10197f72811a3cfd5d578ea3f
Greg Morgan <gregm9999@gmail.com>
2019-03-20 23:37:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -28,16 +28,12 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice();
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
-}
-
-function removeElementFromBeginningOfArray(array) {
-    return array.slice(1)
 }
 
 function removeElementFromEndOfArray(array) {
