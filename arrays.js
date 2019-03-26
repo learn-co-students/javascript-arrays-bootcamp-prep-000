092b0b54646657a53de4fc901dfe021afc075025
Aodhan Vanderbilt <tamegorilla@gmail.com>
2019-03-26 00:34:05 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -4,25 +4,29 @@ function addElementToBeginningOfArray(array, element){
 return[element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-return array.unshift(element)
+array.unshift(element)
+return array
 }
 function addElementToEndOfArray(array, element){
 return[...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element){
-return array.push(element)
+array.push(element)
+return array
 }
 function accessElementInArray(array, index){
 return array[2]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
- return array.shift()
+ array.shift()
+ return array
 }
 function removeElementFromBeginningOfArray(array){
 return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-return array.pop()
+array.pop()
+return array
 }
 function removeElementFromEndOfArray(array){
 return array.slice(0, array.length -1)
