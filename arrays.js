af1468eb63d9add4b766622b1ece2bd2c0396c97
Dan Hope <dan@ckrit.info>
2019-03-29 19:33:59 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kit kat',
+  'skittles'];
+  
+  function addElementToBeginningOfArray(arr, ele) {
+    var newArr = [ele, ...arr];
+    return newArr;
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(arr, ele) {
+    arr.unshift(ele);
+    return arr;
+  }
+  
+  function addElementToEndOfArray(arr, ele) {
+    var newArr = [...arr, ele];
+    return newArr;
+  }
+  
+  function destructivelyAddElementToEndOfArray(arr, ele) {
+    arr.push(ele);
+    return arr;
+  }
+  
+  function accessElementInArray(arr, idx) {
+    return arr[idx];
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(arr) {
+    arr.shift();
+    return arr;
+  }
+  
+  function removeElementFromBeginningOfArray(arr) {
+    var newArr = arr.slice(1);
+    return newArr;
+  }
+  
+  function destructivelyRemoveElementFromEndOfArray(arr) {
+    arr.pop();
+    return arr;
+  }
+  
+  function removeElementFromEndOfArray(arr) {
+    var newArr = arr.slice(0, arr.length -1);
+    return newArr;
+  }
+  
