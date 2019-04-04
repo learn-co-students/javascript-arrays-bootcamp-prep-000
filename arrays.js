eb7bdd46b960eb9796634f13babbe560d6dcdb34
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-04 12:31:23 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,1 @@
 var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
-//define two functions, Both functions take two parameters, an array and an element to add to the beginning of the array,
-//and both functions should add the element to the beginning of the array and then return the whole array
