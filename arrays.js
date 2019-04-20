92ae6e7c72a0343d574651a0e084e44ed9905048
Danielle Martinoli <daniechris5@gmail.com>
2019-04-20 22:25:52 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return console.log(array[1])
+  return console.log(array[0])
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
@@ -25,7 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array.slice(0)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
