f9ba36b43c91dfcf7e2a01389fe67197c0baccde
Min Kang <kangm914@gmail.com>
2019-04-24 16:33:18 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var new_array = [];
+  new_array = array;
+  return (element, ...array);
+}
+
