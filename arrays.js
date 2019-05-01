ea60d56a561730d5d312b341cae2f28d083795d0
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-01 06:19:57 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -5,39 +5,40 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array.unshift(element);
   return array 
-} 
+}
 
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array 
 }
 
 function accessElementInArray(array, index) {
-  return array[index]
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+  array.shift();
   return array 
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  array = array.slice(1);
+  return array 
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+  array.pop();
   return array
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
+  array = array.slice(0, array.length - 1);
+  return array
 }
-
 
