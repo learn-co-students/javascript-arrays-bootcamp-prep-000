3765c011c6dc11364d2c27d78428cc9fe8bad710
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-21 03:04:43 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,12 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-
 function addElementToBeginningOfArray(array, element) {
   const result = [`${element}`, ...array];
   return result;
 } 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var answer = array.unshift(`${element}`);
+  var answer = array.unshift(element);
   return answer;
 }
