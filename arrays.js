284a46979e576e9bb99a7528b22a5c3f91f9a70d
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-21 02:50:23 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element) {
+  const result = [`${element}`, ...array];
+  return result;
+} 
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var answer = array.unshift(`${element}`);
+  return answer;
+}
