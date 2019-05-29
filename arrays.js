2cd65f4b4cb9a9363b54f074cc6a45ef8e652c6b
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-29 21:59:24 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -19,7 +19,25 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array
 }
 
+function accessElementInArray(array, index){
+  return array[index];
+}
 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
 
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
 
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
 
