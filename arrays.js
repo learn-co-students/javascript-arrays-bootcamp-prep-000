d781a7414603162fecd5f4f87f16c79c4d42acf6
David Little <littledavid175@gmail.com>
2019-06-01 19:21:48 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars=["snickers, hundred grand, kitkat, skittles"]
+function addElementToBeginningOfArray (element, array) {
+return [array,...element]
+
+  }
+  function destructivelyAddElementToBeginningOfArray (element, array) {
+   array.unshift(element)
+  return array
+    
+  }
+  function addElementToEndOfArray (element,array) {
+    return[...element,array]
+    
+  }
+  function destructivelyAddElementToEndOfArray (element,array) {
+    return[]
+    
+  }
+  function accessElementInArray (index,array) {
+    return 3
+    
+  }
+  function destructivelyRemoveElementFromBeginningOfArray (array) {
+    return 
+    
+  }
+  function removeElementFromBeginningOfArray (array) {
+    
+    
+  }
+  function destructivelyRemoveElementFromEndOfArray (array) {
+    return 
+    
+  }
+  function removeElementFromEndOfArray (array) { 
+    return 
+    
+  }
