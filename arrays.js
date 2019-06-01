be627515f8acf3a0dbd7f5712e4ba2892d3d57a8
Joshua Coleman <procoleman77@outlook.com>
2019-06-01 23:47:26 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var ingredient1 = "bread" ;
+var ingredient2 = "mild cheese";
+var ingredient3 = "sharp cheese";
+var ingredient4 = "butter";
+var ingredient5 = "tomato" ;
+var ingredient6 = "garlic" ;
+
+var chocolateBars = ["sinckers","hundred grand","kitkat","skittles"] ;
+
+var addElementToBeginningOfArray = [ "Josh","Alex","Peter"];
+addElementToBeginningOfArray.push("Tawanna");
+console.log(addElementToBeginningOfArray);
+
+var destructivelyAddElementToBeginningOfArray= [ "Apple", "Orange", "Lemon"];
+console.log(destructivleyAddElementToBeginningOfArray);
+
