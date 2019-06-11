9037d881132d53b22eee6e01b42a1d432988fffc
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-11 15:42:30 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -27,3 +27,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+ return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+fu
