347cca863a9d4d10bc7ea5994e99e0062ea548ce
Akinyosoye Folasayo <fola4ril@gmail.com>
2019-07-03 14:13:31 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var moreCandy = [element, ...array];
+  return moreCandy;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var lessCandy = [...array, element];
+  return lessCandy;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var result = array.slice(1);
+  return result;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var result = array.slice(0, array.length -1);
+  return result;
+}
