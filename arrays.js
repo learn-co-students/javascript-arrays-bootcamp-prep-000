efa47125096aadd687f68e3b1ac865bbfd1fd38e
Mykle jones <mykle.j91@gmail.com>
2019-07-09 05:15:50 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,52 @@
+var chocolateBars = [
+"snickers",
+"hundred grand",
+"kitkat",
+"skittles"];
+
+// 
+
+function addElementToBeginningOfArray (array, element) {
+  console.log ("array, element")
+  return addElementToBeginningOfArray
+  }
+   //  
+   
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return addElementToBeginningOfArray
+  }
+
+function addElementToEndOfArray(array, element){
+  return addElementToEndOfArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  console.log ("array","element")
+}
+
+function accessElementInArray (array, index){
+  return  array[index]
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(array){
+   array.shift() 
+   return destructivelyRemoveElementFromBeginningOfArray
+  }
+  
+  function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+  }
+    
+  function removeElementFromEndOfArray(array){
+    return array.pop 
+  }
+  
+  function removeElementFromEndOfArray (array) {
+   return array.slice(0,length -1)
+  }
+  
+  function addElementToBeginningOfArray (array, element){
+ 
+    }
+  
+    
