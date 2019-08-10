0b5c98a36fa458160381dcfa1aa3914dbb63481a
john L <johnleavell@gmail.com>
2019-08-10 16:12:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,52 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+]
+
+var candyString = "milky way"
+
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+chocolateBars.unshift(candyString);
+return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+chocolateBars.push(candyString);
+return chocolateBars;
+}
+
+
+function accessElementInArray(chocolateBars, index) {
+  return (chocolateBars[2]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift(candyString);
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars=chocolateBars.slice(1);
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  chocolateBars.pop()
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray(chocolateBars) {
+  chocolateBars=chocolateBars.slice(0, chocolateBars.length-1);
+  return chocolateBars;
+}
