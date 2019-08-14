27aa6136ea97650d66582aff07cbb596b4dcfa53
Vanessa Corlette <nessacor16@gmail.com>
2019-08-14 03:46:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -30,10 +30,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice();
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
   return array;
 }
 
