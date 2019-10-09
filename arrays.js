82668b83cad04a024d047e317e46225a555acb2f
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-10-09 03:20:03 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array,element){
-  
+  return [...array,element]
 }
 
 function destructivelyAddElementToEndOfArray(array,element){
