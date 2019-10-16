abaa44f5a4c3cb8525117b9f368500ecd286e0ea
comical-tag-7436 <shawtynow2008@gmail.com>
2019-10-16 00:05:30 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(array, element){
+  
+}
+function destructivelyAddElementToBeginningOfArray(array, element){ 
+  
+}
+function  addElementToEndOfArray(array, element){
+  
+}
+function  destructivelyAddElementToEndOfArray(array, element){
+  
+}
+function  accessElementInArray(array, index){ ("3")
+  
+}
+function destructivelyAddElementToEndOfArray(array){
+  
+}
+function removeElementFromBeginningOfArray(array){
+
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+
+}
+function removeElementFromEndOfArray(array){
+
+}
