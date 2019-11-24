7b13cf5447e803952ac438f62bc1203d75cf5434
Avinash Nepal <nepalavinash@gmail.com>
2019-11-24 19:13:21 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -34,7 +34,13 @@ return array.slice(1)
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
+}
 function removeElementFromEndOfArray(array) {
  array.slice(0)
- return array.slice(36)
+ return array.slice(n)
+}
+
+function removeElementFromEndOfArray(array) {
+return array.slice(0,array.length-1)
+
 }
