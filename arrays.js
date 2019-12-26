95f99262b85f643e3929cce938e98bf5c481d076
Breaun <pynkylynn@gmail.com>
2019-12-26 21:15:26 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBar = [“  snicker”, ”hundred grand”, ”kitkat” and “sk
 
 var destructiveRemoveElementFromBeginningOfArray = [’chocolateBar’,’blues’,’clue’];
 destructiveRemoveElementFromBeginningOfArray.slice(1);
-var addElementToBeginningOfArray=[’blue’];
+var addElementToBeginningOfArray=[’blues’];
 var destructivelyAddElementToBeginning =();
