dbec35682a211c1aa39963cf72e711e16deada5f
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-20 04:04:34 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+  [...chocolateBars, "payday"]
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  chocolateBars.unshift("milkyway")
+}
+
+function addElementToEndOfArray() {
+  ["crunch", ...chocolateBars]
+}  
+
+function destructivelyAddElementToEndOfArray() {
+  chocolateBars.push("reeses")
+}
+
+function accessElementInArray() {
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  
+}
+
+function removeElementFromBeginningOfArray() {
+  
+}
+
+function removeElementFromEndOfArray() {
+  
+}
