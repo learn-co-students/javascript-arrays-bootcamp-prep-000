7bfab932523b679376e01677f747a39863d26163
Devin Benson <devinbenson@gmail.com>
2020-02-02 19:04:02 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, array[0]]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift('foo')
+  return array
+}
+function addElementToEndOfArray(array,element){
+  array = [1]
+  array.push('foo')
+  return array
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index){
+  return array[2];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+array.shift()
+return array
+}
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+array.pop()
+return array
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
+
+
