69f12af7745bf667f31ef317520e74ce83803f0b
Nikita Savelev <nsavelev@yahoo.com>
2020-03-08 18:27:35 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Arrays
+p# JavaScript Arrays
 
 ## Objectives
 
--- arrays.js
+++ arrays.js
@@ -3,11 +3,43 @@ var chocolateBars = [
   "hundred grand",
   "kitkat",
   "skittles"  
-  ];
+];
+
+var chocolateBars2 = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles",
+  "Mars"
+];
   //adds to the beginning
 function addElementToBeginningOfArray(array,element){
-  array.unshift(element)
-  return array
+  chocolateBars2.unshift()
+  return chocolateBars2
 
 }
 
+
+  //adds to the beginning
+//function addElementToBeginningOfArray(array,element){
+  //array.unshift()
+  
+
+//}
+
+
+
+ //adds to the beginning destructively
+ //function destructivelyAddElementToBeginningOfArray(array, element){
+  // array[element] = 5
+// }
+ 
+ 
+ //adds to the end
+ //function addElementToEndOfArray(array,element){
+  // array.push() 
+ //}
+ //
+ //function destructivelyAddElementToEndOfArray(array,element){}
+ //
+ //
