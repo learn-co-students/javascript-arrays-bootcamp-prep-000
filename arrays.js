462106e71440f86fa4f840732da15b4f3daf6c35
Nikita Savelev <nsavelev@yahoo.com>
2020-03-08 17:37:35 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"  
+  ];
+  //adds to the beginning
+function addElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array
+
+}
+
