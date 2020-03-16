16f974fe03e8bac7a3c5d52901f7a0501b775c73
Connor Bennett <cunit638@gmail.com>
2020-03-16 15:12:06 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,4 +3,14 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray(array, element)
 {
 chocolateBars.unshift("Connor")
+
 }
+
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  chocolateBars = ["Connor", ...chocolateBars]
+
+}
+return addElementToBeginningOfArray()
+return destructivelyAddElementToBeginningOfArray()
