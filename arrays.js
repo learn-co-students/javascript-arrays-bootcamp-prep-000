37486508970470243430bcbf27d4dcc7c3915933
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-12 03:10:11 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -3,8 +3,9 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //add element to the beginning of array
 
 function addElementToBeginningOfArray(array, element){
-  var new_array = [...element, array];
-  return new_array;
+  var arr = array;
+  var new_arr = [...element.split(","), arr];
+  return new_arr;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
@@ -14,8 +15,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 //add element to the end of array
 
 function addElementToEndOfArray(array, element){
-  var new_array = [array, ...element];
-  return new_array;
+  //[array, ...element];
+  return [array, ...element.split(",")];
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
@@ -31,15 +32,18 @@ function accessElementInArray(array, index){
 //Removing Elements from arrays
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift();
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
+  var new_array = array.slice(1);
+  return new_array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop();
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array){
