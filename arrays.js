f88598bd7de909705725489e6bda9c0c40d0582f
Cedric Patton <ceddyp117@gmail.com>
2020-06-09 16:00:41 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -200,4 +200,36 @@ function removeElementFromEndOfArray(){
 }
 
 
+let items = [1, 2, 3, 4]
+ 
+// this will remove everything after index 1 (inclusive)
+// it returns the removed items: [2, 3, 4]
+items.splice(1)
+ 
+items // [1]
+ 
+items = [1, 2, 3, 4]
+ 
+ // "at index 1, remove 1 item"
+ // it returns the removed item(s): [2]
+items.splice(1, 1)
+ 
+items // [1, 3, 4]
+ 
+items = [1, 2, 3, 4]
+ 
+// "at index 1, remove 1 item and add 6 and add 7"
+// it returns the removed items: [2]
+// and adds the items to add starting at the removal index
+items.splice(1, 1, 6, 7)
+ 
+items // [1, 6, 7, 3, 4]
 
+
+var items = [1, 2, 3, 4, 5]
+ 
+// let's remove the third element
+ 
+// a slice from the start up to but not including index 2 (the third element)
+// and a slice from index 3 to the end
+[...items.slice(0, 2), ...items.slice(3)] // [1, 2, 4, 5]
