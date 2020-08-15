7dceff4d2b3d8c25d47669159534bf28f1d0a725
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-15 20:15:36 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,44 +1,45 @@
- var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
-console.log(chocolateBars)
-//["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, element){
-return[element, ...array]
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
 }
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return(array)
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array
 }
-function addElementToEndOfArray(array, element){
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return(array)
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift()
+  return array
 }
-function addElementToEndOfArray(array, element){
-   return [...array, element]
-  }
-  function destructivelyAddElementToEndOfArray(array, element){
-    array.push(element)
-    return array
-  }
-  function accessElementInArray(array, index){
-    return array[index]
-  }
-  function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift() 
-  array
-   }
-  function RemoveElementFromBeginningOfArray(array){
+
+function removeElementFromBeginningOfArray(array){
   return array.slice(1)
- }
-  
-  function destructivelyRemoveElementFromEndOfArray(array) {
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
 
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)}
+  return array.slice(0, array.length - 1)
+}
