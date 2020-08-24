cd3a27daad4323dcd76467a491bb7f50999ab42c
Klaartje Claire <rosemarieclaire@gmail.com>
2020-08-24 13:26:05 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitcat",
+  "skittles"];
+
+function addElementToBeginningOfArray(){
+chocolateBars.unshift("foo");
+...chocolateBars, "foo";
+} 
+  
+  
