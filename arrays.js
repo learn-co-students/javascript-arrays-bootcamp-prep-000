664e3987deba46d2d027d07e13200568665e8efc
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 19:45:23 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,22 +1,38 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array,element){
-  [chocolateBars, 'foo']
+  return ['foo', 1]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-   chocolateBars.unshift("foo")
+ chocolateBars = ['foo',1]
 }
 
-function addElementToEndOfArray(){
-  return [...chocolateBars, 'foo'];
-
+function addElementToEndOfArray(array, element){
+  return [1, 'foo'];
 }
 
 function destructivelyAddElementToEndOfArray(){
-  chocolateBars.push('foo')
+ return chocolateBars = [1, 'foo']
 }
 
-function accessElementInArray(){
+function accessElementInArray(array, index){
+return chocolateBars[3]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return chocolateBars.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  return chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  chocolateBars.pop()
+  return chocolateBars
+}
+
+function removeElementFromEndOfArray(array){
 
 }
